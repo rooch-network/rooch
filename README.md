83b92a4ac9eb48555b9f1bef27ec4abbdf5a11d9
jolestar <jolestar@gmail.com>
2023-01-30 19:20:22 +0800

    improve README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# smt
-Sparse Merkle Tree
+# Sparse Merkle Tree
+
+A Rust library that implements a Sparse Merkle tree for a key-value store, provide state proof for the storage layer.
+
+The origin source from [Starcoin](https://github.com/starcoinorg/starcoin/blob/f9ba8b637bade2eb38ae9e62a7e75f2c18ce7289/commons/forkable-jellyfish-merkle) and [Diem](https://github.com/diem/diem/tree/4eb8093bb190c1dca3706d9a7226a39f2089ef7a/storage/jellyfish-merkle). The tree's optimisations specified in the [Diem whitepaper](https://diem-developers-components.netlify.app/papers/the-diem-blockchain/2020-05-26.pdf).
+
+
+This repository does or will do the following improvements:
+
+1. released as a standalone crate for other projects to depend on.
+2. refactor the interface to make it easier to use.
+4. customize hash methods.
+5. customize encode/decode methods.
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,71 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
+use jellyfish_merkle::{hash::HashValue, proof::SparseMerkleProof};
+use anyhow::Result;
 
 pub mod jellyfish_merkle;
+
+/// Store the tree nodes
+pub trait NodeStore{
+   
+}
+
+/// Store the original values
+pub trait ValueStore{
+   
+}
+
+impl NodeStore for std::collections::HashMap<HashValue, Vec<u8>>{
+   
+}
+
+impl ValueStore for std::collections::HashMap<HashValue, Vec<u8>>{
+
+}
+
+pub struct SMTree<NS:NodeStore,VS:ValueStore>{
+    pub node_store: NS,
+    pub value_store: VS,
+}
+
+impl<NS:NodeStore,VS:ValueStore> SMTree<NS,VS>{
+    pub fn new(node_store: NS, value_store: VS) -> Self{
+        SMTree{
+            node_store,
+            value_store,
+        }
+    }
+
+    pub fn root(&self) -> HashValue{
+        todo!()
+    }
+
+    pub fn get<K:AsRef<[u8]>+ ?Sized>(&self, _key: &K) -> Result<Option<Vec<u8>>>{
+        todo!()
+    }
+
+    pub fn get_with_proof<K:AsRef<[u8]>+ ?Sized>(&self, _key: &K) -> Result<(Option<Vec<u8>>,SparseMerkleProof)>{
+        todo!()
+    }
+
+    pub fn insert<K:Into<Vec<u8>>, V: Into<Vec<u8>>>(&mut self, _key: K, _value: V) -> Result<()>{
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_smt(){
+        let node_store = std::collections::HashMap::new();
+        let value_store = std::collections::HashMap::new();
+        let mut smt = SMTree::new(node_store, value_store);
+        let key = "key";
+        let value = "value";
+        smt.insert(key.to_string(), value).unwrap();
+        smt.get_with_proof(key).unwrap();
+    }
+}
