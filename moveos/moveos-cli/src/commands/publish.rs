ffb16587bdb6156045e8979667bdae6aecfbedf3
0xpause <zhouyafei125@gmail.com>
2023-04-22 21:58:10 +0800

    fix package modules

--- moveos/moveos-cli/src/commands/publish.rs
+++ moveos/moveos-cli/src/commands/publish.rs
@@ -1,6 +1,7 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
+use anyhow::ensure;
 use clap::Parser;
 use move_binary_format::CompiledModule;
 use move_bytecode_utils::dependency_graph::DependencyGraph;
@@ -25,35 +26,26 @@ impl Publish {
     ) -> anyhow::Result<()> {
         let rerooted_path = reroot_path(package_path)?;
         let package = config.compile_package_no_exit(&rerooted_path, &mut stderr())?;
+        let modules = package.root_modules_map().iter_modules_owned();
 
-        //TODO ensure all module at same address.
-        //include all module and dependency modules
-        let modules = package.all_modules_map();
-        let graph = DependencyGraph::new(modules.iter_modules());
-        let order_modules = graph.compute_topological_order()?;
-        let modules: Vec<CompiledModule> = order_modules.cloned().collect();
-
-        // TODO: check all modules at same address
-        let id = modules[0].self_id();
-        // List all module ids and unique them
-        let mut module_ids = modules
-            .clone()
-            .into_iter()
-            .map(|m| m.self_id().address().clone())
-            .collect::<Vec<_>>();
-        module_ids.sort();
-        module_ids.dedup();
-
-        println!("module id: {:?}", module_ids);
-
+        let pkg_address = modules[0].self_id().address().to_owned();
         let mut bundles: Vec<Vec<u8>> = vec![];
+        println!("Packaging Modules:");
         for module in modules {
+            println!("\t {}", module.self_id());
+            let module_address = module.self_id().address().to_owned();
+            ensure!(
+                module_address == pkg_address,
+                "module's address ({:?}) not same as package module address {:?}",
+                module_address,
+                pkg_address.clone(),
+            );
             let mut binary: Vec<u8> = vec![];
             module.serialize(&mut binary)?;
             bundles.push(binary);
         }
 
-        let sender = *id.address();
+        let sender = pkg_address;
         let txn = MoveTransaction::ModuleBundle(bundles);
         let txn = SimpleTransaction::new(sender, txn);
         self.client.submit(txn).await?;
