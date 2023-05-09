66463205206b5e6fb990734fb1f35c099aa0c7ac
sven <sven.tan@icloud.com>
2023-05-10 00:33:44 +0800

    fix fmt

--- crates/rooch-types/src/account.rs
+++ crates/rooch-types/src/account.rs
@@ -138,14 +138,10 @@ impl EncodeDecodeBase64 for PublicKey {
     }
 
     fn decode_base64(value: &str) -> Result<Self, eyre::Report> {
-        let bytes =
-            Base64::decode(value).map_err(|e| eyre!("{}", e.to_string()))?;
+        let bytes = Base64::decode(value).map_err(|e| eyre!("{}", e.to_string()))?;
 
-        let pk = Ed25519PublicKey::from_bytes(
-            bytes
-                .get(1..)
-                .ok_or_else(|| eyre!("Invalid length"))?,
-        )?;
+        let pk =
+            Ed25519PublicKey::from_bytes(bytes.get(1..).ok_or_else(|| eyre!("Invalid length"))?)?;
 
         Ok(PublicKey::Ed25519(pk))
     }
