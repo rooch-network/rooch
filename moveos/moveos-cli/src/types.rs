5836b126eb5a124cad04cd774ff1471caf87b76e
sven Tan <sven.tan@icloud.com>
2023-05-13 09:20:32 +0800

    Feature/named addresses (#122)
    * fix https://github.com/rooch-network/rooch/issues/119
    
    * Support for Named Address

--- moveos/moveos-cli/src/types.rs
+++ moveos/moveos-cli/src/types.rs
@@ -1,20 +1,51 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::{anyhow, bail, Result};
+use anyhow::{anyhow, bail, Error};
 use clap::Parser;
 use move_core_types::account_address::AccountAddress;
 use std::str::FromStr;
+use thiserror::Error;
+
+// TODO: move moveos-cli to rooch, use RoochError
+/// CLI Errors for reporting through telemetry and outputs
+#[derive(Debug, Error)]
+pub enum ErrorX {
+    #[error("Invalid arguments: {0}")]
+    CommandArgumentError(String),
+}
+
+/// A wrapper around `AccountAddress` to be more flexible from strings than AccountAddress
+#[derive(Clone, Copy, Debug)]
+pub struct AccountAddressWrapper {
+    pub account_address: AccountAddress,
+}
+
+impl FromStr for AccountAddressWrapper {
+    type Err = ErrorX;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(AccountAddressWrapper {
+            account_address: load_account_arg(s)?,
+        })
+    }
+}
 
 /// Loads an account arg and allows for naming based on profiles
-pub fn load_account_arg(str: &str) -> Result<AccountAddress> {
+pub fn load_account_arg(str: &str) -> Result<AccountAddress, ErrorX> {
     if str.starts_with("0x") {
-        AccountAddress::from_hex_literal(str)
-            .map_err(|err| anyhow!("Failed to parse AccountAddress {}", err))
+        // AccountAddress::from_hex_literal(str).map_err(|err| {
+        //     CliError::CommandArgumentError(format!("Failed to parse AccountAddress {}", err))
+        // })
+
+        AccountAddress::from_hex_literal(str).map_err(|err| {
+            ErrorX::CommandArgumentError(format!("Failed to parse AccountAddress {}", err))
+        })
     } else if let Ok(account_address) = AccountAddress::from_str(str) {
         Ok(account_address)
     } else {
-        bail!("'--account' must be provided".to_string(),)
+        Err(ErrorX::CommandArgumentError(
+            "'--account' or '--profile' after using rooch init must be provided".to_string(),
+        ))
     }
 }
 
