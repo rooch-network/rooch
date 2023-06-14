25f67423440fb0e2087fc725c97b4603b8d5d8e5
0xpause <zhouyafei125@gmail.com>
2023-06-14 08:53:57 +0800

    fix fmt

--- crates/rooch/src/commands/move_cli/commands/mod.rs
+++ crates/rooch/src/commands/move_cli/commands/mod.rs
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod build;
+pub mod integration_test;
 pub mod new;
 pub mod publish;
 pub mod run_function;
 pub mod run_view_function;
 pub mod unit_test;
-pub mod integration_test;
