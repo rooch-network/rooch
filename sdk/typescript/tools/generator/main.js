7e654b48d11b887366c4c34dfae6526ff88352a5
Owen Wu <1449069+yubing744@users.noreply.github.com>
2023-07-30 20:51:22 +0800

    Feature automatically generate Typescript client based on openRPC documentation (#545)
    * feat: add code auto gen client
    
    * feat: remove petstore-openrpc.json
    
    * feat: ok gen rooch client types
    
    * feat: auto gen client
    
    * feat: auto gen rooch typescript client ok
    
    * feat: remove deps
    
    * feat: update utils comment

---
+++ sdk/typescript/tools/generator/main.js
@@ -0,0 +1,17 @@
+import { Generator } from "./generator.js"
+
+const main = async () => {
+    const generator = new Generator({
+        openrpcDocument: "../../crates/rooch-open-rpc-spec/schemas/openrpc.json",
+        outDir: "./src/client"
+    })
+
+    try {
+        await generator.execute()
+        console.log("generate rooch typescript client ok!")
+    } catch (e) {
+        console.error("generate rooch typescript client error:", e)
+    }
+}
+
+main();
