1624b4dff9c22c6003d8a5bf5a0bb7a2b78755ac
Bai Chuan <muzixinly@gmail.com>
2023-08-10 18:41:17 +0800

    Storage reload consistance testcase (#603)
    * add statedb reload consistence check testcase
    
    * statedb apply dubuging
    
    * fixed node key Missing
    
    * replace TableCache for AccountChangeSet

--- moveos/moveos-store/src/tests/test_state_store.rs
+++ moveos/moveos-store/src/tests/test_state_store.rs
@@ -1,15 +1,122 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use move_core_types::effects::{ChangeSet, Op};
+use crate::MoveOSStore;
+use anyhow::Result;
+use move_core_types::account_address::AccountAddress;
+use move_core_types::effects::{AccountChangeSet, ChangeSet, Op};
 use moveos_types::h256::H256;
-use moveos_types::object::ObjectID;
-use moveos_types::state::{MoveState, StateChangeSet};
-use moveos_types::{move_string::MoveString, state::TableChange};
+use moveos_types::move_string::MoveString;
+use moveos_types::move_types::random_type_tag;
+use moveos_types::object::{NamedTableID, ObjectID};
+use moveos_types::state::{MoveState, State, StateChangeSet, TableChange, TableTypeInfo};
+use moveos_types::storage_context;
+use rand::{thread_rng, Rng};
 use smt::NodeStore;
 use std::str::FromStr;
 
-use crate::MoveOSStore;
+fn random_bytes() -> Vec<u8> {
+    H256::random().0.to_vec()
+}
+
+fn random_account_change_set() -> AccountChangeSet {
+    AccountChangeSet::new()
+
+    // let mut account_change_set = AccountChangeSet::new();
+
+    // let mut rng = thread_rng();
+    // // generate modules
+    // for _n in 0..rng.gen_range(1..=5) {
+    //     account_change_set
+    //         .add_module_op(random_identity(), Op::New(random_bytes()))
+    //         .expect("account_change_set add module op should succ");
+    // }
+    // // generate resources
+    // for _n in 0..rng.gen_range(1..=10) {
+    //     account_change_set
+    //         .add_resource_op(random_struct_tag(), Op::New(random_bytes()))
+    //         .expect("account_change_set add resource op should succ");
+    // }
+
+    // account_change_set
+}
+
+fn random_change_set() -> ChangeSet {
+    let mut change_set = ChangeSet::new();
+
+    let mut rng = thread_rng();
+    for _n in 0..rng.gen_range(1..=5) {
+        let addr = AccountAddress::random();
+        change_set
+            .add_account_changeset(addr, random_account_change_set())
+            .expect("change_set add account change set should succ");
+    }
+    change_set
+}
+
+fn random_table_change() -> TableChange {
+    let mut table_change = TableChange::default();
+
+    let mut rng = thread_rng();
+    for _n in 0..rng.gen_range(1..=10) {
+        table_change.entries.insert(
+            random_bytes(),
+            Op::New(State::new(random_bytes(), random_type_tag())),
+        );
+    }
+    table_change
+}
+
+fn random_state_change_set() -> StateChangeSet {
+    let mut state_change_set = StateChangeSet::default();
+
+    // generate new tables
+    let mut rng = thread_rng();
+    for _n in 0..rng.gen_range(1..=5) {
+        let handle = ObjectID::from(AccountAddress::random());
+        state_change_set
+            .new_tables
+            .insert(handle, TableTypeInfo::new(random_type_tag()));
+    }
+
+    // generate remove tables
+    for _n in 0..rng.gen_range(1..=5) {
+        let handle = ObjectID::from(AccountAddress::random());
+        state_change_set.removed_tables.insert(handle);
+    }
+
+    // generate change tables
+    for _n in 0..rng.gen_range(1..=5) {
+        let handle = ObjectID::from(AccountAddress::random());
+        state_change_set
+            .changes
+            .insert(handle, random_table_change());
+    }
+
+    // generate modules change tables
+    for _n in 0..rng.gen_range(1..=5) {
+        let handle = NamedTableID::Module(AccountAddress::random()).to_object_id();
+        state_change_set
+            .changes
+            .insert(handle, random_table_change());
+    }
+
+    // generate resources change tables
+    for _n in 0..rng.gen_range(1..=10) {
+        let handle = NamedTableID::Resource(AccountAddress::random()).to_object_id();
+        state_change_set
+            .changes
+            .insert(handle, random_table_change());
+    }
+
+    // generate global table
+    state_change_set.changes.insert(
+        storage_context::GLOBAL_OBJECT_STORAGE_HANDLE,
+        random_table_change(),
+    );
+
+    state_change_set
+}
 
 #[test]
 fn test_statedb() {
@@ -58,4 +165,36 @@ fn test_reopen() {
     {
         assert_eq!(node_store.get(&key).unwrap(), Some(node));
     }
+}
+
+#[test]
+fn test_statedb_state_root() -> Result<()> {
+    let moveos_store = MoveOSStore::mock_moveos_store().expect("moveos store mock should succ");
+    let state_root = moveos_store
+        .get_state_store()
+        .apply_change_set(random_change_set(), random_state_change_set())?;
+    let new_state_root = moveos_store
+        .get_state_store()
+        .apply_change_set(random_change_set(), random_state_change_set())?;
+    assert_ne!(state_root, new_state_root);
+    Ok(())
+}
+
+#[test]
+fn test_state_db_dump_and_apply() -> Result<()> {
+    let moveos_store = MoveOSStore::mock_moveos_store().expect("moveos store mock should succ");
+
+    let (change_set, state_change_set) = (random_change_set(), random_state_change_set());
+    let _state_root = moveos_store
+        .get_state_store()
+        .apply_change_set(change_set, state_change_set)?;
+
+    let global_state_set = moveos_store.get_state_store().dump()?;
+    let moveos_store2 = MoveOSStore::mock_moveos_store().expect("moveos store mock should succ");
+    moveos_store2
+        .get_state_store()
+        .apply(global_state_set.clone())?;
+    let global_state_set2 = moveos_store2.get_state_store().dump()?;
+    assert_eq!(global_state_set, global_state_set2);
+    Ok(())
 }
