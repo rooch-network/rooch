0319efeb1396b523dfafda0ffac98ad9259e1f57
Owen Wu <yubing744@163.com>
2023-08-21 09:21:23 +0800

    feat: remove console.log

--- sdk/typescript/test/e2e/view-function.test.ts
+++ sdk/typescript/test/e2e/view-function.test.ts
@@ -5,7 +5,6 @@ describe('viewFunction', () => {
   it('view function should be ok', async () => {
     const provider = new JsonRpcProvider()
     const result = await provider.executeViewFunction('0xbecf2f0f545f16f0c5b69786a4e08a422cec5bf94ffb9ba68e34b730423026ef::counter::value', [], [])
-    console.log("result:", result)
     expect(result).toBeDefined()
   })
 })
