e257ec27735bf91805e3e510250b151228dbda17
jolestar <jolestar@gmail.com>
2023-08-29 19:47:23 +0800

    fixup

--- crates/rooch-types/src/error.rs
+++ crates/rooch-types/src/error.rs
@@ -1,7 +1,7 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use moveos_types::{genesis_info::GenesisInfo};
+use moveos_types::genesis_info::GenesisInfo;
 use serde::{Deserialize, Serialize};
 use std::io;
 use thiserror::Error;
