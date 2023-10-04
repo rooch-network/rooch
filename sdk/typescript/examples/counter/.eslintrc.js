4430b3a0a01353389b3a1a5ab04248f191d9573e
Owen Wu <1449069+yubing744@users.noreply.github.com>
2023-10-04 15:30:26 +0800

    feat: update counter web example use @rooch/sdk (#907)
    * feat: update counter web example use @rooch/sdk
    
    * feat: update pnpm-lock.yaml
    
    * feat: fix dashboard build error

--- sdk/typescript/examples/counter/.eslintrc.js
+++ sdk/typescript/examples/counter/.eslintrc.js
@@ -10,6 +10,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-    '@typescript-eslint/no-unused-vars': 'warn'
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 }
--- sdk/typescript/examples/counter/vite.config.ts
+++ sdk/typescript/examples/counter/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+  },
   plugins: [
     react(),
     nodePolyfills({
@@ -17,6 +24,6 @@ export default defineConfig({
       },
       // Whether to polyfill `node:` protocol imports.
       protocolImports: true,
-    })
+    }),
   ],
 })
