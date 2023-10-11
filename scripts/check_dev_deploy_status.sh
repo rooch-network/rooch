367e442113a35d648551f0f3b0f2e7c014f20d19
yourmoonlight <foxkingmoonlight@gmail.com>
2023-10-11 20:06:07 +0800

    Change the code comments to English

--- scripts/check_dev_deploy_status.sh
+++ scripts/check_dev_deploy_status.sh
@@ -2,28 +2,27 @@
 # Copyright (c) RoochNetwork
 # SPDX-License-Identifier: Apache-2.0
 
-# 关键字
 KEYWORD="rooch"
 
-# 获取容器ID
+# get the container id
 CONTAINER_ID=$(docker ps -a | grep $KEYWORD | awk '{print $1}')
 
 if [ -z "$CONTAINER_ID" ]; then
-    echo "没有找到与关键字 $KEYWORD 相关的容器"
+    echo "No container found related to the keyword $KEYWORD"
     exit 1
 fi
 
-# 获取容器状态
+# get container status
 STATUS=$(docker inspect --format '{{.State.Status}}' $CONTAINER_ID)
 
 if [ "$STATUS" != "running" ]; then
-    echo "容器 $CONTAINER_ID 不在运行状态，尝试清理数据，重新启动，并部署examples..."
-    echo "开始清理数据"
+    echo "Container $CONTAINER_ID is not running，trying to clean data and restart"
+    echo "Start cleaning the data."
     rooch server clean -n dev
     docker start $CONTAINER_ID
     if [ $? -eq 0 ]; then
-        echo "容器 $CONTAINER_ID 已成功重新启动"
-        echo "重新部署examples"
+        echo "Container $CONTAINER_ID Successfully restarted."
+        echo "Redeploy the examples"
         for dir in ../examples/*/; do
             dir=${dir%*/}
             name_addr=$(basename $dir)
@@ -32,8 +31,8 @@ if [ "$STATUS" != "running" ]; then
             rooch move publish --named-addresses rooch_examples=default,$name_addr=default -p ../examples/$name_addr/
         done
     else
-        echo "容器 $CONTAINER_ID 启动失败，请查看原因"
+        echo "Container $CONTAINER_ID Startup failed, please check the reason."
     fi
 else
-    echo "容器 $CONTAINER_ID 正在运行"
+    echo "Container $CONTAINER_ID is running"
 fi
