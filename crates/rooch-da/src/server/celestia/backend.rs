e3de13b54c744ec12f042c10034d9958c0ed73ac
popcnt-subodhi <subodhi@west>
2023-12-13 02:02:19 +0800

    feat(da): refine submit segment to celestia

--- crates/rooch-da/src/server/celestia/backend.rs
+++ crates/rooch-da/src/server/celestia/backend.rs
@@ -1,30 +1,42 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use celestia_rpc::{BlobClient, Client, StateClient};
+use anyhow::Result;
+use celestia_rpc::{BlobClient, Client};
 use celestia_types::Blob;
 use celestia_types::blob::SubmitOptions;
 use celestia_types::nmt::Namespace;
 
 use crate::server::segment::Segment;
 
-struct BackendClient {
+struct Backend {
     namespace: Namespace,
-    celestia_client: Client,
+    client: Client,
 }
 
-impl BackendClient {
+impl Backend {
     async fn new(namespace: Namespace, conn_str: &str, auth_token: &str) -> Self {
-        let celestia_client = Client::new(conn_str, Option::from(auth_token)).await?;
-        Self { namespace, celestia_client }
+        let celestia_client = Client::new(conn_str, Option::from(auth_token)).await.unwrap();
+        Self { namespace, client: celestia_client }
     }
-
-    async fn submit(&self, segment: Segment) -> Result<(), String> {
+    
+    async fn submit(&self, segment: Segment) -> Result<()> {
         let data = bcs::to_bytes(&segment).unwrap();
         let blob = Blob::new(self.namespace, data).unwrap();
 
-        // TODO submit to celestia
-        self.celestia_client.blob_submit(&[blob.clone()], SubmitOptions::default()).await.unwrap();
-        self.celestia_client.state_submit_pay_for_blob()
+        // TODO backoff retry
+        return match self.client.blob_submit(&[blob.clone()], SubmitOptions::default()).await {
+            Ok(_) => Ok(()),
+            Err(e) => {
+                log::warn!(
+                    "failed to submit segment to celestia node, chunk: {}, segment: {}, commitment: {:?}, error:{:?}",
+                    segment.chunk_id,
+                    segment.segment_id,
+                    blob.commitment,
+                    e,
+                );
+                Err(e.into())
+            }
+        };
     }
 }
