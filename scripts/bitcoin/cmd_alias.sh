18dece689b5db3550f1c0f0d094382367ce3cd48
jolestar <jolestar@gmail.com>
2023-12-14 22:26:56 +0800

    fixup

--- scripts/bitcoin/cmd_alias.sh
+++ scripts/bitcoin/cmd_alias.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# Copyright (c) RoochNetwork
+# SPDX-License-Identifier: Apache-2.0
+
 alias ord="ord --regtest --rpc-url http://127.0.0.1:18443 --bitcoin-rpc-user roochuser --bitcoin-rpc-pass roochpass"
 alias bitcoin-cli="docker exec -it bitcoind bitcoin-cli -regtest"
