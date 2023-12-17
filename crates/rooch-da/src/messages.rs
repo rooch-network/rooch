a6f14fd232efe11b7f018a6e597fdb6afd501466
popcnt-subodhi <subodhi@west>
2023-12-18 07:23:06 +0800

    feat(da): split msg for DA and DA server

--- crates/rooch-da/src/messages.rs
+++ crates/rooch-da/src/messages.rs
@@ -1,27 +1,41 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use anyhow::Result;
 use coerce::actor::message::Message;
 use serde::{Deserialize, Serialize};
 
 use moveos_types::h256::H256;
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct PutBatchMessage {
-    pub batch_meta: BatchMeta,
-    pub batch: Vec<u8>,
-
-    // TODO add put policy
-}
-
 pub struct BatchMeta {
     // each batch maps to a L2 block
     pub block_number: u128,
     // sha3_256 hash of the batch data
     pub batch_hash: H256,
+    // TODO use sequencer keypair to sign
     // signature result of BatchMeta
     pub signature: Vec<u8>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Batch {
+    pub meta: BatchMeta,
+    pub data: Vec<u8>,
+}
+
+impl Message for Batch {
+    type Result = anyhow::Result<()>;
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PutBatchMessage {
+    pub batch: Batch,
+
+    // TODO add put policy
+}
+
+impl Message for PutBatchMessage {
+    type Result = anyhow::Result<PutBatchResult>;
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -32,8 +46,4 @@ pub struct PutBatchResult {
     pub checksum: Vec<u8>,
     // signature result of PutBatchResult
     pub signature: Vec<u8>,
-}
-
-impl Message for PutBatchMessage {
-    type Result = Result<PutBatchResult>;
 }
