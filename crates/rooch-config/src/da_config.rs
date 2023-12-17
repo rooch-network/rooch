001379d81e64970196789f11c8f649b895f9a878
popcnt-subodhi <subodhi@west>
2023-12-17 20:39:45 +0800

    feat(da): fix da_config

--- crates/rooch-config/src/da_config.rs
+++ crates/rooch-config/src/da_config.rs
@@ -1,6 +1,8 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
+use std::str::FromStr;
+
 use clap::Parser;
 use serde::{Deserialize, Serialize};
 
@@ -13,7 +15,7 @@ pub enum DAServerType {
 #[derive(Clone, Default, Debug, Deserialize, PartialEq, Serialize, Parser)]
 #[serde(deny_unknown_fields)]
 pub struct DAConfig {
-    #[serde(skip_serializing_if = "ServerType::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     #[clap(name = "da-server", long, help = "specifies the type of DA server to be used. 'celestia' with corresponding Celestia server configuration, 'xxx' with corresponding xxx server configuration, etc.")]
     pub da_server: Option<DAServerType>,
 }
@@ -21,6 +23,8 @@ pub struct DAConfig {
 #[derive(Clone, Default, Debug, Deserialize, PartialEq, Serialize, Parser)]
 #[serde(deny_unknown_fields)]
 pub struct DAServerCelestiaConfig {
+    // TODO add from env
+    // TODO add segment size limit (2MB at start)   
     #[serde(skip_serializing_if = "Option::is_none")]
     #[clap(name = "celestia-namespace", long, help = "DA backend celestia namespace")]
     pub namespace: Option<String>,
@@ -30,4 +34,13 @@ pub struct DAServerCelestiaConfig {
     #[serde(skip_serializing_if = "Option::is_none")]
     #[clap(name = "celestia-auth-token", long, help = "DA backend celestia node auth token")]
     pub auth_token: Option<String>,
+}
+
+impl FromStr for DAServerType {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let deserialized = serde_json::from_str(s)?;
+        Ok(deserialized)
+    }
 }
