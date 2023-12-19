6d1193c4acb2b344f7dd65d5016efdfa15313074
popcnt-subodhi <subodhi@west>
2023-12-19 12:53:58 +0800

    fix license

--- crates/rooch-da/src/server/celestia/mod.rs
+++ crates/rooch-da/src/server/celestia/mod.rs
@@ -1,3 +1,6 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 pub mod actor;
 mod backend;
 pub mod proxy;
--- crates/rooch-da/src/server/chunk.rs
+++ crates/rooch-da/src/server/chunk.rs
@@ -1,3 +1,6 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 use super::segment::Segment;
 
 pub struct Chunker {
