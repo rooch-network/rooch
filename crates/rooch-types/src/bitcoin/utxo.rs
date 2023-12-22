dc9fb09b4823272c9b56f98e6faf6bf5230f8b05
jolestar <jolestar@gmail.com>
2023-12-22 09:53:08 +0800

    fixup

--- crates/rooch-types/src/bitcoin/utxo.rs
+++ crates/rooch-types/src/bitcoin/utxo.rs
@@ -117,10 +117,10 @@ impl<'a> ModuleBinding<'a> for UTXOModule<'a> {
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
     use super::*;
     use move_core_types::account_address::AccountAddress;
     use moveos_types::moveos_std::object;
+    use std::str::FromStr;
 
     #[test]
     fn test_id() {
