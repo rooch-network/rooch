52a176d67ab9759f1a4772bd5f579e6b9e98b867
stevenlaw123 <141568913+stevenlaw123@users.noreply.github.com>
2024-01-08 20:00:51 +0800

    Counting gas in several types (#1285)
    * counting gas in several types
    
    * output gas statement when the tx execution is finished
    
    * charge the change_set and io_write
    
    * add the gas statement info in the tx output
    
    * cargo fmt
    
    * get transaction size from tx_context
    
    * increase the maxGasAmount
    
    ---------
    
    Co-authored-by: stevenlaw123 <stevenlaw123@gmail.com>

--- sdk/typescript/rooch-sdk/test/e2e/sdk.test.ts
+++ sdk/typescript/rooch-sdk/test/e2e/sdk.test.ts
@@ -92,7 +92,7 @@ describe('SDK', () => {
           },
         ],
         {
-          maxGasAmount: 1000000,
+          maxGasAmount: 2000000,
         },
       )
 
@@ -121,7 +121,7 @@ describe('SDK', () => {
           },
         ],
         {
-          maxGasAmount: 1000000,
+          maxGasAmount: 2000000,
         },
       )
 
@@ -153,7 +153,7 @@ describe('SDK', () => {
           },
         ],
         {
-          maxGasAmount: 1000000,
+          maxGasAmount: 2000000,
         },
       )
 
@@ -216,7 +216,7 @@ describe('SDK', () => {
 
       // run function with sessoin key
       const tx = await sessionAccount.runFunction('0x3::empty::empty', [], [], {
-        maxGasAmount: 100000000,
+        maxGasAmount: 200000000,
       })
 
       expect(tx).toBeDefined()
@@ -250,7 +250,7 @@ describe('SDK', () => {
 
       // run function with sessoin key
       const tx = await sessionAccount.runFunction('0x3::empty::empty', [], [], {
-        maxGasAmount: 100000000,
+        maxGasAmount: 200000000,
       })
 
       expect(tx).toBeDefined()
@@ -335,7 +335,7 @@ describe('SDK', () => {
 
       // run function with sessoin key
       const tx = await sessionAccount.runFunction('0x3::empty::empty', [], [], {
-        maxGasAmount: 100000000,
+        maxGasAmount: 200000000,
       })
 
       expect(tx).toBeDefined()
@@ -360,7 +360,7 @@ describe('SDK', () => {
 
       // run function with sessoin key
       const tx = await sessionAccount.runFunction('0x3::empty::empty', [], [], {
-        maxGasAmount: 100000000,
+        maxGasAmount: 200000000,
       })
 
       expect(tx).toBeDefined()
