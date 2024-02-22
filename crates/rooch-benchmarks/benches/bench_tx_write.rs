ac1a826a5b7dbd69173c3273e79eb1e405027c1b
popcnt-subodhi <subodhi@west>
2024-02-22 17:05:23 +0800

    ci: move build tx out of execution in bench
    huge improvement(It's actually closer to the real server-side execution overhead） , the major cost is from tokio wake
    
    ```
    RUST_LOG=error cargo bench --bench bench_tx_write
       Compiling rooch-benchmarks v0.1.0 (/Users/templex/rooch/bodhi/rooch/crates/rooch-benchmarks)
        Finished bench [optimized + debuginfo] target(s) in 12.05s
         Running benches/bench_tx_write.rs (/Users/templex/rooch/bodhi/rooch/target/release/deps/bench_tx_write-624c3ac78421ba7f)
    execute_tx              time:   [388.70 µs 389.91 µs 391.12 µs]
                            change: [-97.069% -97.059% -97.049%] (p = 0.00 < 0.05)
                            Performance has improved.
    ```

--- crates/rooch-benchmarks/benches/bench_tx_write.rs
+++ crates/rooch-benchmarks/benches/bench_tx_write.rs
@@ -3,10 +3,11 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 use moveos_config::temp_dir;
-use rooch_benchmarks::tx::{create_publish_transaction, create_transaction, setup_service};
+use rooch_benchmarks::tx::{create_transaction, setup_service};
 use rooch_key::keystore::account_keystore::AccountKeystore;
 use rooch_key::keystore::memory_keystore::InMemKeystore;
 use rooch_test_transaction_builder::TestTransactionBuilder;
+use std::fs::File;
 use std::time::Duration;
 use tokio::runtime::Runtime;
 
@@ -20,21 +21,34 @@ pub fn transaction_write_benchmark(c: &mut Criterion) {
 
     let default_account = keystore.addresses()[0];
     let mut test_transaction_builder = TestTransactionBuilder::new(default_account.into());
-    let tx = create_publish_transaction(&test_transaction_builder, &keystore).unwrap();
-    let _publish_result = rt.block_on(async { rpc_service.execute_tx(tx).await.unwrap() });
 
-    let mut transactions = (1..500)
-        .cycle()
-        .map(|n| create_transaction(&mut test_transaction_builder, &keystore, n).unwrap());
+    let transactions: Vec<_> = (0..10000)
+        .map(|n| create_transaction(&mut test_transaction_builder, &keystore, n).unwrap())
+        .collect();
+    let mut transactions_iter = transactions.into_iter().cycle();
+
+    let guard = pprof::ProfilerGuardBuilder::default()
+        .frequency(1000)
+        .blocklist(&["libc", "libgcc", "pthread", "vdso"])
+        .build()
+        .unwrap();
+
     c.bench_function("execute_tx", |b| {
-        b.to_async(Runtime::new().unwrap())
-            .iter(|| rpc_service.execute_tx(transactions.next().unwrap()))
+        b.to_async(Runtime::new().unwrap()).iter(|| {
+            let tx = transactions_iter.next().unwrap();
+            rpc_service.execute_tx(tx)
+        });
     });
+
+    if let Ok(report) = guard.report().build() {
+        let file = File::create("flamegraph.svg").unwrap();
+        report.flamegraph(file).unwrap();
+    };
 }
 
 criterion_group! {
     name = rooch_tx_write_bench;
-    config = Criterion::default().sample_size(200).measurement_time(Duration::from_secs(100));
+    config = Criterion::default().warm_up_time(Duration::from_millis(100)).sample_size(10).measurement_time(Duration::from_secs(3));
     targets = transaction_write_benchmark
 }
 
--- crates/rooch-benchmarks/src/tx.rs
+++ crates/rooch-benchmarks/src/tx.rs
@@ -1,3 +1,6 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 use anyhow::Result;
 use coerce::actor::scheduler::timer::Timer;
 use coerce::actor::system::ActorSystem;
