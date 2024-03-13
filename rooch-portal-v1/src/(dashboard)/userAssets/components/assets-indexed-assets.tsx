35e8257ed5453ad5aef2fda05e62e2321bc9d872
TwilightLogic <twilightlogic@gmail.com>
2024-03-13 16:56:06 +0800

    feat: complete the `indexed assets` page

--- rooch-portal-v1/src/(dashboard)/userAssets/components/assets-indexed-assets.tsx
+++ rooch-portal-v1/src/(dashboard)/userAssets/components/assets-indexed-assets.tsx
@@ -6,16 +6,16 @@ export const AssetsIndexedAssets = () => {
     <div className="h-full flex-1 flex-col space-y-6 flex rounded-lg">
       {/* BTC @Bitcoin */}
       <div>
-        <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-2xl font-bold tracking-tight text-primary/80">BTC @Bitcoin</h1>
+        <div className="flex items-center justify-between space-y-2 mb-2">
+          <h1 className="text-2xl font-semibold tracking-tight text-primary">BTC @Bitcoin</h1>
         </div>
         <IndexedAssetsBTC />
       </div>
 
       {/* Ordi @Bitcoin */}
       <div>
-        <div className="flex items-center justify-between space-y-2">
-          <h1 className="text-2xl font-bold tracking-tight text-primary/80">Ordi @Bitcoin</h1>
+        <div className="flex items-center justify-between space-y-2 mb-2">
+          <h1 className="text-2xl font-semibold tracking-tight text-primary">Ordi @Bitcoin</h1>
         </div>
         <IndexedAssetsOrdi />
       </div>
--- rooch-portal-v1/src/(dashboard)/userAssets/components/indexed-assets-btc.tsx
+++ rooch-portal-v1/src/(dashboard)/userAssets/components/indexed-assets-btc.tsx
@@ -1,3 +1,20 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+
 export const IndexedAssetsBTC = () => {
-  return <div>BTC</div>
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <Card className="rounded-lg border border-border/40 dark:bg-zinc-800/90 dark:hover:border-primary/20 hover:shadow-md overflow-hidden">
+        <CardHeader className="flex items-center justify-center">
+          <h3 className="text-2xl">UTXO #123</h3>
+        </CardHeader>
+        <CardContent className="flex items-center justify-center">Amount 1,234</CardContent>
+      </Card>
+      <Card className="rounded-lg border border-border/40 dark:bg-zinc-800/90 dark:hover:border-primary/20 hover:shadow-md overflow-hidden">
+        <CardHeader className="flex items-center justify-center">
+          <h3 className="text-2xl">UTXO #123</h3>
+        </CardHeader>
+        <CardContent className="flex items-center justify-center">Amount 1,234</CardContent>
+      </Card>
+    </div>
+  )
 }
