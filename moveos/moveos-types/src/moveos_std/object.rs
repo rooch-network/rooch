11485ef4ce15de879b52061dfff8392e36851381
jolestar <jolestar@gmail.com>
2024-03-14 08:19:26 +0800

    fixup

--- moveos/moveos-types/src/moveos_std/object.rs
+++ moveos/moveos-types/src/moveos_std/object.rs
@@ -794,7 +794,7 @@ mod tests {
         let object_id = ObjectID::from(address);
         let object_id2 = ObjectID::from_str(&object_id.to_string()).unwrap();
         assert_eq!(object_id, object_id2);
-    } 
+    }
 
     #[test]
     fn test_resource_and_module_object_id() {
