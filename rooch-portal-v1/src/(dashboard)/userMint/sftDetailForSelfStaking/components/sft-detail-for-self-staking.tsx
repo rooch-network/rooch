1c0d6ef5a93dfa477bff35a6867530d4d4035c2d
TwilightLogic <twilightlogic@gmail.com>
2024-03-22 14:03:13 +0800

    chore: remove the unused packages

--- rooch-portal-v1/src/(dashboard)/userMint/sftDetailForSelfStaking/components/sft-detail-for-self-staking.tsx
+++ rooch-portal-v1/src/(dashboard)/userMint/sftDetailForSelfStaking/components/sft-detail-for-self-staking.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Progress } from '@/components/ui/progress'
 import { useEffect, useState } from 'react'
 import { SelfStakingCard } from './self-staking-card'
