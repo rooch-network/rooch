69a83922649c53d3f6a875d92fa63436d143195b
popcnt-subodhi <ism3y@proton.me>
2024-03-28 05:29:14 +0800

    ci(bench): refine tx_exec bench and add more bench
    1. bench each step inside execute: validate, sequence, execute
    2. no more RPC async call, using sync bench, avoiding async framework noise
    3. refine codes

--- crates/rooch-benchmarks/src/helper.rs
+++ crates/rooch-benchmarks/src/helper.rs
@@ -2,10 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::helper::PProfOut::{Flamegraph, Protobuf};
+use criterion::Criterion;
 use lazy_static::lazy_static;
+use pprof::criterion::{Output, PProfProfiler};
 use std::env;
+use std::ffi::c_int;
 use std::fmt::Display;
 use std::str::FromStr;
+use std::time::Duration;
 
 #[derive(PartialEq, Eq)]
 pub enum PProfOut {
@@ -36,8 +40,38 @@ impl Display for PProfOut {
 lazy_static! {
     pub static ref PPROF_OUT: PProfOut = {
         let pprof_out_str = env::var("PPROF_OUT").unwrap_or_else(|_| String::from("flamegraph"));
-        pprof_out_str
-            .parse::<PProfOut>()
-            .unwrap_or(PProfOut::Flamegraph)
+        pprof_out_str.parse::<PProfOut>().unwrap_or(Flamegraph)
     };
+}
+
+pub struct ProfileConfig {
+    pub sample_size: usize,
+    pub warm_up_time: Duration,
+    pub frequency: c_int,
+    pub measurement_time: Duration,
+}
+
+impl Default for ProfileConfig {
+    fn default() -> Self {
+        Self {
+            sample_size: 10,
+            warm_up_time: Duration::from_millis(10),
+            frequency: 2000,
+            measurement_time: Duration::from_millis(500),
+        }
+    }
+}
+
+pub fn profiled(config: Option<ProfileConfig>) -> Criterion {
+    let cfg = config.unwrap_or_default();
+
+    let profiler = match *PPROF_OUT {
+        Protobuf => PProfProfiler::new(cfg.frequency, Output::Protobuf),
+        Flamegraph => PProfProfiler::new(cfg.frequency, Output::Flamegraph(None)),
+    };
+    Criterion::default()
+        .with_profiler(profiler)
+        .warm_up_time(cfg.warm_up_time) // no need to warm this heavy operation
+        .sample_size(cfg.sample_size)
+        .measurement_time(cfg.measurement_time)
 }
--- crates/rooch-framework-tests/src/binding_test.rs
+++ crates/rooch-framework-tests/src/binding_test.rs
@@ -2,14 +2,17 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::{bail, Result};
+use lazy_static::lazy_static;
 use move_core_types::account_address::AccountAddress;
 use move_core_types::vm_status::KeptVMStatus;
+use moveos_config::DataDirPath;
 use moveos_store::MoveOSStore;
 use moveos_types::function_return_value::FunctionResult;
 use moveos_types::module_binding::MoveFunctionCaller;
 use moveos_types::moveos_std::object::ObjectEntity;
 use moveos_types::moveos_std::tx_context::TxContext;
-use moveos_types::transaction::FunctionCall;
+use moveos_types::transaction::{FunctionCall, VerifiedMoveOSTransaction};
+use rooch_config::store_config::StoreConfig;
 use rooch_executor::actor::reader_executor::ReaderExecutorActor;
 use rooch_executor::actor::{executor::ExecutorActor, messages::ExecuteTransactionResult};
 use rooch_framework::natives::default_gas_schedule;
@@ -17,6 +20,24 @@ use rooch_store::RoochStore;
 use rooch_types::bitcoin::genesis::BitcoinGenesisContext;
 use rooch_types::bitcoin::network::Network;
 use rooch_types::{chain_id::RoochChainID, transaction::AbstractTransaction};
+use std::env;
+use std::sync::Arc;
+use tempfile::TempDir;
+
+lazy_static! {
+    pub static ref DATA_DIR: DataDirPath = get_data_dir();
+}
+
+pub fn get_data_dir() -> DataDirPath {
+    match env::var("ROOCH_TEST_DATA_DIR") {
+        Ok(path_str) => {
+            let temp_dir = TempDir::new_in(path_str)
+                .expect("failed to create temp dir in provided data dir path");
+            DataDirPath::TempPath(Arc::from(temp_dir))
+        }
+        Err(_) => moveos_config::temp_dir(),
+    }
+}
 
 pub struct RustBindingTest {
     pub executor: ExecutorActor,
@@ -25,8 +46,19 @@ pub struct RustBindingTest {
 
 impl RustBindingTest {
     pub fn new() -> Result<Self> {
-        let moveos_store = MoveOSStore::mock_moveos_store()?;
-        let rooch_store = RoochStore::mock_rooch_store()?;
+        let (rooch_db_path, moveos_db_path) = (
+            StoreConfig::get_mock_moveos_store_dir(&DATA_DIR),
+            StoreConfig::get_mock_rooch_store_dir(&DATA_DIR),
+        );
+        if !rooch_db_path.exists() {
+            std::fs::create_dir_all(rooch_db_path.clone())?;
+        }
+        if !moveos_db_path.exists() {
+            std::fs::create_dir_all(moveos_db_path.clone())?;
+        }
+
+        let moveos_store = MoveOSStore::mock_moveos_store_with_data_dir(moveos_db_path.as_path())?;
+        let rooch_store = RoochStore::mock_rooch_store(rooch_db_path.as_path())?;
         let sequencer = AccountAddress::ONE.into();
         let gas_schedule_blob = bcs::to_bytes(&default_gas_schedule())
             .expect("Failure serializing genesis gas schedule");
@@ -70,6 +102,30 @@ impl RustBindingTest {
     ) -> Result<ExecuteTransactionResult> {
         let verified_tx = self.executor.validate(tx)?;
         let result = self.executor.execute(verified_tx)?;
+        let root = ObjectEntity::root_object(
+            result.transaction_info.state_root,
+            result.transaction_info.size,
+        );
+        self.reader_executor.refresh_state(root, false)?;
+        Ok(result)
+    }
+
+    pub fn execute_verified_tx(&mut self, tx: VerifiedMoveOSTransaction) -> Result<()> {
+        let execute_result = self.execute_verified_tx_as_result(tx)?;
+        if execute_result.transaction_info.status != KeptVMStatus::Executed {
+            bail!(
+                "tx should success, error: {:?}",
+                execute_result.transaction_info.status
+            );
+        }
+        Ok(())
+    }
+
+    pub fn execute_verified_tx_as_result(
+        &mut self,
+        tx: VerifiedMoveOSTransaction,
+    ) -> Result<ExecuteTransactionResult> {
+        let result = self.executor.execute(tx)?;
         let root = ObjectEntity::root_object(
             result.transaction_info.state_root,
             result.transaction_info.size,
