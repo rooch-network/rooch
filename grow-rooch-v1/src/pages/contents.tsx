7e70b1e458237249994e2cef00b8360cb9217120
TwilightLogic <twilightlogic@gmail.com>
2024-03-27 14:47:52 +0800

    feat: update the opacity of `Contents`

--- grow-rooch-v1/src/pages/contents.tsx
+++ grow-rooch-v1/src/pages/contents.tsx
@@ -37,7 +37,7 @@ export const Contents = () => {
             aria-hidden="true"
           >
             <div
-              className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#a8c0ff] to-[#3f2b96] opacity-35"
+              className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#a8c0ff] to-[#3f2b96] opacity-30"
               style={{
                 clipPath:
                   'polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)',
