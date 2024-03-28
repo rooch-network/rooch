66822362b4d029068689e0198718f5f485d75b1e
TwilightLogic <twilightlogic@gmail.com>
2024-03-28 11:15:00 +0800

    feat: add spinner animation on connecting to the wallet

--- rooch-portal-v1/src/(dashboard)/components/wallet-connect.tsx
+++ rooch-portal-v1/src/(dashboard)/components/wallet-connect.tsx
@@ -14,7 +14,7 @@ import { Button } from '@/components/ui/button'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 import { formatAddress } from '@/utils/format'
-import { capitalizeFirstLetter } from '@/lib/utils'
+import { capitalizeFirstLetter, cn } from '@/lib/utils'
 import { walletsMaterialMap } from '../common/constant'
 
 import {
@@ -26,6 +26,7 @@ import {
 } from '@roochnetwork/rooch-sdk-kit'
 
 export const WalletConnect = () => {
+  const [isLoading, setIsLoading] = useState(false)
   const [isDialogOpen, setIsDialogOpen] = useState(false)
   const { mutateAsync: connectWallet } = useConnectWallet()
   const account = useWalletStore((state) => state.currentAccount)
@@ -40,6 +41,7 @@ export const WalletConnect = () => {
   // ** Connect specific wallet
   const handleConnectSpecificWallet = async (wallet: BaseWallet) => {
     try {
+      setIsLoading(true)
       await connectWallet({ wallet: wallet })
 
       setCurrentWallet(wallet)
@@ -48,6 +50,8 @@ export const WalletConnect = () => {
       toast.success('Connected to the wallet')
     } catch (error) {
       toast.error('Connection failed.')
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -69,7 +73,35 @@ export const WalletConnect = () => {
 
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <DialogTrigger asChild />
-        <DialogContent className="sm:max-w-[425px]">
+        <DialogContent
+          className={cn(
+            'sm:max-w-[425px]',
+            isLoading ? 'border-gray-500 dark:border-gray-600' : '',
+          )}
+        >
+          {isLoading && (
+            <div className="absolute inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-10">
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-900"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+                <span className="sr-only">Loading...</span>
+              </div>
+            </div>
+          )}
           <DialogHeader>
             <DialogTitle className="text-2xl text-center">Connect Wallet</DialogTitle>
           </DialogHeader>
