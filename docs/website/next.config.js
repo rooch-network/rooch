543603d230216b7932dbf8b5176d836fed94e580
Joe Chen <geometryolife@gmail.com>
2024-03-28 09:54:08 +0800

    complete learn partial redirect

--- docs/website/next.config.js
+++ docs/website/next.config.js
@@ -40,6 +40,26 @@ module.exports = withNextra({
         destination: "/learn/core-concepts/objects/object",
       },
       {
+        source: "/docs/developer-guides/installation",
+        permanent: false,
+        destination: "/build/getting-started/installation",
+      },
+      {
+        source: "/docs/developer-guides/run-local-testnet",
+        permanent: false,
+        destination: "/build/getting-started/connect-to-rooch/run-local-testnet",
+      },
+      {
+        source: "/docs/developer-guides/connect-devnet",
+        permanent: false,
+        destination: "/build/getting-started/connect-to-rooch/connect-devnet",
+      },
+      {
+        source: "/docs/developer-guides/connect-testnet",
+        permanent: false,
+        destination: "/build/getting-started/connect-to-rooch/connect-testnet",
+      },
+      {
         source: "/docs/developer-guides/quick-start",
         permanent: false,
         destination: "/build/getting-started/first-contract/quick-start",
@@ -58,6 +78,46 @@ module.exports = withNextra({
         source: "/docs/getting-started",
         permanent: false,
         destination: "/build/getting-started/first-blog-system",
+      },
+      {
+        source: "/developer-guides/hash",
+        permanent: false,
+        destination: "/build/rooch-framework/cryptographic-primitives/hash",
+      },
+      {
+        source: "/docs/developer-guides/timestamp",
+        permanent: false,
+        destination: "/build/rooch-framework/timestamp",
+      },
+      {
+        source: "/docs/developer-guides/private-generics",
+        permanent: false,
+        destination: "/build/rooch-framework/private-generics",
+      },
+      {
+        source: "/docs/developer-guides/unit-test",
+        permanent: false,
+        destination: "/build/rooch-framework/unit-test",
+      },
+      {
+        source: "/docs/example-guides",
+        permanent: false,
+        destination: "/build/example-guides",
+      },
+      {
+        source: "/docs/developer-guides/cli",
+        permanent: false,
+        destination: "/build/reference/rooch-cli",
+      },
+      {
+        source: "/docs/developer-guides/typescript-sdk",
+        permanent: false,
+        destination: "/build/reference/sdk/typescript-sdk",
+      },
+      {
+        source: "/docs/developer-guides/access-path",
+        permanent: false,
+        destination: "/build/reference/rpc/access-path",
       },
     ];
   },
