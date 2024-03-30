f5db2e2a3cd76f2e324fc3303ed0bda3df01e041
TwilightLogic <twilightlogic@gmail.com>
2024-03-30 23:21:20 +0800

    feat: update the `skeleton-list.tsx`

--- rooch-portal-v1/src/components/skeleton-list.tsx
+++ rooch-portal-v1/src/components/skeleton-list.tsx
@@ -53,6 +53,13 @@ export const SkeletonList = () => {
         </div>
         <div className="h-2.5 bg-zinc-300 rounded-full dark:bg-zinc-700 w-12"></div>
       </div>
+      <div className="flex items-center justify-between pt-4">
+        <div>
+          <div className="h-2.5 bg-zinc-300 rounded-full dark:bg-zinc-600 w-24 mb-2.5"></div>
+          <div className="w-32 h-2 bg-zinc-200 rounded-full dark:bg-zinc-700"></div>
+        </div>
+        <div className="h-2.5 bg-zinc-300 rounded-full dark:bg-zinc-700 w-12"></div>
+      </div>
       <span className="sr-only">Loading...</span>
     </div>
   )
