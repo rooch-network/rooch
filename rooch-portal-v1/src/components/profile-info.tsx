b5170a2be4b02f9fc14084a88347a7870216f652
TwilightLogic <twilightlogic@gmail.com>
2024-04-03 22:45:50 +0800

    feat: add jazzicon

--- rooch-portal-v1/src/components/profile-info.tsx
+++ rooch-portal-v1/src/components/profile-info.tsx
@@ -13,7 +13,9 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Avatar } from '@/components/ui/avatar'
+
+import Jazzicon, { jsNumberForAddress } from 'react-jazzicon'
 
 import { formatAddress } from '@/utils/format'
 import { useWalletStore } from '@roochnetwork/rooch-sdk-kit'
@@ -30,10 +32,8 @@ export const ProfileInfo = () => {
           className="h-12 w-full cursor-pointer flex items-center justify-start transition-all ease-in-out"
         >
           <div className="flex items-center justify-start gap-x-3">
-            <Avatar>
-              {/* TODO: add jazzicons as user profile */}
-              <AvatarImage src="https://github.com/shadcn.png" alt="Rooch Network" />
-              <AvatarFallback>RH</AvatarFallback>
+            <Avatar className="">
+              <Jazzicon diameter={55} seed={jsNumberForAddress(account?.address as string)} />
             </Avatar>
             <div className="h-full w-full flex flex-col items-start justify-center">
               <h3 className="text-base font-semibold text-zinc-500 dark:text-zinc-300">
