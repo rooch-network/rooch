1aaff252e079c065cb9c438b254074a890de9f7f
popcnt-subodhi <ism3y@proton.me>
2024-04-08 02:18:52 +0800

    fix copyright

--- crates/rooch-benchmarks/benches/bench_tx_exec_blk.rs
+++ crates/rooch-benchmarks/benches/bench_tx_exec_blk.rs
@@ -1,12 +1,15 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 use criterion::{criterion_group, criterion_main};
 
 use rooch_benchmarks::helper::profiled;
 use rooch_benchmarks::tx::tx_exec_benchmark;
 
 criterion_group! {
-    name = tx_blk_exec_bench;
+    name = tx_exec_blk_bench;
     config = profiled(None).measurement_time(std::time::Duration::from_secs(20));
     targets = tx_exec_benchmark
 }
 
-criterion_main!(tx_blk_exec_bench);
+criterion_main!(tx_exec_blk_bench);
