0af69c38bab8c7f6aa412486142c888898d78b63
TwilightLogic <twilightlogic@gmail.com>
2024-04-08 17:05:04 +0800

    refactor: set ethereum status as comming soon on Settings page

--- rooch-portal-v1/src/pages/settings/components/connected-account.tsx
+++ rooch-portal-v1/src/pages/settings/components/connected-account.tsx
@@ -8,7 +8,8 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import { CircleDot, CircleDotDashed, Copy, RefreshCcwDot } from 'lucide-react'
+import { CircleDot, CircleDotDashed, Copy } from 'lucide-react'
+import { formatAddress } from '../../../utils/format'
 
 const networks = [
   {
@@ -27,86 +28,61 @@ export const ConnectedAccount = () => {
   return (
     <div className="rounded-lg border w-full">
       <Table>
-        <TableCaption className="text-left pl-2 mb-2">
-          Switch between networks with ease.
-        </TableCaption>
+        <TableCaption className="text-left pl-2 mb-2">Network Status</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[100px]">Networks</TableHead>
             <TableHead>Address</TableHead>
             <TableHead className="text-center">Status</TableHead>
-            <TableHead className="text-center">Action</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {networks.map((network) => (
             <TableRow key={network.network}>
               <TableCell className="font-medium">{network.network}</TableCell>
-              {/* 完整地址仅在较大屏幕上显示 */}
-              <TableCell className="hidden md:table-cell">
-                <span className="flex items-center justify-start gap-0.5 text-muted-foreground">
-                  {network.address}
-                  <Button variant="ghost" size="icon" className=" w-6 h-6">
-                    <Copy className="w-3 h-3" />
-                  </Button>
-                </span>
-              </TableCell>
 
-              {/* 缩短的地址仅在移动设备上显示 */}
-              <TableCell className="md:hidden">
-                <span className="flex items-center justify-start gap-0.5 text-muted-foreground">
-                  {`${network.address.substring(0, 3)}...${network.address.substring(
-                    network.address.length - 3,
-                  )}`}
-                  <Button variant="ghost" size="icon" className=" w-6 h-6">
-                    <Copy className="w-3 h-3" />
-                  </Button>
-                </span>
-              </TableCell>
-              <TableCell>
-                {network.status ? (
-                  <span className="text-green-500 dark:text-green-400 flex items-center justify-center">
-                    <CircleDot className="w-5 h-5 pr-1" /> active
-                  </span>
-                ) : (
-                  <span className="text-zinc-500 dark:text-zinc-400 flex items-center justify-center">
-                    <CircleDotDashed className="w-5 h-5 pr-1" />
-                    inactive
-                  </span>
-                )}
-              </TableCell>
-              <TableCell className="text-center hidden md:table-cell">
-                {network.status ? (
-                  <Button
-                    className="text-green-500 dark:text-green-400"
-                    variant="link"
-                    size="sm"
-                    disabled
-                  >
-                    Current
-                  </Button>
-                ) : (
-                  <Button variant="default" size="sm">
-                    Switch
-                  </Button>
-                )}
-              </TableCell>
-              <TableCell className="text-center md:hidden">
-                {network.status ? (
-                  <Button
-                    className="text-green-500 dark:text-green-400"
-                    variant="ghost"
-                    size="icon"
-                    disabled
-                  >
-                    <RefreshCcwDot className="w-5 h-5" />
-                  </Button>
-                ) : (
-                  <Button variant="ghost" size="icon">
-                    <RefreshCcwDot className="w-5 h-5" />
-                  </Button>
-                )}
-              </TableCell>
+              {network.network === 'Ethereum' ? (
+                <>
+                  <TableCell>
+                    <span className="flex items-center justify-start gap-0.5 text-muted-foreground">
+                      Coming soon ⌛️
+                    </span>
+                  </TableCell>
+                  <TableCell></TableCell>
+                </>
+              ) : (
+                <>
+                  <TableCell className="hidden md:table-cell">
+                    <span className="flex items-center justify-start gap-0.5 text-muted-foreground">
+                      {network.address}
+                      <Button variant="ghost" size="icon" className=" w-6 h-6">
+                        <Copy className="w-3 h-3" />
+                      </Button>
+                    </span>
+                  </TableCell>
+                  <TableCell className="md:hidden">
+                    <span className="flex items-center justify-start gap-0.5 text-muted-foreground">
+                      {formatAddress(network.address)}
+                      <Button variant="ghost" size="icon" className=" w-6 h-6">
+                        <Copy className="w-3 h-3" />
+                      </Button>
+                    </span>
+                  </TableCell>
+
+                  <TableCell>
+                    {network.status ? (
+                      <span className="text-green-500 dark:text-green-400 flex items-center justify-center">
+                        <CircleDot className="w-5 h-5 pr-1" /> active
+                      </span>
+                    ) : (
+                      <span className="text-zinc-500 dark:text-zinc-400 flex items-center justify-center">
+                        <CircleDotDashed className="w-5 h-5 pr-1" />
+                        inactive
+                      </span>
+                    )}
+                  </TableCell>
+                </>
+              )}
             </TableRow>
           ))}
         </TableBody>
