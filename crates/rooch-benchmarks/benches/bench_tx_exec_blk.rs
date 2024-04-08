b49d0697d617ff71b6b0b79fe9752d5aefaf6a46
popcnt <142196625+popcnt-subodhi@users.noreply.github.com>
2024-04-09 02:44:19 +0800

    ci(bench): add exec l1 block bench (#1535)
    * ci(bench): add l1 block tx bench
    
    1. add l1 block tx bench
    2. add data import mode for bindingtest

--- crates/rooch-benchmarks/benches/bench_tx_exec_blk.rs
+++ crates/rooch-benchmarks/benches/bench_tx_exec_blk.rs
@@ -2,13 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use criterion::{criterion_group, criterion_main};
+
 use rooch_benchmarks::helper::profiled;
 use rooch_benchmarks::tx::tx_exec_benchmark;
 
 criterion_group! {
-    name = tx_exec_bench;
-    config = profiled(None);
+    name = tx_exec_blk_bench;
+    config = profiled(None).measurement_time(std::time::Duration::from_secs(2));
     targets = tx_exec_benchmark
 }
 
-criterion_main!(tx_exec_bench);
+criterion_main!(tx_exec_blk_bench);
