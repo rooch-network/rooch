a606416cbf1c0f2007df7ef2835fe95de6edaffb
TwilightLogic <twilightlogic@gmail.com>
2024-04-12 18:42:51 +0800

    chore: remove unused code

--- rooch-portal-v1/src/pages/txblock/tabs/raw-json.tsx
+++ rooch-portal-v1/src/pages/txblock/tabs/raw-json.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { useRoochClientQuery } from '@roochnetwork/rooch-sdk-kit'
 import { Copy } from 'lucide-react'
