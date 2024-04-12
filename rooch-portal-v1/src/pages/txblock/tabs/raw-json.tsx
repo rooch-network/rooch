b3bad93de5fceb9165445f45b93e9f99b9162e97
TwilightLogic <twilightlogic@gmail.com>
2024-04-12 18:42:15 +0800

    feat: complete the UI of transactions browser

--- rooch-portal-v1/src/pages/txblock/tabs/raw-json.tsx
+++ rooch-portal-v1/src/pages/txblock/tabs/raw-json.tsx
@@ -5,6 +5,9 @@ import { Copy } from 'lucide-react'
 import { useState } from 'react'
 import toast from 'react-hot-toast'
 
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
 export const RawJson = () => {
   const [accessPath, setAccessPath] = useState('/object/0x1')
 
@@ -30,17 +33,16 @@ export const RawJson = () => {
   const isDataEmpty = !data || JSON.stringify(data) === '{}' || JSON.stringify(data) === '[]'
 
   return (
-    <div className="container p-0 dark:bg-inherit">
+    <div className="p-0 dark:bg-inherit">
       <div className="flex flex-col items-start justify-start gap-3">
         <div className="flex flex-col items-start justify-start gap-5 font-medium">
+          {/* Access Path */}
           <div className="flex items-center justify-start gap-6 text-sm text-muted-foreground/75 dark:text-muted-foreground">
-            <label htmlFor="access-path-id" className="">
-              Access Path:
-            </label>
+            <label htmlFor="access-path-id">Access Path:</label>
             <Input
               type="text"
               id="access-path-id"
-              className="bg-gray-50 dark:bg-zinc-900 border border-gray-300 text-gray-900 dark:bg-inherit dark:border-gray-600 dark:text-gray-300 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block focus:outline-gray-500 p-2.5 w-max"
+              className="bg-gray-50 dark:bg-zinc-900 border border-gray-300 text-gray-900 dark:bg-inherit dark:border-gray-600 dark:text-gray-300 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block focus:outline-gray-500 p-2.5 w-4/5"
               value={accessPath}
               onChange={(e) => setAccessPath(e.target.value)}
             />
@@ -49,23 +51,39 @@ export const RawJson = () => {
             </p>
           </div>
 
-          {isPending ? (
-            <p className="dark:text-gray-300">Loading...</p>
-          ) : !isDataEmpty ? (
-            <div className="relative p-4 bg-gray-100 dark:bg-zinc-900 rounded-lg overflow-auto">
-              <pre className="whitespace-pre-wrap text-sm text-gray-900 dark:text-gray-300 word-break break-all">
-                {JSON.stringify(data, null, 2)}
-              </pre>
-              <Button
-                variant="outline"
-                size="icon"
-                onClick={copyToClipboard}
-                className="absolute top-2 right-2 m-2 w-8 h-8 rounded-lg"
-              >
-                <Copy className="w-3 h-3" />
-              </Button>
+          {/* Raw Json */}
+          <div className="flex flex-col items-start justify-start font-medium">
+            <div
+              className="flex items-center justify-start gap-1 text-sm text-muted-foreground/75 dark:text-muted-foreground cursor-pointer"
+              onClick={copyToClipboard}
+            >
+              <span>Raw Json</span>
+              <div>
+                <Copy className="w-4 h-4" />
+              </div>
             </div>
-          ) : null}
+            {isPending ? (
+              <p className="dark:text-gray-300">Loading...</p>
+            ) : !isDataEmpty ? (
+              <div className="rounded-lg flex flex-col items-start">
+                <SyntaxHighlighter
+                  language="json"
+                  style={vscDarkPlus}
+                  customStyle={{
+                    whiteSpace: 'pre-wrap',
+                    overflow: 'scroll',
+                    width: '100%',
+                    borderRadius: '9px',
+                  }}
+                  wrapLongLines={true}
+                >
+                  {JSON.stringify(data, null, 2)}
+                </SyntaxHighlighter>
+              </div>
+            ) : (
+              <p className="dark:text-gray-300">No data found</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
