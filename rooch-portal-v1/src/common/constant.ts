7aa988a0e5203779e250d536177b554d1980c287
TwilightLogic <82878302+TwilightLogic@users.noreply.github.com>
2024-04-13 19:11:05 +0800

    feature: complete the transfer NFT modal (#1566)
    * feat: add jazzicon
    
    * feat: add transfer modal
    
    * feat: complete the dark mode UI design on tranfer nft modal
    
    * feat: complete the responsive design
    
    * feat: complete close moadl event
    
    * feat(indexed-assets): improve the UI

--- rooch-portal-v1/src/common/constant.ts
+++ rooch-portal-v1/src/common/constant.ts
@@ -1,5 +1,6 @@
 import { WalletsMaterialProps } from './interface'
 
+// ** Wallet Connect
 export const walletsMaterialMap = new Map<string, WalletsMaterialProps>([
   [
     'unisat',
@@ -32,3 +33,63 @@ export const walletsMaterialMap = new Map<string, WalletsMaterialProps>([
     },
   ],
 ])
+
+// ** Assets NFT
+export const nftData = [
+  {
+    id: 1,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/96f26dfaeb80982b4c48ef7b6d1a42a1.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/7700594825d9090b03f7134a9f96d9f0.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/d0f989ab16333bbf348fc74f0d4a6d8d.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 4,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/c8edb3d3eb5549a10f3cd2a919c7e6e6.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 5,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/96f26dfaeb80982b4c48ef7b6d1a42a1.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/7700594825d9090b03f7134a9f96d9f0.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 7,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/d0f989ab16333bbf348fc74f0d4a6d8d.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+  {
+    id: 8,
+    imageUrl:
+      'https://i.seadn.io/s/raw/files/c8edb3d3eb5549a10f3cd2a919c7e6e6.png?auto=format&dpr=1&w=640',
+    title: 'Rooch OG NFT',
+    price: '6.988 ETH',
+  },
+]
