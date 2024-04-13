c2dddf0c8e50c52aeafaa4382e70bc6cd233552d
TwilightLogic <twilightlogic@gmail.com>
2024-04-13 14:54:13 +0800

    feat: add transfer modal

--- rooch-portal-v1/src/pages/assets/components/nft-card.tsx
+++ rooch-portal-v1/src/pages/assets/components/nft-card.tsx
@@ -1,5 +1,5 @@
-import { AspectRatio } from '@/components/ui/aspect-ratio'
-import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+
 import {
   Card,
   CardContent,
@@ -8,68 +8,17 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+
+import { nftData } from '@/common/constant'
+import { ArrowLeft, Copy } from 'lucide-react'
+import { formatAddress } from '../../../utils/format'
+import { Input } from '@/components/ui/input'
+
+console.log(nftData)
 
 export const NftCard = () => {
-  const nftData = [
-    {
-      id: 1,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/96f26dfaeb80982b4c48ef7b6d1a42a1.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 2,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/7700594825d9090b03f7134a9f96d9f0.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 3,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/d0f989ab16333bbf348fc74f0d4a6d8d.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 4,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/c8edb3d3eb5549a10f3cd2a919c7e6e6.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 5,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/96f26dfaeb80982b4c48ef7b6d1a42a1.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 6,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/7700594825d9090b03f7134a9f96d9f0.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 7,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/d0f989ab16333bbf348fc74f0d4a6d8d.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-    {
-      id: 8,
-      imageUrl:
-        'https://i.seadn.io/s/raw/files/c8edb3d3eb5549a10f3cd2a919c7e6e6.png?auto=format&dpr=1&w=640',
-      title: 'Rooch OG NFT',
-      price: '6.988 ETH',
-    },
-  ]
-
   const [modalOpen, setModalOpen] = useState(false)
   const [selectedImage, setSelectedImage] = useState('')
 
@@ -117,17 +66,70 @@ export const NftCard = () => {
       ))}
 
       {modalOpen && (
-        <div
-          onClick={handleCloseModal}
-          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
-        >
-          <div className="bg-white rounded-lg flex flex-col items-start justify-center">
-            <div className="p-4">
-              <img
-                src={selectedImage}
-                alt="Selected NFT"
-                className="w-[480px] h-auto md:w-[640px] rounded-lg"
-              />
+        <div className="flex items-center justify-center font-mono">
+          <div
+            className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50"
+            onClick={handleCloseModal}
+          >
+            <div className="bg-zinc-900 rounded-lg flex flex-col items-start justify-center p-6">
+              {/* Back */}
+              <div className="mb-4">
+                <Button variant="secondary" size="sm" className="h-8 w-14 rounded-2xl">
+                  <ArrowLeft className="w-5 h-5 text-gray-200" />
+                </Button>
+              </div>
+
+              {/* Content */}
+              <div className="flex items-start justify-start gap-12 mr-6">
+                {/* NFT Image */}
+                <div>
+                  <img
+                    src={selectedImage}
+                    alt="Selected NFT"
+                    className="w-full md:max-w-[420px] h-auto rounded-lg"
+                  />
+                </div>
+
+                {/* Transfer Description */}
+                <div className="flex flex-col items-start justify-start gap-3 w-[320px]">
+                  {/* From Address */}
+                  <div>
+                    <span className="text-base font-normal text-gray-100 flex items-center justify-start gap-2">
+                      <p>
+                        {formatAddress(
+                          'bc1pk33n2t5zulq7nz3k8rq55dywjt89szfukypjua5zmuhfg40338wsv9ss7q',
+                        )}
+                      </p>
+                      <Copy className="w-4 h-4 text-muted-foreground" />
+                    </span>
+                  </div>
+
+                  {/* Send */}
+                  <span className="text-zinc-500 font-normal">Send</span>
+
+                  {/* NFT Name */}
+                  <span className="text-gray-50 text-3xl font-normal tracking-wide">
+                    Rooch OG NFT
+                  </span>
+
+                  {/* To */}
+                  <span className="text-zinc-500 font-normal">To</span>
+
+                  {/* To Address (Input) */}
+                  <Input
+                    className="h-12 rounded-2xl bg-gray-50 text-gray-800"
+                    placeholder="bc1pr6mdxnc348lua02c32ad4uyyaw3kavjz4c8jzkh5ffvuq4ryvxhsf879j5"
+                  />
+
+                  {/* CTA */}
+                  <button
+                    type="button"
+                    className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mt-24 duration-300 ease-in-out w-full h-11"
+                  >
+                    Transfer
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
