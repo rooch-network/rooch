5620ce95b0742ed3d4b54c1c2b4b83a419fbbd24
TwilightLogic <twilightlogic@gmail.com>
2024-04-14 15:21:28 +0800

    refactor: comment the batch mode on SFT assets page

--- rooch-portal-v1/src/pages/assets/assets-details/tabs/sft/assets-sft.tsx
+++ rooch-portal-v1/src/pages/assets/assets-details/tabs/sft/assets-sft.tsx
@@ -1,12 +1,12 @@
 import { SftCard } from './components/sft-card'
-import { SftTabHeader } from './components/sft-tab-header'
+// import { SftTabHeader } from './components/sft-tab-header'
 
 export const AssetsSft = () => {
   return (
     <>
-      <div>
+      {/* <div>
         <SftTabHeader />
-      </div>
+      </div> */}
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full place-items-start mt-2">
         <SftCard />
       </div>
