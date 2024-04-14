1f0ce310d9c1494f439fd90fe2b8dbb076f1dd2b
TwilightLogic <82878302+TwilightLogic@users.noreply.github.com>
2024-04-14 22:16:43 +0800

    refactor: improve the UI (#1569)
    * feat: add jazzicon
    
    * feat: add transfer modal
    
    * feat: complete the dark mode UI design on tranfer nft modal
    
    * feat: complete the responsive design
    
    * feat: complete close moadl event
    
    * feat(indexed-assets): improve the UI
    
    * refactor: improve UI on aasets page
    
    * refactor: update tab UI
    
    * refactor: update UI on mint page
    
    * refactor: comment the batch mode on SFT assets page
    
    * feat: add banner
    
    * feat: update some UI details
    
    * feat: update some UI details on banner
    
    * feat: update some UI details on wallet connect

--- rooch-portal-v1/src/pages/dashboard-layout.tsx
+++ rooch-portal-v1/src/pages/dashboard-layout.tsx
@@ -1,17 +1,34 @@
 import { MainContent } from '../components/main-content'
 import { Navbar } from '../components/navbar'
 import { Sidebar } from '../components/sidebar'
+import { Banner } from '../components/banner'
+import { useState } from 'react'
 
 export const DashboardLayout = () => {
+  const [isBannerVisible, setIsBannerVisible] = useState(true)
+
+  const handleCloseBanner = () => {
+    setIsBannerVisible(false)
+  }
+
   return (
     <div className="h-full scroll-smooth font-sans">
-      <div className="h-[56px] md:h-[64px] md:pl-60 fixed inset-y-0 w-full z-50">
+      {isBannerVisible && <Banner onClose={handleCloseBanner} />}
+      <div
+        className={`fixed inset-x-0 top-0 w-full z-40 ${isBannerVisible ? 'pt-[37px]' : 'pt-0'}`}
+      >
         <Navbar />
       </div>
-      <div className="hidden md:flex h-full w-60 flex-col fixed inset-y-0 z-50">
+      <div
+        className={`hidden md:flex h-full w-60 flex-col fixed inset-y-0 z-50 ${
+          isBannerVisible ? 'pt-[37px]' : 'pt-0'
+        }`}
+      >
         <Sidebar onClose={() => {}} />
       </div>
-      <main className="md:pl-60 pt-[64px] h-full w-full overflow-y-auto">
+      <main
+        className={`md:pl-60 h-full w-full overflow-y-auto ${isBannerVisible ? 'pt-32' : ' pt-20'}`}
+      >
         <MainContent />
       </main>
     </div>
