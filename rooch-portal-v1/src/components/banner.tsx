a724d58cad759235786e6723274405f238f5d2be
TwilightLogic <twilightlogic@gmail.com>
2024-04-14 16:50:05 +0800

    feat: update some UI details on banner

--- rooch-portal-v1/src/components/banner.tsx
+++ rooch-portal-v1/src/components/banner.tsx
@@ -11,16 +11,8 @@ export const Banner: React.FC<BannerProps> = ({ onClose }) => {
     >
       <div className="flex items-center mx-auto">
         <p className="flex items-center text-sm font-normal text-zinc-500 dark:text-zinc-400">
-          <span className="inline-flex p-1 me-3 bg-zinc-200 rounded-full dark:bg-zinc-600 w-6 h-6 items-center justify-center flex-shrink-0">
-            <svg
-              className="w-3 h-3 text-zinc-500 dark:text-zinc-400"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 18 19"
-            >
-              <path d="M15 1.943v12.114a1 1 0 0 1-1.581.814L8 11V5l5.419-3.871A1 1 0 0 1 15 1.943ZM7 4H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2v5a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V4ZM4 17v-5h1v5H4ZM16 5.183v5.634a2.984 2.984 0 0 0 0-5.634Z" />
-            </svg>
+          <span className="inline-flex p-1 me-2 bg-zinc-400 rounded-full dark:bg-zinc-600 w-5 h-5 items-center justify-center flex-shrink-0">
+            <img src="/rooch_white_logo.svg" alt="rooc" className="w-3 h-3" />
             <span className="sr-only">Light bulb</span>
           </span>
           <span>
