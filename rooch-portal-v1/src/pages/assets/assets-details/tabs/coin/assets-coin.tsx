1af3d9133eac9e03227a24ecd54bdb63d20f6e24
TwilightLogic <twilightlogic@gmail.com>
2024-04-17 12:44:14 +0800

    feat: add no data page on Assets Coin page

--- rooch-portal-v1/src/pages/assets/assets-details/tabs/coin/assets-coin.tsx
+++ rooch-portal-v1/src/pages/assets/assets-details/tabs/coin/assets-coin.tsx
@@ -21,31 +21,69 @@ import {
 
 import { Button } from '@/components/ui/button'
 import { GripVerticalIcon } from 'lucide-react'
+import { useTheme } from '@/components/theme-provider'
 
-const coins = [
+interface Coin {
+  coin: string
+  balance: number
+  value: string
+}
+
+// const coins: Coin[] = [
+//   {
+//     coin: 'ROOCH',
+//     balance: 288.88,
+//     value: '$1,146.98',
+//   },
+//   {
+//     coin: 'BTC',
+//     balance: 1.88,
+//     value: '$52,988.00',
+//   },
+//   {
+//     coin: 'USDC',
+//     balance: 12.88,
+//     value: '$0.99',
+//   },
+//   {
+//     coin: 'ETH',
+//     balance: 10.99,
+//     value: '$2,800.00',
+//   },
+// ]
+
+const coins: Coin[] = [
   {
-    coin: 'ROOCH',
-    balance: 288.88,
-    value: '$1,146.98',
-  },
-  {
-    coin: 'BTC',
-    balance: 1.88,
-    value: '$52,988.00',
-  },
-  {
-    coin: 'USDC',
-    balance: 12.88,
-    value: '$0.99',
-  },
-  {
-    coin: 'ETH',
-    balance: 10.99,
-    value: '$2,800.00',
+    coin: '',
+    balance: 0,
+    value: '',
   },
 ]
 
 export const AssetsCoin = () => {
+  const { theme } = useTheme()
+
+  const logoSrc = theme === 'dark' ? '/rooch_white_logo.svg' : '/rooch_black_logo.svg'
+
+  const hasValidData = (coins: Coin[]): boolean => {
+    return coins.some((coin) => coin.coin.trim() !== '' && coin.balance !== 0)
+  }
+
+  if (!hasValidData(coins)) {
+    return (
+      <div className="rounded-lg border w-full">
+        <Table>
+          <TableRow>
+            <div className="flex justify-center items-center flex-col" style={{ height: '80vh' }}>
+              <img src={logoSrc} alt="No Data" style={{ width: '200px', height: '200px' }} />
+              <p className="text-gray-500 mt-4">No data found :)</p>
+            </div>
+          </TableRow>
+        </Table>
+      </div>
+    )
+  }
+
   return (
     <div className="rounded-lg border overflow-hidden">
       <Table>
