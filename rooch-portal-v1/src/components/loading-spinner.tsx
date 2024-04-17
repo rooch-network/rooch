91802296e1e0acb94bac947886df23f3169ef356
TwilightLogic <twilightlogic@gmail.com>
2024-04-17 13:45:00 +0800

    refactor: update the UI design on spinner modal

--- rooch-portal-v1/src/components/loading-spinner.tsx
+++ rooch-portal-v1/src/components/loading-spinner.tsx
@@ -3,7 +3,7 @@ export const LoadingSpinner = () => {
     <div role="status">
       <svg
         aria-hidden="true"
-        className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-gray-900"
+        className="w-8 h-8 text-zinc-200 animate-spin dark:text-zinc-300 fill-zinc-900"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
