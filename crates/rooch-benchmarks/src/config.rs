096742b57f61ab1ce03d25b65dc7a123a8cd34b0
popcnt <142196625+popcnt1@users.noreply.github.com>
2024-04-17 17:26:06 +0800

    chore： fix fmt (#1584)

--- crates/rooch-benchmarks/src/config.rs
+++ crates/rooch-benchmarks/src/config.rs
@@ -81,7 +81,7 @@ impl Display for TxType {
 pub struct BenchTxConfig {
     pub tx_type: Option<TxType>, // empty(default)/transfer/btc-block
     pub data_import_mode: Option<DataImportMode>, // utxo(default)/ord/none/full
-    pub btc_block_dir: Option<String>,  // btc block dir, file name: <height>.hex
+    pub btc_block_dir: Option<String>, // btc block dir, file name: <height>.hex
     pub pprof_output: Option<PProfOutput>, // flamegraph(default)/proto
 }
 
