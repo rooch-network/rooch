42befb2a90f38180a59f631986a32235e054749e
TwilightLogic <twilightlogic@gmail.com>
2024-04-17 13:35:14 +0800

    chore: add overflow auto on session key modal

--- rooch-portal-v1/src/components/session-key-modal.tsx
+++ rooch-portal-v1/src/components/session-key-modal.tsx
@@ -1,4 +1,3 @@
-import { formatAddress } from '@/utils/format'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 
@@ -25,13 +24,11 @@ export const SessionKeyModal: React.FC<SessionKeyModalProps> = ({
         </p>
         <div className="bg-zinc-700 p-4 rounded-lg">
           {/* SCOPE */}
-          <div className="flex flex-col items-start justify-start text-gray-300 text-sm">
+          <div className="flex flex-col items-start justify-start text-gray-300 text-sm overflow-auto">
             <h3 className="text-xs mb-1 font-medium text-gray-400">Scope</h3>
             <span>0x1..</span>
             <span>0x2..</span>
-            <span>
-              {formatAddress('0xd8a78bcf08402de9c19be5b958694ad9027e1c6f482fdaee6b7327ca1982549e')}
-            </span>
+            <span>0xd8a78bcf08402de9c19be5b958694ad9027e1c6f482fdaee6b7327ca1982549e</span>
             <span>0x6..</span>
           </div>
           <Separator className="bg-muted-foreground/50 h-[0.5px] my-1.5" />
