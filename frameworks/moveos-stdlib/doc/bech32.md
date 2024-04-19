e1739398b43ab39d9b67774c6f0ce670eda5e425
feliciss <22887031+feliciss@users.noreply.github.com>
2024-04-19 20:23:16 +0900

    [gh-1552] upgrade bech32 pkg and add new features of move bech32. (#1586)
    * [gh-1552] upgrade bech32 pkg and add new features of move.
    
    * [gh-1552] add missing code df.
    
    * [gh-1552] fix syntax.
    
    ---------
    
    Co-authored-by: Feliciss <10203-feliciss@users.noreply.0xacab.org>

--- frameworks/moveos-stdlib/doc/bech32.md
+++ frameworks/moveos-stdlib/doc/bech32.md
@@ -7,8 +7,10 @@ Module which defines bech32 functions.
 
 
 -  [Constants](#@Constants_0)
--  [Function `encoding`](#0x2_bech32_encoding)
--  [Function `decoding`](#0x2_bech32_decoding)
+-  [Function `encode`](#0x2_bech32_encode)
+-  [Function `segwit_encode`](#0x2_bech32_segwit_encode)
+-  [Function `decode`](#0x2_bech32_decode)
+-  [Function `segwit_decode`](#0x2_bech32_segwit_decode)
 
 
 <pre><code></code></pre>
@@ -24,32 +26,99 @@ Module which defines bech32 functions.
 
 
 
-<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_DECODE_FAILED">E_DECODE_FAILED</a>: u64 = 1;
+<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_DECODE_FAILED">E_DECODE_FAILED</a>: u64 = 2;
 </code></pre>
 
 
 
-<a name="0x2_bech32_encoding"></a>
-
-## Function `encoding`
-
-@param public_key: 20 or 32 bytes public keys
-@param witness_version: 0 for bech32 encoding and 1-16 for bech32m encoding.
-Encode the public keys with Bech32 or Bech32m encoding algorithm and returns 42 or 62 length Bech32 or Bech32m addresses.
+<a name="0x2_bech32_E_ENCODE_FAILED"></a>
 
 
-<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_encoding">encoding</a>(public_key: &<a href="">vector</a>&lt;u8&gt;, witness_version: u8): <a href="">vector</a>&lt;u8&gt;
+
+<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_ENCODE_FAILED">E_ENCODE_FAILED</a>: u64 = 1;
 </code></pre>
 
 
 
-<a name="0x2_bech32_decoding"></a>
-
-## Function `decoding`
-
-@param data: 42 or 62 length Bech32 or Bech32m address bytes
-Decode the encoded 42 or 62 length Bech32 or Bech32m address bytes with Bech32 or Bech32m decoding algorithm and returns 20 or 32 bytes of public keys.
+<a name="0x2_bech32_E_INVALID_BIP_CODE"></a>
 
 
-<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_decoding">decoding</a>(data: &<a href="">vector</a>&lt;u8&gt;): <a href="">vector</a>&lt;u8&gt;
+
+<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_INVALID_BIP_CODE">E_INVALID_BIP_CODE</a>: u64 = 3;
+</code></pre>
+
+
+
+<a name="0x2_bech32_E_INVALID_NETWORK"></a>
+
+
+
+<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_INVALID_NETWORK">E_INVALID_NETWORK</a>: u64 = 4;
+</code></pre>
+
+
+
+<a name="0x2_bech32_E_INVALID_WITNESS_VERSION"></a>
+
+
+
+<pre><code><b>const</b> <a href="bech32.md#0x2_bech32_E_INVALID_WITNESS_VERSION">E_INVALID_WITNESS_VERSION</a>: u64 = 5;
+</code></pre>
+
+
+
+<a name="0x2_bech32_encode"></a>
+
+## Function `encode`
+
+@param hrp: human-readable part in string
+@param data: arbitrary data to be encoded.
+Encode arbitrary data using string as the human-readable part and append a bech32 checksum.
+
+
+<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_encode">encode</a>(bip: u16, hrp: <a href="">vector</a>&lt;u8&gt;, data: <a href="">vector</a>&lt;u8&gt;): <a href="">vector</a>&lt;u8&gt;
+</code></pre>
+
+
+
+<a name="0x2_bech32_segwit_encode"></a>
+
+## Function `segwit_encode`
+
+@param network: network to be selected, i.e. bc, tb, or bcrt
+@param witness_version: segwit witness version. 0 for bech32, 1 for bech32m and taproot, and 2-16 is included.
+@param data: arbitrary data to be encoded.
+Encode arbitrary data to a Bitcoin address using string as the network, number as the witness version
+
+
+<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_segwit_encode">segwit_encode</a>(network: <a href="">vector</a>&lt;u8&gt;, witness_version: u8, data: <a href="">vector</a>&lt;u8&gt;): <a href="">vector</a>&lt;u8&gt;
+</code></pre>
+
+
+
+<a name="0x2_bech32_decode"></a>
+
+## Function `decode`
+
+@param hrp: human-readable part bytes to be used as a decoding input
+@param encoded: encoded bytes to be decoded as data
+Decode a bech32 encoded string that includes a bech32 checksum.
+
+
+<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_decode">decode</a>(hrp: <a href="">vector</a>&lt;u8&gt;, encoded: <a href="">vector</a>&lt;u8&gt;): <a href="">vector</a>&lt;u8&gt;
+</code></pre>
+
+
+
+<a name="0x2_bech32_segwit_decode"></a>
+
+## Function `segwit_decode`
+
+@param hrp: human-readable part bytes to be used as a decoding input
+@param witness_ascii_version: segwit witness ASCII version to be used as a decoding input
+@param encoded: encoded bytes to be decoded as data
+Decode an encoded Bitcoin address
+
+
+<pre><code><b>public</b> <b>fun</b> <a href="bech32.md#0x2_bech32_segwit_decode">segwit_decode</a>(hrp: <a href="">vector</a>&lt;u8&gt;, witness_ascii_version: u8, encoded: <a href="">vector</a>&lt;u8&gt;): <a href="">vector</a>&lt;u8&gt;
 </code></pre>
--- frameworks/moveos-stdlib/src/natives/moveos_stdlib/bech32.rs
+++ frameworks/moveos-stdlib/src/natives/moveos_stdlib/bech32.rs
@@ -2,52 +2,77 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::natives::helpers::{make_module_natives, make_native};
-use bech32::{self, ToBase32, Variant};
+use bech32::{
+    primitives::hrp,
+    segwit::{self, VERSION_0, VERSION_1},
+    Bech32, Bech32m, Fe32, Hrp, NoChecksum,
+};
 use move_binary_format::errors::PartialVMResult;
 use move_core_types::gas_algebra::{InternalGas, InternalGasPerByte, NumBytes};
 use move_vm_runtime::native_functions::{NativeContext, NativeFunction};
 use move_vm_types::{
-    loaded_data::runtime_types::Type,
-    natives::function::NativeResult,
-    pop_arg,
-    values::{Value, VectorRef},
+    loaded_data::runtime_types::Type, natives::function::NativeResult, pop_arg, values::Value,
 };
 use smallvec::smallvec;
 use std::collections::VecDeque;
 
-pub const E_DECODE_FAILED: u64 = 1;
+pub const E_ENCODE_FAILED: u64 = 1;
+pub const E_DECODE_FAILED: u64 = 2;
+pub const E_INVALID_BIP_CODE: u64 = 3;
+pub const E_INVALID_NETWORK: u64 = 4;
+pub const E_INVALID_WITNESS_VERSION: u64 = 5;
 
 /***************************************************************************************************
- * native fun encoding
- * Implementation of the Move native function `bech32::encoding(public_key: &vector<u8>, witness_version: u8): vector<u8>`
- *   gas cost: encoding_cost_base                               | base cost for function call and fixed opers
- *              + encoding_data_cost_per_byte * msg.len()       | cost depends on length of message
- *              + encoding_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
+ * native fun encode
+ * Implementation of the Move native function `bech32::encode(bip: u16, hrp: vector<u8>, data: vector<u8>): vector<u8>`
+ *   gas cost: encode_cost_base                               | base cost for function call and fixed opers
+ *              + encode_data_cost_per_byte * msg.len()       | cost depends on length of message
+ *              + encode_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
  **************************************************************************************************/
-pub fn native_encoding(
+pub fn native_encode(
     gas_params: &FromBytesGasParameters,
     _context: &mut NativeContext,
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
     debug_assert!(ty_args.is_empty());
-    debug_assert!(args.len() == 2);
+    debug_assert!(args.len() == 3);
 
-    let witness_version = pop_arg!(args, u8);
-    let public_key = pop_arg!(args, VectorRef);
+    let data = pop_arg!(args, Vec<u8>);
+    let hrp = pop_arg!(args, Vec<u8>);
+    let bip = pop_arg!(args, u16);
 
     let cost = gas_params.base
-        + (gas_params.per_byte * NumBytes::new(public_key.as_bytes_ref().len() as u64));
+        + (gas_params.per_byte * NumBytes::new(hrp.len() as u64 + data.len() as u64 + bip as u64));
 
-    let (hrp, variant) = if witness_version == 0 {
-        ("bech32", Variant::Bech32)
-    } else {
-        ("bech32m", Variant::Bech32m)
+    let hrp_string = match String::from_utf8(hrp.to_vec()) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+    };
+    let hrp = match Hrp::parse(&hrp_string) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
     };
 
-    // TODO: handle the encode error
-    let encoded =
-        bech32::encode(hrp, public_key.as_bytes_ref().to_vec().to_base32(), variant).unwrap();
+    // bip173 = bech32; bip350 = bech32m; bip0 = NoChecksum.
+    let encoded = if bip == 173 {
+        match bech32::encode::<Bech32>(hrp, &data.to_vec()) {
+            Ok(v) => v,
+            Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+        }
+    } else if bip == 350 {
+        match bech32::encode::<Bech32m>(hrp, &data.to_vec()) {
+            Ok(v) => v,
+            Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+        }
+    } else if bip == 0 {
+        match bech32::encode::<NoChecksum>(hrp, &data.to_vec()) {
+            Ok(v) => v,
+            Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+        }
+    } else {
+        return Ok(NativeResult::err(cost, E_INVALID_BIP_CODE));
+    };
 
     Ok(NativeResult::ok(
         cost,
@@ -56,54 +81,164 @@ pub fn native_encoding(
 }
 
 /***************************************************************************************************
- * native fun decoding
- * Implementation of the Move native function `bech32::decoding(data: u8): vector<u8>`
- *   gas cost: decoding_cost_base                               | base cost for function call and fixed opers
- *              + decoding_data_cost_per_byte * msg.len()       | cost depends on length of message
- *              + decoding_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
+ * native fun segwit_encode
+ * Implementation of the Move native function `bech32::segwit_encode(network: vector<u8>, witness_version: u8, data: vector<u8>): vector<u8>`
+ *   gas cost: segwit_encode_cost_base                               | base cost for function call and fixed opers
+ *              + segwit_encode_data_cost_per_byte * msg.len()       | cost depends on length of message
+ *              + segwit_encode_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
  **************************************************************************************************/
-pub fn native_decoding(
+pub fn native_segwit_encode(
     gas_params: &FromBytesGasParameters,
     _context: &mut NativeContext,
     ty_args: Vec<Type>,
     mut args: VecDeque<Value>,
 ) -> PartialVMResult<NativeResult> {
     debug_assert!(ty_args.is_empty());
-    debug_assert!(args.len() == 1);
+    debug_assert!(args.len() == 3);
 
-    let data = pop_arg!(args, VectorRef);
+    let data = pop_arg!(args, Vec<u8>);
+    let witness_version = pop_arg!(args, u8);
+    let network = pop_arg!(args, Vec<u8>);
 
-    let cost =
-        gas_params.base + (gas_params.per_byte * NumBytes::new(data.as_bytes_ref().len() as u64));
+    let cost = gas_params.base
+        + (gas_params.per_byte
+            * NumBytes::new(network.len() as u64 + data.len() as u64 + witness_version as u64));
 
-    let binding = data.as_bytes_ref();
-    let data_str = match std::str::from_utf8(&binding) {
+    let network_string = match String::from_utf8(network.to_vec()) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+    };
+
+    let hrp = if network_string == "bc" {
+        hrp::BC // bitcoin mainnet network
+    } else if network_string == "tb" {
+        hrp::TB // bitcoin testnet or signet network
+    } else if network_string == "bcrt" {
+        hrp::BCRT // bitcoin regtest network
+    } else {
+        return Ok(NativeResult::err(cost, E_INVALID_NETWORK));
+    };
+
+    let version = if witness_version == 0 {
+        VERSION_0 // bech32 -> q
+    } else if (1..=16).contains(&witness_version) {
+        VERSION_1 // bech32m -> p (taproot)
+    } else {
+        return Ok(NativeResult::err(cost, E_INVALID_WITNESS_VERSION));
+    };
+
+    let encoded = match segwit::encode(hrp, version, &data) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_ENCODE_FAILED)),
+    };
+
+    Ok(NativeResult::ok(
+        cost,
+        smallvec![Value::vector_u8(encoded.bytes())],
+    ))
+}
+
+/***************************************************************************************************
+ * native fun decode
+ * Implementation of the Move native function `bech32::decode(hrp: vector<u8>, encoded: vector<u8>): vector<u8>`
+ *   gas cost: decode_cost_base                               | base cost for function call and fixed opers
+ *              + decode_data_cost_per_byte * msg.len()       | cost depends on length of message
+ *              + decode_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
+ **************************************************************************************************/
+pub fn native_decode(
+    gas_params: &FromBytesGasParameters,
+    _context: &mut NativeContext,
+    ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(ty_args.is_empty());
+    debug_assert!(args.len() == 2);
+
+    let encoded = pop_arg!(args, Vec<u8>);
+    let hrp = pop_arg!(args, Vec<u8>);
+
+    let cost = gas_params.base
+        + (gas_params.per_byte * NumBytes::new(hrp.len() as u64 + encoded.len() as u64));
+
+    let binding = encoded;
+    let encoded_str = match std::str::from_utf8(&binding) {
         Ok(v) => v,
         Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
     };
 
-    let (hrp, public_key, variant) = match bech32::decode(data_str) {
-        Ok((v1, v2, v3)) => (v1, v2, v3),
+    let hrp_string = match String::from_utf8(hrp.to_vec()) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    let hrp_origin = match Hrp::parse(&hrp_string) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    let (hrp, data) = match bech32::decode(encoded_str) {
+        Ok((v1, v2)) => (v1, v2),
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    if hrp != hrp_origin {
+        return Ok(NativeResult::err(cost, E_DECODE_FAILED));
+    };
+
+    Ok(NativeResult::ok(cost, smallvec![Value::vector_u8(data)]))
+}
+
+/***************************************************************************************************
+ * native fun segwit_decode
+ * Implementation of the Move native function `bech32::segwit_decode(hrp: vector<u8>, witness_ascii_version: u8, encoded: vector<u8>): vector<u8>`
+ *   gas cost: segwit_decode_cost_base                               | base cost for function call and fixed opers
+ *              + segwit_decode_data_cost_per_byte * msg.len()       | cost depends on length of message
+ *              + segwit_decode_data_cost_per_block * num_blocks     | cost depends on number of blocks in message
+ **************************************************************************************************/
+pub fn native_segwit_decode(
+    gas_params: &FromBytesGasParameters,
+    _context: &mut NativeContext,
+    ty_args: Vec<Type>,
+    mut args: VecDeque<Value>,
+) -> PartialVMResult<NativeResult> {
+    debug_assert!(ty_args.is_empty());
+    debug_assert!(args.len() == 3);
+
+    let encoded = pop_arg!(args, Vec<u8>);
+    let witness_ascii_version = pop_arg!(args, u8);
+    let hrp = pop_arg!(args, Vec<u8>);
+
+    let cost = gas_params.base
+        + (gas_params.per_byte
+            * NumBytes::new(
+                encoded.len() as u64 + witness_ascii_version as u64 + hrp.len() as u64,
+            ));
+
+    let binding = encoded;
+    let encoded_str = match std::str::from_utf8(&binding) {
+        Ok(v) => v,
         Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
     };
 
-    if hrp != "bech32" && hrp != "bech32m" {
+    let hrp_string = match String::from_utf8(hrp.to_vec()) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    let hrp_origin = match Hrp::parse(&hrp_string) {
+        Ok(v) => v,
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    let version_origin = Fe32::from_char_unchecked(witness_ascii_version);
+
+    let (hrp, version, data) = match segwit::decode(encoded_str) {
+        Ok((v1, v2, v3)) => (v1, v2, v3),
+        Err(_) => return Ok(NativeResult::err(cost, E_DECODE_FAILED)),
+    };
+    if hrp != hrp_origin {
         return Ok(NativeResult::err(cost, E_DECODE_FAILED));
     };
-
-    if variant != Variant::Bech32 && variant != Variant::Bech32m {
+    if version != version_origin {
         return Ok(NativeResult::err(cost, E_DECODE_FAILED));
     }
 
-    // TODO redact public length to 20 and 32 bytes
-    if public_key.len() != 32 && public_key.len() != 52 {
-        return Ok(NativeResult::err(cost, E_DECODE_FAILED));
-    }
-
-    Ok(NativeResult::ok(
-        cost,
-        smallvec![Value::vector_u8(public_key.into_iter().map(u8::from))],
-    ))
+    Ok(NativeResult::ok(cost, smallvec![Value::vector_u8(data)]))
 }
 
 #[derive(Debug, Clone)]
@@ -127,30 +262,35 @@ impl FromBytesGasParameters {
 
 #[derive(Debug, Clone)]
 pub struct GasParameters {
-    pub encoding: FromBytesGasParameters,
-    pub decoding: FromBytesGasParameters,
+    pub encode: FromBytesGasParameters,
+    pub segwit_encode: FromBytesGasParameters,
+    pub decode: FromBytesGasParameters,
+    pub segwit_decode: FromBytesGasParameters,
 }
 
 impl GasParameters {
     pub fn zeros() -> Self {
         Self {
-            encoding: FromBytesGasParameters::zeros(),
-            decoding: FromBytesGasParameters::zeros(),
+            encode: FromBytesGasParameters::zeros(),
+            segwit_encode: FromBytesGasParameters::zeros(),
+            decode: FromBytesGasParameters::zeros(),
+            segwit_decode: FromBytesGasParameters::zeros(),
         }
     }
 }
 
 pub fn make_all(gas_params: GasParameters) -> impl Iterator<Item = (String, NativeFunction)> {
     let natives = [
+        ("encode", make_native(gas_params.encode, native_encode)),
         (
-            "encoding",
-            make_native(gas_params.encoding, native_encoding),
+            "segwit_encode",
+            make_native(gas_params.segwit_encode, native_segwit_encode),
         ),
+        ("decode", make_native(gas_params.decode, native_decode)),
         (
-            "decoding",
-            make_native(gas_params.decoding, native_decoding),
+            "segwit_decode",
+            make_native(gas_params.segwit_decode, native_segwit_decode),
         ),
     ];
-
     make_module_natives(natives)
 }
