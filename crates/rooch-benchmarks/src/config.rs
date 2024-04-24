8cfba6710bbe6ae094d6056fdd4066f32402e64e
popcnt <ism3y@proton.me>
2024-04-24 10:50:28 +0800

    fix comments format

--- crates/rooch-benchmarks/src/config.rs
+++ crates/rooch-benchmarks/src/config.rs
@@ -79,12 +79,9 @@ impl Display for TxType {
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize, Parser, Eq)]
 pub struct BenchTxConfig {
-    pub tx_type: Option<TxType>,
-    // empty(default)/transfer/btc-block
-    pub data_import_mode: Option<DataImportMode>,
-    // utxo(default)/ord/none/full
-    pub btc_block_dir: Option<String>,
-    // btc block dir, file name: <height>.hex
+    pub tx_type: Option<TxType>, // empty(default)/transfer/btc-block
+    pub data_import_mode: Option<DataImportMode>, // utxo(default)/ord/none/full
+    pub btc_block_dir: Option<String>, // btc block dir, file name: <height>.hex
     pub pprof_output: Option<PProfOutput>, // flamegraph(default)/proto
 }
 
