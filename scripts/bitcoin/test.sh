b9bcd6d7ff1a9344b2dca303b0565db0e79300fc
Owen Wu <yubing744@163.com>
2024-04-26 09:47:11 +0800

    feat: add test for metaprotocol_validity

--- scripts/bitcoin/test.sh
+++ scripts/bitcoin/test.sh
@@ -2,4 +2,7 @@
 # Copyright (c) RoochNetwork
 # SPDX-License-Identifier: Apache-2.0
 
-RUST_LOG=debug cargo test -p testsuite --test integration -- --name "rooch bitcoin test"
+set -e
+
+cargo run --bin rooch move test -p frameworks/bitcoin-move
+cargo test -p testsuite --test integration -- --name "rooch bitcoin test"
