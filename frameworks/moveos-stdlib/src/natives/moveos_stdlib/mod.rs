8ce00d7ab75bbdc6b6cce6f684e30cd9af0c01a8
Owen Wu <1449069+yubing744@users.noreply.github.com>
2024-05-06 14:16:13 +0800

    Owen/moveos stdlib/cbor (#1633)
    * feat: rebase from main
    
    * feat: update cbor
    
    * feat: cbor type cast
    
    * feat: fix some error
    
    * feat: update cbor
    
    * feat: fix syntax error
    
    * feat: test ok for cbor.move
    
    * feat: test cbor to_map
    
    * feat: rebase from main 2
    
    * feat: rebase from main 3
    
    * feat: test Option and u256
    
    * feat: fmt code
    
    * feat: lint code
    
    * feat: fix cbor comments
    
    * feat: fix test_gas_parameters fail

--- frameworks/moveos-stdlib/src/natives/moveos_stdlib/mod.rs
+++ frameworks/moveos-stdlib/src/natives/moveos_stdlib/mod.rs
@@ -6,6 +6,7 @@ pub mod base58;
 pub mod bcs;
 pub mod bech32;
 pub mod bls12381;
+pub mod cbor;
 pub mod event;
 pub mod hash;
 pub mod json;
--- frameworks/rooch-framework/src/natives/gas_parameter/mod.rs
+++ frameworks/rooch-framework/src/natives/gas_parameter/mod.rs
@@ -5,6 +5,7 @@ mod base58;
 mod bcd;
 mod bech32;
 pub mod bls12381;
+mod cbor;
 mod ecdsa_k1;
 mod ed25519;
 mod events;
