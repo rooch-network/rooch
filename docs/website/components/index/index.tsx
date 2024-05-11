a470365369b3ac2a6def12bdf633c7e8227abd36
TwilightLogic <twilightlogic@gmail.com>
2024-05-11 19:50:09 +0800

    feat: complete the Explore page

--- docs/website/components/index/index.tsx
+++ docs/website/components/index/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-interface Feature {
+interface Card {
   title: string
   description: string
   logo: string
@@ -17,11 +17,12 @@ interface IndexProps {
 
   // FEATURES
   featuresTitle: string
-  features: Feature[]
+  features: Card[]
 
   // EXPLORE
   exploreTitle: string
   exploreContent: string
+  explores: Card[]
 
   // ECOSYSTEM
   ecosystemTitle: string
@@ -42,6 +43,7 @@ export default function Index({
   features,
   exploreTitle,
   exploreContent,
+  explores,
   ecosystemTitle,
   ecosystemContent,
   blogsTitle,
@@ -97,7 +99,7 @@ export default function Index({
             {featuresTitle.split(' ').slice(0, -1).join(' ')}{' '}
             <span className="text-[#FF914B]">{featuresTitle.split(' ').slice(-1)}</span>
           </h2>
-          <div className="mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="mt-16 md:mt-8 grid grid-cols-1 md:grid-cols-3 gap-6">
             {features?.map((feature, index) => (
               <div
                 key={index}
@@ -116,7 +118,6 @@ export default function Index({
             ))}
           </div>
         </div>
-        {/* FEATURES LOGO */}
         <div>
           <Image
             src="/logo/features/features_logo.svg"
@@ -128,10 +129,43 @@ export default function Index({
       </div>
 
       {/* EXPLORE */}
-      <div className="py-20">
-        <div className="container mx-auto px-4 text-center">
-          <h2 className="text-3xl font-bold text-gray-800">{exploreTitle}</h2>
-          <p className="text-gray-600 mt-4">{exploreContent}</p>
+      <div className="py-16 md:py-32 px-6 md:px-24 bg-white flex flex-col md:flex-row items-center justify-center gap-6 md:gap-0">
+        <div className="px-4 max-w-[854px] h-full w-full">
+          <h2 className="text-4xl md:text-6xl font-semibold text-center md:text-start text-[#2E2929]">
+            {exploreTitle.split(' ').slice(0, -2).join(' ')}{' '}
+            <span className="text-[#46977E]">{exploreTitle.split(' ').slice(-2).join(' ')}</span>
+          </h2>
+          <div className="flex flex-col items-center justify-center md:items-start md:justify-start gap-6">
+            <h3 className="text-[#737B7D] text-base font-normal max-w-2xl">{exploreContent}</h3>
+            <Image
+              src="/logo/explore/explore_logo.svg"
+              alt="explore logo"
+              width={280}
+              height={280}
+            />
+            <div className="mt-16">
+              <Image
+                src="/logo/explore/explore_button.svg"
+                alt="explore button"
+                width={360}
+                height={60}
+                className="hover:cursor-pointer overflow-hidden hover:opacity-80 transition-all"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* EXPLORE CARDS */}
+        <div className="flex flex-wrap justify-center gap-4">
+          {explores.map((explore) => (
+            <div className="bg-[#BFC9C6] rounded-2xl border border-1 border-black shadow-lg p-6 w-full md:w-60 md:h-85 flex flex-col items-start justify-center gap-1">
+              <div className="flex items-center justify-end w-full">
+                <Image src={explore.logo} alt={explore.title} width={100} height={100} />
+              </div>
+              <h4 className="text-[#413434] font-bold text-2xl">{explore.title}</h4>
+              <h4 className="text-[#413434] text-sm">{explore.description}</h4>
+            </div>
+          ))}
         </div>
       </div>
 
