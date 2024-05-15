af68d2a1d47927dfb0d1b3a04a62b969c0e8545a
TwilightLogic <twilightlogic@gmail.com>
2024-05-15 23:41:22 +0800

    feat: add coming-soon.tsx component

--- rooch-portal-v1/src/pages/apps/apps-layout.tsx
+++ rooch-portal-v1/src/pages/apps/apps-layout.tsx
@@ -1,78 +1,85 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
+import { useState } from 'react'
 import { AppsItem } from './components/apps-item'
+import { ComingSoon } from '@/components/coming-soon'
 
-const mockApps = [
-  {
-    id: 1,
-    name: 'App One',
-    description: 'Description for App One.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
-    logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
-    type: 'Tag',
-  },
-  {
-    id: 2,
-    name: 'App Two',
-    description: 'Description for App Two.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreib5gpyab5fipyk7mvs3sbbcophl2gwpldoal3mt7hwzxgbu6pdjpq.png',
-    logoUrl: 'https://cdn.lxdao.io/bafkreifmpi4vszs4zqvm25us2omgpfr6gkxmc7cwvmle6xph6d5axsm4jm.png',
-    type: 'Bridge',
-  },
-  {
-    id: 3,
-    name: 'App One',
-    description: 'Description for App One.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
-    logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
-    type: 'Game',
-  },
-  {
-    id: 4,
-    name: 'App One',
-    description: 'Description for App One.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
-    logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
-    type: 'Tag',
-  },
-  {
-    id: 5,
-    name: 'App Two',
-    description: 'Description for App Two.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreib5gpyab5fipyk7mvs3sbbcophl2gwpldoal3mt7hwzxgbu6pdjpq.png',
-    logoUrl: 'https://cdn.lxdao.io/bafkreifmpi4vszs4zqvm25us2omgpfr6gkxmc7cwvmle6xph6d5axsm4jm.png',
-    type: 'Bridge',
-  },
-  {
-    id: 6,
-    name: 'App One',
-    description: 'Description for App One.',
-    profileUrl:
-      'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
-    logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
-    type: 'Game',
-  },
+interface App {
+  id: number
+  name: string
+  description: string
+  profileUrl: string
+  logoUrl: string
+  type: string
+}
+
+const mockApps: App[] = [
+  // {
+  //   id: 1,
+  //   name: 'App One',
+  //   description: 'Description for App One.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
+  //   type: 'Tag',
+  // },
+  // {
+  //   id: 2,
+  //   name: 'App Two',
+  //   description: 'Description for App Two.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreib5gpyab5fipyk7mvs3sbbcophl2gwpldoal3mt7hwzxgbu6pdjpq.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafkreifmpi4vszs4zqvm25us2omgpfr6gkxmc7cwvmle6xph6d5axsm4jm.png',
+  //   type: 'Bridge',
+  // },
+  // {
+  //   id: 3,
+  //   name: 'App One',
+  //   description: 'Description for App One.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
+  //   type: 'Game',
+  // },
+  // {
+  //   id: 4,
+  //   name: 'App One',
+  //   description: 'Description for App One.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
+  //   type: 'Tag',
+  // },
+  // {
+  //   id: 5,
+  //   name: 'App Two',
+  //   description: 'Description for App Two.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreib5gpyab5fipyk7mvs3sbbcophl2gwpldoal3mt7hwzxgbu6pdjpq.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafkreifmpi4vszs4zqvm25us2omgpfr6gkxmc7cwvmle6xph6d5axsm4jm.png',
+  //   type: 'Bridge',
+  // },
+  // {
+  //   id: 6,
+  //   name: 'App One',
+  //   description: 'Description for App One.',
+  //   profileUrl:
+  //     'https://cdn.lxdao.io/bafkreig3psglqxqiejrcokqwcoucbv4i2nkp4rumqawok2vjvhey5ps63i.png',
+  //   logoUrl: 'https://cdn.lxdao.io/bafybeibietdc7lxki2jeggdu5namnyisuujhgej2zsq26nn7orn2cngm6y.png',
+  //   type: 'Game',
+  // },
 ]
 
 export const AppsLayout = () => {
-  return (
-    <div className="h-full flex-1 flex-col space-y-6 flex rounded-lg md:shadow-custom md:p-4 md:dark:shadow-muted">
-      <div className="flex items-center justify-between space-y-2">
-        <span>
-          <h1 className="text-3xl font-bold tracking-tight">Apps</h1>
-          <p className="text-muted-foreground text-wrap">
-            Explore a variety of apps supported by Rooch, enhancing your Bitcoin Layer-2 experience.
-          </p>
-        </span>
-      </div>
-      {/* AppItem */}
+  const [apps] = useState<App[]>(mockApps)
+
+  const renderContent = () => {
+    if (apps.length === 0) {
+      return <ComingSoon />
+    }
+    return (
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full place-items-center">
-        {mockApps.map((app) => (
+        {apps.map((app) => (
           <AppsItem
             key={app.id}
             id={app.id}
@@ -84,6 +91,20 @@ export const AppsLayout = () => {
           />
         ))}
       </div>
+    )
+  }
+
+  return (
+    <div className="h-full flex-1 flex-col space-y-6 flex rounded-lg md:shadow-custom md:p-4 md:dark:shadow-muted">
+      <div className="flex items-center justify-between space-y-2">
+        <span>
+          <h1 className="text-3xl font-bold tracking-tight">Apps</h1>
+          <p className="text-muted-foreground text-wrap">
+            Explore a variety of apps supported by Rooch, enhancing your Bitcoin Layer-2 experience.
+          </p>
+        </span>
+      </div>
+      {renderContent()}
     </div>
   )
 }
--- rooch-portal-v1/src/pages/mint/components/sft-tabs.tsx
+++ rooch-portal-v1/src/pages/mint/components/sft-tabs.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { FeaturedSfts } from './featured-sfts'
 import { Tokens } from './tokens'
 import { TabItem } from '@/common/interface'
-import { AlertCircle } from 'lucide-react'
+import { ComingSoon } from '@/components/coming-soon.tsx'
 
 const mintTabItems: TabItem[] = [
   { id: 'allTokens', label: 'All Tokens', available: false },
@@ -20,15 +20,7 @@ export const SftTabs = () => {
     }
   }
 
-  const renderComingSoon = () => (
-    <div className="flex flex-col items-center justify-center text-center text-xl text-muted-foreground mt-10 animate-pulse">
-      <AlertCircle className="w-12 h-12 mb-4 text-blue-500" />
-      <p className="mb-2 font-semibold">Coming Soon!</p>
-      <p className="text-base text-gray-500">
-        We're working hard to bring this feature to you. Stay tuned!
-      </p>
-    </div>
-  )
+  const renderComingSoon = () => <ComingSoon />
 
   const renderTabContent = () => {
     const activeTab = mintTabItems.find((item) => item.id === activeId)
