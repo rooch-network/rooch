a637bda0b648e4ba15bd1ed61a36122b7b7efb7c
jolestar <jolestar@gmail.com>
2024-05-18 13:39:41 +0800

    [framework] Refactor the error_mapping generator and binary include (#1701)

--- frameworks/framework-builder/src/stdlib_configs.rs
+++ frameworks/framework-builder/src/stdlib_configs.rs
@@ -1,7 +1,9 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use crate::{path_in_crate, Stdlib, StdlibBuildConfig};
+use std::fs;
+
+use crate::{path_in_crate, release_dir, stdlib_version::StdlibVersion, Stdlib, StdlibBuildConfig};
 use anyhow::Result;
 use move_package::BuildConfig;
 use once_cell::sync::Lazy;
@@ -25,11 +27,14 @@ static STDLIB_BUILD_CONFIGS: Lazy<Vec<StdlibBuildConfig>> = Lazy::new(|| {
         .canonicalize()
         .expect("canonicalize path failed");
 
+    let latest_version_dir = release_dir().join(StdlibVersion::Latest.to_string());
+    fs::create_dir_all(&latest_version_dir).expect("create latest failed");
     vec![
         StdlibBuildConfig {
             path: move_stdlib_path.clone(),
             error_prefix: "E".to_string(),
-            error_code_map_output_file: move_stdlib_path.join("error_description.errmap"),
+            error_code_map_output_file: latest_version_dir
+                .join("move_stdlib_error_description.errmap"),
             document_template: move_stdlib_path.join("doc_template/README.md"),
             document_output_directory: move_stdlib_path.join("doc"),
             build_config: BuildConfig::default(),
@@ -38,7 +43,8 @@ static STDLIB_BUILD_CONFIGS: Lazy<Vec<StdlibBuildConfig>> = Lazy::new(|| {
         StdlibBuildConfig {
             path: moveos_stdlib_path.clone(),
             error_prefix: "Error".to_string(),
-            error_code_map_output_file: moveos_stdlib_path.join("error_description.errmap"),
+            error_code_map_output_file: latest_version_dir
+                .join("moveos_stdlib_error_description.errmap"),
             document_template: moveos_stdlib_path.join("doc_template/README.md"),
             document_output_directory: moveos_stdlib_path.join("doc"),
             build_config: BuildConfig::default(),
@@ -47,7 +53,8 @@ static STDLIB_BUILD_CONFIGS: Lazy<Vec<StdlibBuildConfig>> = Lazy::new(|| {
         StdlibBuildConfig {
             path: rooch_framework_path.clone(),
             error_prefix: "Error".to_string(),
-            error_code_map_output_file: rooch_framework_path.join("error_description.errmap"),
+            error_code_map_output_file: latest_version_dir
+                .join("rooch_framework_error_description.errmap"),
             document_template: rooch_framework_path.join("doc_template/README.md"),
             document_output_directory: rooch_framework_path.join("doc"),
             build_config: BuildConfig::default(),
@@ -56,7 +63,8 @@ static STDLIB_BUILD_CONFIGS: Lazy<Vec<StdlibBuildConfig>> = Lazy::new(|| {
         StdlibBuildConfig {
             path: bitcoin_move_path.clone(),
             error_prefix: "Error".to_string(),
-            error_code_map_output_file: bitcoin_move_path.join("error_description.errmap"),
+            error_code_map_output_file: latest_version_dir
+                .join("bitcoin_move_error_description.errmap"),
             document_template: bitcoin_move_path.join("doc_template/README.md"),
             document_output_directory: bitcoin_move_path.join("doc"),
             build_config: BuildConfig::default(),
@@ -65,7 +73,8 @@ static STDLIB_BUILD_CONFIGS: Lazy<Vec<StdlibBuildConfig>> = Lazy::new(|| {
         StdlibBuildConfig {
             path: rooch_nursery_path.clone(),
             error_prefix: "Error".to_string(),
-            error_code_map_output_file: rooch_nursery_path.join("error_description.errmap"),
+            error_code_map_output_file: latest_version_dir
+                .join("rooch_nursery_error_description.errmap"),
             document_template: rooch_nursery_path.join("doc_template/README.md"),
             document_output_directory: rooch_nursery_path.join("doc"),
             build_config: BuildConfig::default(),
