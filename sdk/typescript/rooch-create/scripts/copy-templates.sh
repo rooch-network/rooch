4b1a4308523518626d15da0f4cc323ceb93b72cb
sven Tan <sven.tan@icloud.com>
2024-05-27 03:04:20 +0800

    Refactor create-rooch & update templates

--- sdk/typescript/rooch-create/scripts/copy-templates.sh
+++ sdk/typescript/rooch-create/scripts/copy-templates.sh
@@ -2,20 +2,48 @@
 # Copyright (c) RoochNetwork
 # SPDX-License-Identifier: Apache-2.0
 
-#!/usr/bin/env bash
-# Copyright (c) RoochNetwork
-# SPDX-License-Identifier: Apache-2.0
-
 # Find git-aware template files (ignores things like node_modules, etc.)
 # and copy them to dist/templates
 git ls-files ../templates/ | rsync --files-from=- ../ dist
 
-# Replace all Rooch package links with mustache placeholder used by create-create-app
-# that will be replaced with the latest Rooch version number when the template is used
+# Function to get the latest version from NPM
+get_latest_version() {
+  local package_name=$1
+  npm show "$package_name" version
+}
+
+# Find all package.json files
 find ./dist/templates/* -name "package.json" -type f | while read -r file; do
   echo "Before replacement in $file:"
   cat "$file"
-  perl -pi -e 's|"(?=@roochnetwork)([^"]+)":\s*"link:[^"]+"|"\1": "{{rooch-version}}"|g' "$file"
+
+  # Read the package.json content
+  content=$(cat "$file")
+
+  # Extract all @roochnetwork packages with link: or workspace: versions
+  packages=$(echo "$content" | jq -r '
+    .dependencies // {} | to_entries |
+    map(select(.key | test("^@roochnetwork/")) | select(.value | test("^link:|^workspace:"))) |
+    .[].key
+  ')
+
+  if [ -z "$packages" ]; then
+    echo "No @roochnetwork packages to update."
+    continue
+  fi
+
+  updated_content=$content
+
+  # Iterate over each package to update its version
+  for package in $packages; do
+    latest_version=$(get_latest_version "$package")
+    updated_content=$(echo "$updated_content" | jq --arg package "$package" --arg version "$latest_version" '
+      .dependencies[$package] = $version
+    ')
+  done
+
+  echo "$updated_content" > "$file"
+
   echo "After replacement in $file:"
   cat "$file"
   echo
---
+++ sdk/typescript/templates/react-counter/src/App.tsx
@@ -0,0 +1,163 @@
+import {Box, Button, Container, Flex, Heading, Text} from "@radix-ui/themes";
+import {
+  SupportChain,
+  useCurrentSession,
+  useWallets,
+  useWalletStore,
+  useRoochClientQuery, useConnectWallet, useCurrentAccount, useCreateSessionKey
+} from "@roochnetwork/rooch-sdk-kit";
+import {useState} from "react";
+
+// Your publish counter contract address
+const devCounterAddress = "0xb7ac336861ff431cf867400a1eaa9708b5666954b5268486e6bd2e948aab1a42"
+const devCounterModule = `${devCounterAddress}::counter`
+
+function App() {
+  const account = useCurrentAccount();
+  const sessionKey = useCurrentSession();
+  const connectionStatus = useWalletStore((state) => state.connectionStatus)
+  const wallets = useWallets().filter((wallet) => wallet.getChain() === SupportChain.BITCOIN)
+  const [loading, setLoading] = useState(false)
+  const [sessionLoading, setSessionLoading] = useState(false)
+  const {mutateAsync: connectWallet} = useConnectWallet()
+  const {mutateAsync: createSessionKey} = useCreateSessionKey()
+
+  let {data, error, isPending, refetch} = useRoochClientQuery("executeViewFunction", {
+    funcId: `${devCounterModule}::value`,
+  })
+
+  const handlerCreateSessionKey = () => {
+    if (sessionLoading) {
+      return
+    }
+    setSessionLoading(true)
+
+    const defaultScopes = [
+      `${devCounterAddress}::*::*`,
+    ]
+    createSessionKey(
+      {
+        appName: "rooch_test",
+        appUrl: "https://test.com",
+        maxInactiveInterval: 1000,
+        scopes: defaultScopes
+      },
+      {
+        onSuccess: (result) => {
+          console.log("session key", result);
+        },
+        onError: (why) => {
+          console.log(why)
+        }
+      },
+    ).finally(() => setSessionLoading(false))
+  }
+
+  const handlerIncrease = () => {
+    if (loading) {
+      return
+    }
+
+    setLoading(true)
+
+    const func = `${devCounterModule}::increase`
+
+    sessionKey?.sendTransaction(func, [], []).finally(async () => {
+      await refetch()
+      setLoading(false)
+    })
+  }
+
+  return (
+    <>
+      <Flex
+        position="sticky"
+        px="4"
+        py="2"
+        justify="between"
+        style={{
+          borderBottom: "1px solid var(--gray-a2)"
+        }}
+      >
+        <Box>
+          <Heading>dApp Counter Template</Heading>
+        </Box>
+
+        {wallets.length === 0
+          ? "Please install the wallet and try again"
+          : connectionStatus !== "disconnected"
+            ? connectionStatus
+            : (
+              <Box>
+                <Button
+                  onClick={async () => {
+                    await connectWallet({
+                      wallet: wallets[0],
+                    });
+                  }}>
+                  Connect Wallet
+                </Button>
+              </Box>
+            )
+        }
+      </Flex>
+
+      <Container
+        mt="5"
+        pt="2"
+        px="4"
+        style={{background: "var(--gray-a2)", minHeight: 500}}
+      >
+        <Box mt="2">
+          <Text style={{fontWeight: "bold"}}>Address: </Text>
+          <Text style={{wordWrap: "break-word"}}>{account?.address}</Text>
+        </Box>
+
+        <Box mt="4">
+          <Text style={{fontWeight: "bold"}}>PublicKey: </Text>
+          <Text style={{wordWrap: "break-word"}}>{account?.publicKey}</Text>
+        </Box>
+
+        <Box mt="4">
+          <Text style={{fontWeight: "bold"}}>Compressed PublicKey: </Text>
+          <Text style={{wordWrap: "break-word"}}>{account?.compressedPublicKey}</Text>
+        </Box>
+
+        <Box mt="4">
+          <Text style={{fontWeight: "bold"}}>Session Account Address: </Text>
+          <Text style={{wordWrap: "break-word"}}>{sessionKey?.getAddress()}</Text>
+        </Box>
+
+        <Heading size="3" mt="6">{sessionKey ? "Counter" : "Create session key"}</Heading>
+
+        {devCounterAddress.length !== 0 ?
+          <Flex direction="column" gap="2">
+            {sessionKey ? (
+              <Text>
+                {isPending ? "loading..." : error ? "counter module not published" : `${data?.return_values?.[0]?.decoded_value}`}
+              </Text>
+            ) : null}
+            <Flex direction="row" gap="2" mt="2">
+              {
+                <Button
+                  disabled={loading || sessionLoading}
+                  onClick={sessionKey ? handlerIncrease : handlerCreateSessionKey}
+                >
+                  {sessionKey ? "Increment" : "Create"}
+                </Button>
+              }
+            </Flex>
+          </Flex>
+          : <><Box>
+            <Text>Please refer to the contract published by readme before trying again.</Text>
+          </Box>
+            <Text>If you have published a contract, enter the contract address correctly into devCounterAddress.</Text>
+          </>
+
+        }
+      </Container>
+    </>
+  );
+}
+
+export default App;
