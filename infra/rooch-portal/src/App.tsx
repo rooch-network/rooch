9bdc43e4c00e004dfd7477410b7fa45ae905366c
TwilightLogic <twilightlogic@gmail.com>
2024-05-27 16:04:29 +0800

    feat: 'complete the `Leap` component and `Trade` component'

--- infra/rooch-portal/src/App.tsx
+++ infra/rooch-portal/src/App.tsx
@@ -8,7 +8,7 @@ import { createEmotionCache } from '@/utils/create-emotion-cache'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-import { TestNetwork } from '@roochnetwork/rooch-sdk'
+import { LocalNetwork } from '@roochnetwork/rooch-sdk'
 import { WalletProvider, RoochClientProvider, SupportChain } from '@roochnetwork/rooch-sdk-kit'
 
 import { DashboardLayout } from '@/pages/dashboard-layout'
@@ -24,18 +24,23 @@ function App() {
     <>
       <CacheProvider value={clientSideEmotionCache}>
         <QueryClientProvider client={queryClient}>
-          <RoochClientProvider network={TestNetwork}>
-            <WalletProvider chain={SupportChain.BITCOIN} autoConnect fallback={
-              <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-zinc-900 to-zinc-800">
-                <div className="text-center">
-                  <div className="flex items-center justify-center mb-6">
-                    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+          <RoochClientProvider network={LocalNetwork}>
+            <WalletProvider
+              chain={SupportChain.BITCOIN}
+              autoConnect
+              fallback={
+                <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-zinc-900 to-zinc-800">
+                  <div className="text-center">
+                    <div className="flex items-center justify-center mb-6">
+                      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+                    </div>
+                    <h3 className="text-white text-2xl font-semibold">Loading data...</h3>
+                    <p className="text-gray-400 mt-2">
+                      Please wait a moment while we fetch your data.
+                    </p>
                   </div>
-                  <h3 className="text-white text-2xl font-semibold">Loading data...</h3>
-                  <p className="text-gray-400 mt-2">Please wait a moment while we fetch your data.</p>
                 </div>
-              </div>
-            }
+              }
             >
               <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
                 <ToastProvider />
