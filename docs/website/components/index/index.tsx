957d79bad185ef1328191df0539adec4169da865
TwilightLogic <twilightlogic@gmail.com>
2024-05-30 17:37:39 +0800

    refactor: complete the hero page

--- docs/website/components/index/index.tsx
+++ docs/website/components/index/index.tsx
@@ -116,35 +116,40 @@ const Index = ({
     <>
       <div className="antialiased">
         {/* HERO */}
-        <div className="flex flex-col items-center justify-center h-[85vh] px-4 sm:px-6 md:px-8 lg:px-20 dark:border-b dark:border-b-zinc-800">
-          <div className="mt-5 max-w-3xl text-center mx-auto">
-            <p className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
-              {heroTitle}
-            </p>
-            {heroSlogan ? (
-              <p className="block pt-2 font-bold text-gray-800 text-2xl md:text-3xl lg:text-4xl dark:text-gray-200">
-                {heroSlogan}
+        <div className="flex flex-col md:flex-row items-center justify-center h-[90vh] px-4 sm:px-6 md:px-8 lg:px-20 dark:border-b dark:border-b-zinc-800">
+          <div className="flex flex-col items-center justify-center">
+            <div className="mt-5 max-w-3xl text-center mx-auto">
+              <p className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl dark:text-gray-200">
+                {heroTitle}
               </p>
-            ) : null}
+              {heroSlogan ? (
+                <p className="block pt-2 font-bold text-gray-800 text-2xl md:text-3xl lg:text-4xl dark:text-gray-200">
+                  {heroSlogan}
+                </p>
+              ) : null}
+            </div>
+            <div className="mt-5 max-w-2xl text-center mx-auto">
+              <p className="text-lg text-gray-600 dark:text-gray-400">{heroDescription}</p>
+            </div>
+            <div className="mt-8 grid gap-3 w-full sm:inline-flex sm:justify-center cta-container">
+              <Link
+                className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl border border-transparent text-sm font-medium rounded-md py-3 px-6 cta"
+                href={heroButtonHref}
+              >
+                {heroButton}
+                <svg className="w-3 h-3" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                  <path
+                    d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                  />
+                </svg>
+              </Link>
+            </div>
           </div>
-          <div className="mt-5 max-w-2xl text-center mx-auto">
-            <p className="text-lg text-gray-600 dark:text-gray-400">{heroDescription}</p>
-          </div>
-          <div className="mt-8 grid gap-3 w-full sm:inline-flex sm:justify-center cta-container">
-            <Link
-              className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl border border-transparent text-sm font-medium rounded-md py-3 px-6 cta"
-              href={heroButtonHref}
-            >
-              {heroButton}
-              <svg className="w-3 h-3" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                <path
-                  d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-              </svg>
-            </Link>
+          <div className="md:w-3/5 w-full mt-4 md:mt-0">
+            <img src="/hero/hero.svg" alt="hero" />
           </div>
         </div>
 
