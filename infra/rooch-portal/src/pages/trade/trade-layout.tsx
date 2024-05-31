4f8f99b448d3d38d666d0ff93cddeca7360b371d
TwilightLogic <82878302+TwilightLogic@users.noreply.github.com>
2024-05-31 20:00:22 +0800

    feat: portal-improvement-week1 (#1748)
    * feat: 'complete the `Leap` component and `Trade` component'
    
    * feat: update UI for manage-session.tssx
    
    * feat: automatically Clear the expired session keys
    
    * feat: 'complete the `Leap` component and `Trade` component'
    
    * feat: add validation check on transfering coins
    
    * feat: add loading on transfering coins
    
    * feat: add success UI on transfering coins
    
    * feat: validate the address on transfering coins
    
    * feat: update some description on assets coin
    
    * refactor: use testnent on Rooch client

--- infra/rooch-portal/src/pages/trade/trade-layout.tsx
+++ infra/rooch-portal/src/pages/trade/trade-layout.tsx
@@ -1,5 +1,19 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
+import { ComingSoon } from '@/components/coming-soon.tsx'
+
 export const TradeLayout = () => {
-  return <div>Trade Layout</div>
+  return (
+    <div className="h-full flex-1 flex-col space-y-6 flex rounded-lg md:shadow-custom md:p-4 md:dark:shadow-muted">
+      <div className="flex items-center justify-between space-y-2">
+        <span>
+          <h1 className="text-3xl font-bold tracking-tight">Leap</h1>
+          <p className="text-muted-foreground text-wrap">
+            Explore a variety of apps supported by Rooch, enhancing your Bitcoin Layer-2 experience.
+          </p>
+        </span>
+      </div>
+      <ComingSoon />
+    </div>
+  )
 }
