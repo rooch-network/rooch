7a634aeca77e7c2cc6cae6a18eb35b4e2ef01182
jolestar <jolestar@gmail.com>
2024-06-01 02:16:28 +0800

    [frameworks]fix test and release v1

--- frameworks/framework-release/build.rs
+++ frameworks/framework-release/build.rs
@@ -5,6 +5,7 @@ use std::env::current_dir;
 
 fn main() {
     if std::env::var("SKIP_STDLIB_BUILD").is_err() {
+        std::env::set_var("RUST_LOG", "WARN");
         let _ = tracing_subscriber::fmt::try_init();
         let current_dir = current_dir().expect("Should be able to get current dir");
         // Get the project root directory
