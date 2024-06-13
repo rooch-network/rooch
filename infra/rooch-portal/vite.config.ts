8974ab0b2caa34abdca076f7915fdf3de49c5dc6
TwilightLogic <82878302+TwilightLogic@users.noreply.github.com>
2024-06-14 01:40:23 +0800

    chore: fix #1790 (#1877)
    * chore: lower case the sender address
    
    * chore: fix the `buffer` error
    
    * minor
    
    * minor
    
    * chore: vite config
    
    * chore: vite config

--- infra/rooch-portal/vite.config.ts
+++ infra/rooch-portal/vite.config.ts
@@ -3,10 +3,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
