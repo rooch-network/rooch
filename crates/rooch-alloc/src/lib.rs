c345118e34edf3ca394cdcfe2867946a2bf5da44
blackanger <blackanger.z@gmail.com>
2024-06-29 00:42:36 +0800

    feat: Add jemalloc Memory Allocator

---
+++ crates/rooch-alloc/src/lib.rs
@@ -0,0 +1,24 @@
+#[cfg(feature = "jemalloc")]
+#[macro_use]
+extern crate lazy_static;
+
+pub mod error;
+
+#[cfg(not(all(unix, not(fuzzing), feature = "jemalloc")))]
+mod default;
+
+pub type AllocStats = Vec<(&'static str, usize)>;
+
+// Allocators
+#[cfg(all(unix, not(fuzzing), feature = "jemalloc"))]
+#[path = "jemalloc.rs"]
+mod imp;
+
+#[cfg(not(all(unix, not(fuzzing), any(feature = "jemalloc"))))]
+#[path = "system.rs"]
+mod imp;
+
+pub use crate::imp::*;
+
+#[global_allocator]
+static ALLOC: imp::Allocator = imp::allocator();
