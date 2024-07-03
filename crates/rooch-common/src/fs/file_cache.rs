419a1cf975eef43b08a5384e498dc78db471d484
popcnt <ism3y@proton.me>
2024-07-04 06:55:06 +0800

    feat(common): add file cache manager
    introduces a new module, "fs", and within that module, a new "file_cache.rs" file that creates a FileCacheManager. This manager handles file caching using the std and libc libraries.
    
    Will be used in export/import jobs for releasing memory immediately

---
+++ crates/rooch-common/src/fs/file_cache.rs
@@ -0,0 +1,37 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
+use std::fs::File;
+use std::io::{Error, Result};
+use std::os::unix::io::AsRawFd;
+
+use libc::{posix_fadvise, POSIX_FADV_DONTNEED};
+
+struct FileCacheManager {
+    file: File,
+}
+
+impl FileCacheManager {
+    pub fn new(file_path: &str) -> Result<Self> {
+        let file = File::open(file_path)?;
+        Ok(FileCacheManager { file })
+    }
+
+    pub fn drop_cache_range(&self, offset: u64, len: u64) -> Result<()> {
+        let fd = self.file.as_raw_fd();
+        let ret = unsafe {
+            posix_fadvise(
+                fd,
+                offset as libc::off_t,
+                len as libc::off_t,
+                POSIX_FADV_DONTNEED,
+            )
+        };
+
+        if ret != 0 {
+            return Err(Error::from_raw_os_error(ret));
+        }
+
+        Ok(())
+    }
+}
