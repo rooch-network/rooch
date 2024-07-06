8a6d4f74d437348d6e89d3dc8a5399bc0225ac86
popcnt <ism3y@proton.me>
2024-07-07 04:26:34 +0800

    Refactor file import in file_cache.rs
    The commit updates file import in file_cache.rs by replacing individual usages with direct namespace access. These changes include the way the File and Error classes from the std library are accessed. The aim is to improve readability and maintainability.

--- crates/rooch-common/src/fs/file_cache.rs
+++ crates/rooch-common/src/fs/file_cache.rs
@@ -1,25 +1,25 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-use std::fs::File;
-use std::io::{Error, Result};
-use std::os::unix::io::AsRawFd;
+use std::io::Result;
 use std::path::PathBuf;
 
 pub struct FileCacheManager {
     #[cfg(target_os = "linux")]
-    file: File,
+    file: std::fs::File,
 }
 
 impl FileCacheManager {
     #[cfg(target_os = "linux")]
     pub fn new(file_path: PathBuf) -> Result<Self> {
-        let file = File::open(file_path)?;
+        let file = std::fs::File::open(file_path)?;
         Ok(FileCacheManager { file })
     }
 
     #[cfg(target_os = "linux")]
     pub fn drop_cache_range(&self, offset: u64, len: u64) -> Result<()> {
+        use std::os::unix::io::AsRawFd;
+
         let fd = self.file.as_raw_fd();
         let ret = unsafe {
             libc::posix_fadvise(
@@ -31,7 +31,7 @@ impl FileCacheManager {
         };
 
         if ret != 0 {
-            return Err(Error::from_raw_os_error(ret));
+            return Err(std::io::Error::from_raw_os_error(ret));
         }
 
         Ok(())
