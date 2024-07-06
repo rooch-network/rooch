436533ad3ca1716e3ae31690ed1d561cabddb3f5
popcnt <ism3y@proton.me>
2024-07-07 04:05:36 +0800

    fix(common): add Linux-specific file caching logic
    Implemented conditional logic in the FileCacheManager module where certain methods are only applicable when the target OS is Linux. Methods like `new()` and `drop_cache_range()` are now specifically designated for Linux, while others are designated otherwise. This ensures correct functioning across diverse OS.

--- crates/rooch-common/src/fs/file_cache.rs
+++ crates/rooch-common/src/fs/file_cache.rs
@@ -8,15 +8,18 @@ use std::os::unix::io::AsRawFd;
 use std::path::PathBuf;
 
 pub struct FileCacheManager {
+    #[cfg(target_os = "linux")]
     file: File,
 }
 
 impl FileCacheManager {
+    #[cfg(target_os = "linux")]
     pub fn new(file_path: PathBuf) -> Result<Self> {
         let file = File::open(file_path)?;
         Ok(FileCacheManager { file })
     }
 
+    #[cfg(target_os = "linux")]
     pub fn drop_cache_range(&self, offset: u64, len: u64) -> Result<()> {
         let fd = self.file.as_raw_fd();
         let ret = unsafe {
@@ -32,6 +35,16 @@ impl FileCacheManager {
             return Err(Error::from_raw_os_error(ret));
         }
 
+        Ok(())
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    pub fn new(_: PathBuf) -> Result<Self> {
+        Ok(FileCacheManager {})
+    }
+
+    #[cfg(not(target_os = "linux"))]
+    pub fn drop_cache_range(&self, _: u64, _: u64) -> Result<()> {
         Ok(())
     }
 }
