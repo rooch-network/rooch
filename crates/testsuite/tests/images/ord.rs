ed79dffcda3b8b773fe10d3e5baddf9ff6b52d2e
Owen Wu <yubing744@163.com>
2024-07-12 08:55:00 +0800

    feat: update ord image tag

--- crates/testsuite/tests/images/ord.rs
+++ crates/testsuite/tests/images/ord.rs
@@ -9,7 +9,7 @@ use testcontainers::{
 };
 
 const NAME: &str = "bitseed/ord";
-const TAG: &str = "0.18.0-burn-rc2";
+const TAG: &str = "0.18.0-burn";
 
 #[derive(Debug, Default, Clone)]
 pub struct OrdImageArgs {
