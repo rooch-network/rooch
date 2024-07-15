d0a9ba6dd18d3298b2945e62eba5ac6179713907
Birch <birch17261@gmail.com>
2024-07-15 19:30:37 +0800

    update sdk-kit: use enums for SupportChain and SupportWallet

--- sdk/typescript/rooch-sdk-kit/src/feature/index.ts
+++ sdk/typescript/rooch-sdk-kit/src/feature/index.ts
@@ -1,5 +1,14 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-export type SupportChain = 'rooch' | 'bitcoin'
-export type SupportWallet = 'unisat' | 'okx' | 'onekey' //| 'onekey-hardware'
+export enum SupportChain {
+    Rooch = 'rooch',
+    Bitcoin = 'bitcoin'
+}
+
+export enum SupportWallet {
+    Unisat = 'unisat',
+    Okx = 'okx',
+    Onekey = 'onekey'
+    // OnekeyHardware = 'onekey-hardware'
+}
