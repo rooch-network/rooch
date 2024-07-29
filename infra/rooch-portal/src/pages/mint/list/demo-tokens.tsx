fdcdc5a45f3f5364da72f05c8ff5e6d5057c4f26
sven Tan <sven.tan@icloud.com>
2024-07-29 15:45:43 +0800

    add google analysis & fix utxo hint  (#2304)
    * pptimize portal
    
    * error hint
    
    * add google

--- infra/rooch-portal/src/pages/mint/list/demo-tokens.tsx
+++ infra/rooch-portal/src/pages/mint/list/demo-tokens.tsx
@@ -1,6 +1,10 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
+import { useEffect, useState } from 'react'
+import { MousePointer2 } from 'lucide-react'
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+
 import {
   Table,
   TableBody,
@@ -11,13 +15,10 @@ import {
 } from '@/components/ui/table'
 import { Progress } from '@/components/ui/progress'
 import { Button } from '@/components/ui/button'
-import { MousePointer2 } from 'lucide-react'
 import { useNetworkVariable } from '@/networks'
 import { useRoochClient } from '@roochnetwork/rooch-sdk-kit'
-import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getTokenInfo, TokenInfo } from '@/pages/mint/util/get-token-info'
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 
 export const DemoTokens = () => {
   const navigate = useNavigate()
@@ -27,7 +28,6 @@ export const DemoTokens = () => {
   const addresses = useNetworkVariable('mintAddress')
 
   useEffect(() => {
-
     let tokens: TokenInfo[] = []
 
     addresses.forEach((item) => {
@@ -38,7 +38,6 @@ export const DemoTokens = () => {
         }
       })
     })
-
   }, [client, addresses])
 
   return (
@@ -47,44 +46,58 @@ export const DemoTokens = () => {
         <Table>
           <TableHeader>
             <TableRow>
-              {tokens.length > 0 ? <>
-                <TableHead className="w-[150px]">Symbol</TableHead>
-                <TableHead className="w-[150px]">Name</TableHead>
-                <TableHead className="w-[200px]">Distribution Mechanism</TableHead>
-                <TableHead>Progress</TableHead>
-                <TableHead className="text-center w-[250px]" >Action</TableHead>
-              </> : <TableHead className="w-full"><Skeleton/></TableHead>}
+              {tokens.length > 0 ? (
+                <>
+                  <TableHead className="w-[150px]">Symbol</TableHead>
+                  <TableHead className="w-[150px]">Name</TableHead>
+                  <TableHead className="w-[200px]">Distribution Mechanism</TableHead>
+                  <TableHead>Progress</TableHead>
+                  <TableHead className="text-center w-[250px]">Action</TableHead>
+                </>
+              ) : (
+                <TableHead className="w-full">
+                  <Skeleton />
+                </TableHead>
+              )}
             </TableRow>
           </TableHeader>
-            <TableBody>
-              {
-                tokens.length > 0 ? tokens.map((token) => (
-                  <TableRow key={token.starTime}>
-                    <TableCell className="font-medium">{token.coin.symbol}</TableCell>
-                    <TableCell className="font-medium">{token.coin.name}</TableCell>
-                    <TableCell className="font-medium">Self-Staking (without Lock)</TableCell>
-                    <TableCell>
-                      <div className="flex items-center justify-start gap-1">
-                        <Progress value={token.progress} className="w-[60%]" />
-                        <span>{token.progress}%</span>
-                      </div>
-                    </TableCell>
-                    <TableCell className="text-center">
-                      <Button variant="link" size="sm" onClick={() => {
+          <TableBody>
+            {tokens.length > 0 ? (
+              tokens.map((token) => (
+                <TableRow key={token.starTime}>
+                  <TableCell className="font-medium">{token.coin.symbol}</TableCell>
+                  <TableCell className="font-medium">{token.coin.name}</TableCell>
+                  <TableCell className="font-medium">Self-Staking (without Lock)</TableCell>
+                  <TableCell>
+                    <div className="flex items-center justify-start gap-1">
+                      <Progress value={token.progress} className="w-[60%]" />
+                      <span>{token.progress}%</span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-center">
+                    <Button
+                      variant="link"
+                      size="sm"
+                      onClick={() => {
                         navigate(`/mint/detail/${token.address}`)
-                      }}>
-                  <span className="flex items-center justify-center">
-                    <MousePointer2 className="w-4 h-4 mr-1" />
-                    Mint
-                  </span>
-                      </Button>
-                    </TableCell>
-                  </TableRow>
-                )): <TableRow key='loading'>
-                  <TableCell className="font-medium w-full"><Skeleton/></TableCell>
+                      }}
+                    >
+                      <span className="flex items-center justify-center">
+                        <MousePointer2 className="w-4 h-4 mr-1" />
+                        Mint
+                      </span>
+                    </Button>
+                  </TableCell>
                 </TableRow>
-              }
-            </TableBody>
+              ))
+            ) : (
+              <TableRow key="loading">
+                <TableCell className="font-medium w-full">
+                  <Skeleton />
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
         </Table>
       </SkeletonTheme>
     </div>
