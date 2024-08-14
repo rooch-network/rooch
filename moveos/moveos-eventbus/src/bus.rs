93c2ed79968198ee3dc0c0c34f674efef8dfe07a
steelgeek091 <steelgeek091@gmail.com>
2024-08-14 15:52:32 +0800

    add license info

--- moveos/moveos-eventbus/src/bus.rs
+++ moveos/moveos-eventbus/src/bus.rs
@@ -1,3 +1,6 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 use crossbeam_channel::{unbounded, Receiver, Sender};
 use std::any::{Any, TypeId};
 use std::collections::HashMap;
