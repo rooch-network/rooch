87bea6ae5b370f979fd88dde2685d32f9544751d
jolestar <jolestar@gmail.com>
2024-08-14 18:16:01 +0800

    [test] Add tests to bitcoin multisign validator

---
+++ crates/rooch-framework-tests/src/tests/bitcoin_multisign_validator_tests.rs
@@ -0,0 +1,88 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::binding_test;
+use moveos_types::module_binding::MoveFunctionCaller;
+use moveos_types::transaction::{MoveAction, MoveOSTransaction};
+use rooch_types::crypto::{RoochKeyPair, RoochSignature};
+use rooch_types::framework::auth_payload::SignData;
+use rooch_types::framework::auth_validator::BuiltinAuthValidator;
+use rooch_types::framework::empty::Empty;
+use rooch_types::nursery::bitcoin_multisign_validator::{
+    AuthPayload, BitcoinMultisignValidatorModule,
+};
+use rooch_types::nursery::multisign_account::{self, MultisignAccountModule};
+use rooch_types::transaction::rooch::RoochTransactionData;
+use rooch_types::transaction::{Authenticator, RoochTransaction};
+
+#[test]
+fn test_validate() {
+    let mut binding_test = binding_test::RustBindingTest::new().unwrap();
+    let root = binding_test.root().clone();
+
+    let kp1 = RoochKeyPair::generate_secp256k1();
+    let kp2 = RoochKeyPair::generate_secp256k1();
+    let kp3 = RoochKeyPair::generate_secp256k1();
+
+    let u1 = kp1.public().bitcoin_address().unwrap().to_rooch_address();
+    //let u2 = kp2.public().bitcoin_address().unwrap().to_rooch_address();
+    //let u3 = kp3.public().bitcoin_address().unwrap().to_rooch_address();
+
+    let pubkeys = vec![
+        kp1.bitcoin_public_key().unwrap(),
+        kp2.bitcoin_public_key().unwrap(),
+        kp3.bitcoin_public_key().unwrap(),
+    ];
+
+    let pubkeys = pubkeys
+        .into_iter()
+        .map(|pk| pk.to_bytes())
+        .collect::<Vec<_>>();
+
+    let bitcoin_address_from_rust =
+        multisign_account::generate_multisign_address(2, pubkeys.clone()).unwrap();
+    //println!("bitcoin_address_from_rust: {}", bitcoin_address_from_rust);
+
+    //Initialize the multisign account
+    let action = MultisignAccountModule::initialize_multisig_account_action(2, pubkeys.to_vec());
+    let tx_data = RoochTransactionData::new_for_test(u1, 0, action);
+    let tx = tx_data.sign(&kp1);
+    binding_test.execute(tx).unwrap();
+
+    let sender = bitcoin_address_from_rust.to_rooch_address();
+    let sequence_number = 0;
+    let action = MoveAction::new_function_call(Empty::empty_function_id(), vec![], vec![]);
+    let tx_data = RoochTransactionData::new_for_test(sender, sequence_number, action);
+
+    let sign_data = SignData::new_with_default(&tx_data);
+    let data_hash = sign_data.data_hash();
+
+    let signature1 = kp1.sign(data_hash.as_bytes());
+    let signature2 = kp2.sign(data_hash.as_bytes());
+
+    let message_info = sign_data.message_info_without_tx_hash();
+    let payload = AuthPayload {
+        signatures: vec![
+            signature1.signature_bytes().to_vec(),
+            signature2.signature_bytes().to_vec(),
+        ],
+        message_prefix: sign_data.message_prefix,
+        message_info,
+        public_keys: pubkeys[0..2].to_vec(),
+    };
+
+    let authenticator = Authenticator::new(
+        BuiltinAuthValidator::BitcoinMultisign.flag().into(),
+        bcs::to_bytes(&payload).unwrap(),
+    );
+
+    let tx = RoochTransaction::new(tx_data, authenticator);
+
+    let auth_info = tx.authenticator_info();
+
+    let move_tx: MoveOSTransaction = tx.into_moveos_transaction(root);
+
+    let validator_caller = binding_test.as_module_binding::<BitcoinMultisignValidatorModule>();
+    let result = validator_caller.validate(&move_tx.ctx, auth_info.authenticator.payload);
+    assert!(result.is_ok());
+}
---
+++ crates/rooch-types/src/nursery/bitcoin_multisign_validator.rs
@@ -0,0 +1,77 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
+use crate::framework::auth_validator::BuiltinAuthValidator;
+use anyhow::Result;
+use framework_types::addresses::ROOCH_NURSERY_ADDRESS;
+use move_core_types::{
+    account_address::AccountAddress, ident_str, identifier::IdentStr, value::MoveValue,
+};
+use moveos_types::{
+    module_binding::{ModuleBinding, MoveFunctionCaller},
+    moveos_std::tx_context::TxContext,
+    state::MoveStructType,
+    transaction::FunctionCall,
+};
+use serde::{Deserialize, Serialize};
+
+const MODULE_NAME: &IdentStr = ident_str!("bitcoin_multisign_validator");
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct AuthPayload {
+    pub signatures: Vec<Vec<u8>>,
+    pub message_prefix: Vec<u8>,
+    pub message_info: Vec<u8>,
+    pub public_keys: Vec<Vec<u8>>,
+}
+
+/// Bitcoin Multisign Auth Validator
+pub struct BitcoinMultisignValidator {}
+
+impl BitcoinMultisignValidator {
+    pub fn auth_validator_id() -> u64 {
+        BuiltinAuthValidator::BitcoinMultisign.flag().into()
+    }
+}
+
+impl MoveStructType for BitcoinMultisignValidator {
+    const ADDRESS: AccountAddress = ROOCH_NURSERY_ADDRESS;
+    const MODULE_NAME: &'static IdentStr = MODULE_NAME;
+    const STRUCT_NAME: &'static IdentStr = ident_str!("BitcoinMultisignValidator");
+}
+
+/// Rust bindings for RoochFramework bitcoin_validator module
+pub struct BitcoinMultisignValidatorModule<'a> {
+    caller: &'a dyn MoveFunctionCaller,
+}
+
+impl<'a> BitcoinMultisignValidatorModule<'a> {
+    const VALIDATE_FUNCTION_NAME: &'static IdentStr = ident_str!("validate");
+
+    pub fn validate(&self, ctx: &TxContext, payload: Vec<u8>) -> Result<()> {
+        let auth_validator_call = FunctionCall::new(
+            Self::function_id(Self::VALIDATE_FUNCTION_NAME),
+            vec![],
+            vec![MoveValue::vector_u8(payload).simple_serialize().unwrap()],
+        );
+        self.caller
+            .call_function(ctx, auth_validator_call)?
+            .into_result()
+            .map(|values| {
+                debug_assert!(values.is_empty(), "should not have return values");
+            })?;
+        Ok(())
+    }
+}
+
+impl<'a> ModuleBinding<'a> for BitcoinMultisignValidatorModule<'a> {
+    const MODULE_ADDRESS: AccountAddress = ROOCH_NURSERY_ADDRESS;
+    const MODULE_NAME: &'static IdentStr = MODULE_NAME;
+
+    fn new(caller: &'a impl MoveFunctionCaller) -> Self
+    where
+        Self: Sized,
+    {
+        Self { caller }
+    }
+}
