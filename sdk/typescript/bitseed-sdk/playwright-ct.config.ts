a61d6a4e07aee70bbf49b870ccd3867722c9a8b0
Owen Wu <yubing744@163.com>
2024-08-19 22:46:49 +0800

    feat: config playwright deps

--- sdk/typescript/bitseed-sdk/playwright-ct.config.ts
+++ sdk/typescript/bitseed-sdk/playwright-ct.config.ts
@@ -59,7 +59,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -68,6 +67,5 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-    */
   ],
 })
