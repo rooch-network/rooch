9773b1bae56c1dc2b2af6a78edd30e6adb820f05
newraina <newraina@users.noreply.github.com>
2024-08-31 01:28:20 +0900

    fix: current session may be invalid (#2516)
    * remove duplicate current session variable
    
    * call `removeSession` when receives expired error
    
    * ignore expired sessions when the SDK initializes
    
    * fix `isExpired()`
    
    * optimize imports
    
    * Revert "fix `isExpired()`"
    
    This reverts commit 003d7c7cabc4dc3fa6f873b999252e2432b1cd49.
    
    * Revert "ignore expired sessions when the SDK initializes"
    
    This reverts commit 23eb5b9fb83d1b372ed696eb2fe4b3188f762381.
    
    * rename to useDefaultClient
    
    * e2e test
    
    * add back missing import
    
    * add back missing import

---
+++ sdk/typescript/rooch-sdk-kit/src/provider/useDefaultClient.ts
@@ -0,0 +1,52 @@
+import { useCallback, useMemo } from 'react'
+import { isRoochClient, RoochClient } from '@roochnetwork/rooch-sdk'
+import { useSessionStore } from '../hooks/useSessionsStore.js'
+import { NetworkConfig } from '../hooks/index.js'
+import { NetworkConfigs } from './clientProvider.js'
+import { HTTPTransport } from '../http/httpTransport.js'
+
+const DEFAULT_CREATE_CLIENT = (
+  _name: string,
+  config: NetworkConfig | RoochClient,
+  setCurrentSession: any,
+) => {
+  if (isRoochClient(config)) {
+    return config
+  }
+
+  config.transport = new HTTPTransport(
+    {
+      url: config.url!.toString(),
+    },
+    setCurrentSession,
+  )
+
+  return new RoochClient(config)
+}
+
+interface UseRoochClientParams {
+  currentNetwork: string
+  networks: NetworkConfigs
+}
+
+export function useDefaultClient(params: UseRoochClientParams) {
+  const { currentNetwork, networks } = params
+
+  const currentSession = useSessionStore((state) => state.currentSession)
+  const removeSession = useSessionStore((state) => state.removeSession)
+  const clearSession = useCallback(() => {
+    try {
+      if (currentSession) {
+        removeSession(currentSession)
+      }
+    } catch (e) {
+      console.error(e)
+    }
+  }, [removeSession, currentSession])
+
+  const client = useMemo(() => {
+    return DEFAULT_CREATE_CLIENT(currentNetwork, networks[currentNetwork], clearSession)
+  }, [currentNetwork, networks, clearSession])
+
+  return client
+}
