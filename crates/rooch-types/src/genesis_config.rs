9b427cf3bdaf3c4fa62ebe5303cf38ce820c8ec5
popcnt1 <ism3y@proton.me>
2024-09-06 01:46:31 +0800

    fix(genesis): update genesis block header

--- crates/rooch-types/src/genesis_config.rs
+++ crates/rooch-types/src/genesis_config.rs
@@ -162,8 +162,8 @@ pub static G_TEST_CONFIG: Lazy<GenesisConfig> = Lazy::new(|| {
 //curl -sSL https://mempool.space/api/block/$(curl -sSL https://mempool.space/api/block-height/859000)/header
 
 static MAIN_GENESIS_HEIGHT_HEADER: Lazy<(u64, Header)> = Lazy::new(|| {
-    (859000, bitcoin::consensus::deserialize(
-        &hex::decode("000000285e0683a9e4add89c9bead199a768bb25040fe165a41f00000000000000000000d57cb8c2ecb8f33f548772a0bffe6a156137ac63a96f11bad1c1e334ca44f444a3e8d0665b250317bc183bd0")
+    (859001, bitcoin::consensus::deserialize(
+        &hex::decode("00e0ff274e6e46285bf4133faaafcf248ed461ffcdf8e2b33fba020000000000000000004275ffbb1e17c5b8abb04a9e57bc479c83dcf44c7bed3bc7f94c8449b6c2250619ecd0665b250317b7bc8d78")
             .expect("Should be valid"),
     ).expect("Should be valid"))
 });
