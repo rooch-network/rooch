dd4ae1b440355dcc83f4397d94e09eeaa4ee6f66
sven Tan <sven.tan@icloud.com>
2024-09-05 21:14:32 +0800

    add rpc limiter & ip blocklist

--- crates/rooch-rpc-server/src/service/mod.rs
+++ crates/rooch-rpc-server/src/service/mod.rs
@@ -2,5 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod aggregate_service;
-pub mod rpc_logger;
+// pub mod rpc_logger;
+pub mod error;
 pub mod rpc_service;
+
+pub mod routing;
+
+pub mod blocklist;
