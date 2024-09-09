3cf74bb206edcf47764edcfb91a85dc8a6173ac8
newraina <newraina@users.noreply.github.com>
2024-09-09 13:52:30 +0900

    [portal] add csp rule (#2583)
    * [portal] add csp rule
    
    * migrate to CSP middleware
    
    * let emotion support nonce
    
    * ColorSchemeScript support nonce
    
    * fix eslint error
    
    * update style rule

--- infra/rooch-portal-v2/src/app/layout.tsx
+++ infra/rooch-portal-v2/src/app/layout.tsx
@@ -3,13 +3,15 @@ import '@fontsource-variable/raleway/wght.css';
 import '@fontsource-variable/plus-jakarta-sans/wght.css';
 
 import type { Viewport } from 'next';
+import { headers } from 'next/headers';
 
 import '@fontsource-variable/red-hat-mono';
 
 import { primary } from 'src/theme/core/palette';
 import { DashboardLayout } from 'src/layouts/dashboard';
 import { ThemeProvider } from 'src/theme/theme-provider';
-import { getInitColorSchemeScript } from 'src/theme/color-scheme-script';
+import { schemeConfig } from 'src/theme/color-scheme-script';
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
 
 import { Snackbar } from 'src/components/snackbar';
 import { ProgressBar } from 'src/components/progress-bar';
@@ -17,6 +19,8 @@ import { MotionLazy } from 'src/components/animate/motion-lazy';
 import { SettingsDrawer, defaultSettings, SettingsProvider } from 'src/components/settings';
 
 import RoochDappProvider from './rooch-dapp-provider';
+
+export const dynamic = 'force-dynamic';
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -32,13 +36,15 @@ export default async function RootLayout({ children }: Props) {
   // const settings = CONFIG.isStaticExport ? defaultSettings : await detectSettings();
   const settings = defaultSettings;
 
+  const nonce = headers().get('x-nonce') || '';
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        {getInitColorSchemeScript}
+        <InitColorSchemeScript {...schemeConfig} nonce={nonce} />
         <RoochDappProvider>
           <SettingsProvider settings={settings} caches="localStorage">
-            <ThemeProvider>
+            <ThemeProvider nonce={nonce}>
               <MotionLazy>
                 <Snackbar />
                 <ProgressBar />
--- infra/rooch-portal-v2/src/theme/color-scheme-script.ts
+++ infra/rooch-portal-v2/src/theme/color-scheme-script.ts
@@ -1,10 +1,6 @@
 'use client';
 
-import { getInitColorSchemeScript as _getInitColorSchemeScript } from '@mui/material/styles';
-
 export const schemeConfig = {
   modeStorageKey: 'theme-mode',
   defaultMode: 'light' as const,
 };
-
-export const getInitColorSchemeScript = _getInitColorSchemeScript(schemeConfig);
