062d9285585d0f551bb8e13cebb3db55a62eba0b
Owen Wu <yubing744@163.com>
2024-09-11 08:51:36 +0800

    feat: impl scan for MoveStorage

--- crates/rooch-cosmwasm-vm/src/backend.rs
+++ crates/rooch-cosmwasm-vm/src/backend.rs
@@ -70,6 +70,8 @@ pub struct MoveStorage<'a> {
     object: Arc<Mutex<RuntimeObject>>,
     layout_loader: &'a dyn TypeLayoutLoader,
     resolver: &'a dyn StatelessResolver,
+    iterator_id_counter: u32,
+    iterators: HashMap<u32, (Vec<(FieldKey, Vec<u8>)>, usize)>,
 }
 
 impl<'a> MoveStorage<'a> {
@@ -82,6 +84,8 @@ impl<'a> MoveStorage<'a> {
             object,
             layout_loader,
             resolver,
+            iterator_id_counter: 0,
+            iterators: HashMap::new(),
         }
     }
 
@@ -111,7 +115,7 @@ impl<'a> Storage for MoveStorage<'a>  {
         let move_layout = Vec::<u8>::type_layout();
         let move_type = Type::Vector(Box::new(Type::U8));
 
-        match object.borroww_field(self.layout_loader, self.resolver, field_key, &move_type) {
+        match object.get_field(self.layout_loader, self.resolver, field_key, &move_type) {
             Ok(value) => match self.serialize_value(&move_layout, &value.0) {
                 Ok(bytes) => (Ok(Some(bytes)), GasInfo::new(1, 0)),
                 Err(e) => (Err(e), GasInfo::new(1, 0)),
@@ -162,6 +166,72 @@ impl<'a> Storage for MoveStorage<'a>  {
         }
     }
 
+    fn scan(
+        &mut self,
+        start: Option<&[u8]>,
+        end: Option<&[u8]>,
+        order: Order,
+    ) -> BackendResult<u32> {
+        let object = self.object.lock().unwrap();
+        let cursor = start.map(|s| FieldKey::new(h256::sha3_256_of(s).into()));
+
+        let (values, _bytes_len_opt) = object.scan_fields(
+            self.layout_loader,
+            self.resolver,
+            cursor,
+            usize::MAX,
+            &Type::Vector(Box::new(Type::U8)), // Assuming values are Vec<u8>
+        )
+        .map_err(|e| BackendError::Unknown { msg: e.to_string() })?;
+
+        let move_layout = Vec::<u8>::type_layout();
+
+        let mut records: Vec<(FieldKey, Vec<u8>)> = values
+            .into_iter()
+            .filter_map(|(key, value)| {
+                self.serialize_value(&move_layout, &value)
+                    .ok()
+                    .map(|bytes| (key, bytes))
+            })
+            .collect();
+
+        if order == Order::Descending {
+            records.reverse();
+        }
+
+        // Apply end filter
+        if let Some(end_bytes) = end {
+            let end_key = FieldKey::new(h256::sha3_256_of(end_bytes).into());
+            records.retain(|(key, _)| match order {
+                Order::Ascending => *key < end_key,
+                Order::Descending => *key > end_key,
+            });
+        }
+
+        let id = self.iterator_id_counter;
+        self.iterator_id_counter += 1;
+        self.iterators.insert(id, (records, 0));
+
+        (Ok(id), GasInfo::new(1, 0))
+    }
+
+    fn next(&mut self, iterator_id: u32) -> BackendResult<Option<Record>> {
+        let (records, index) = match self.iterators.get_mut(&iterator_id) {
+            Some(it) => it,
+            None => return (Err(BackendError::IteratorDoesNotExist {id: iterator_id}), GasInfo::new(1, 0)),
+        };
+
+        if *index >= records.len() {
+            return (Ok(None), GasInfo::new(1, 0));
+        }
+
+        let (key, value) = &records[*index];
+        *index += 1;
+
+        let key_bytes: [u8; 32] = key.0.into();
+
+        (Ok(Some((key_bytes.to_vec(), value.clone()))), GasInfo::new(1, 0))
+    }
 }
 
 /* 
