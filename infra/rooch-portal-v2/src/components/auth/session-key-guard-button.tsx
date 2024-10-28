777e8f9294a4dea0aded3cf5563e292d7b1bb5ad
Jojoo <jojoo.eth@gmail.com>
2024-10-29 01:22:50 +0800

    feat: twitter verify

--- infra/rooch-portal-v2/src/components/auth/session-key-guard-button.tsx
+++ infra/rooch-portal-v2/src/components/auth/session-key-guard-button.tsx
@@ -49,6 +49,7 @@ export default function SessionKeyGuardButton({ children }: { children: ReactNod
               '0x1::*::*',
               '0x3::*::*',
               '0x176214bed3764a1c6a43dc1add387be5578ff8dbc263369f5bdc33a885a501ae::*::*',
+              '0x701c21bf1c8cd5af8c42983890d8ca55e7a820171b8e744c13f2d9998bf76cc3::*::*',
             ],
             maxInactiveInterval: 60 * 60 * 8,
           });
--- infra/rooch-portal-v2/src/layouts/components/nav-upgrade.tsx
+++ infra/rooch-portal-v2/src/layouts/components/nav-upgrade.tsx
@@ -62,6 +62,7 @@ export function NavUpgrade({ sx, ...other }: StackProps) {
                   '0x1::*::*',
                   '0x3::*::*',
                   '0x176214bed3764a1c6a43dc1add387be5578ff8dbc263369f5bdc33a885a501ae::*::*',
+                  '0x701c21bf1c8cd5af8c42983890d8ca55e7a820171b8e744c13f2d9998bf76cc3::*::*',
                 ],
                 maxInactiveInterval: 60 * 60 * 8,
               });
