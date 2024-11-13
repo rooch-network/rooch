43b30b29a7d7081442df9ef4b2811b3206eb0e70
Jojoo <jojoo.eth@gmail.com>
2024-11-13 12:04:47 +0800

    fix: build error

--- infra/rooch-portal-v2/src/app/trade/[tick]/page.tsx
+++ infra/rooch-portal-v2/src/app/trade/[tick]/page.tsx
@@ -1,12 +1,10 @@
 import MarketplaceView from 'src/sections/trade/view';
 
 export const metadata = {
-  title: 'MRC20 | Market',
+  title: 'Market | Orderbook',
 };
 
 export default function Page({ params }: { params: { tick: string } }) {
-  const { tick } = params;
-
   // if (!Object.keys(NETWORK_PACKAGE[NETWORK].tickInfo).includes(tick)) {
   //   return <NotMarketplaceFoundView />;
   // }
--- infra/rooch-portal-v2/src/app/trade/page.tsx
+++ infra/rooch-portal-v2/src/app/trade/page.tsx
@@ -3,7 +3,7 @@ import HomeView from 'src/sections/trade/home-view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'MRC20 | Innovation Zone',
+  title: 'Market | Orderbook',
 };
 
 export default function Page() {
