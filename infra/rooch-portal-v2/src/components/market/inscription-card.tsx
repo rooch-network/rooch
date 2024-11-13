a797dbb469004d1bbf09ee51a9ae9ed705007170
jojoo-eth <122259402+jojoo-eth@users.noreply.github.com>
2024-11-13 12:28:13 +0800

    Portal orderbook (#2896)
    * feat: orderbook
    
    * feat: orderbook beta
    
    * fix: build error
    
    * fix: build error

---
+++ infra/rooch-portal-v2/src/components/market/inscription-card.tsx
@@ -0,0 +1,91 @@
+import Link from 'next/link';
+
+import { yellow } from '@mui/material/colors';
+import { Chip, Stack, Typography } from '@mui/material';
+
+import { Iconify } from 'src/components/iconify';
+
+export interface InscriptionCardProps {
+  objectId?: string;
+  tick: string;
+  isVerified: boolean;
+  tokenBalance: string;
+  selectMode?: boolean;
+}
+
+export default function InscriptionCard({
+  objectId,
+  tick,
+  isVerified,
+  tokenBalance,
+  selectMode,
+}: InscriptionCardProps) {
+  return (
+    <Stack
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+      sx={{
+        p: 1,
+        borderRadius: '4px',
+      }}
+    >
+      <Stack
+        sx={{
+          width: '100%',
+        }}
+        direction="row"
+        justifyContent="space-between"
+      >
+        <Chip
+          size="small"
+          label={
+            <Stack direction="row" alignItems="center">
+              {tick}
+              {isVerified && (
+                <Iconify
+                  icon="solar:verified-check-bold"
+                  color={yellow.A200}
+                  width={16}
+                  sx={{
+                    ml: 0.5,
+                  }}
+                />
+              )}
+            </Stack>
+          }
+          variant="filled"
+          color="secondary"
+        />
+        {objectId && (
+          <>
+            {selectMode ? (
+              <Chip size="small" label={`#${objectId.slice(2, 8)}`} variant="soft" color="info" />
+            ) : (
+              <Link href={`https://suivision.xyz/object/${objectId}`} target="_blank">
+                <Chip size="small" label={`#${objectId.slice(2, 8)}`} variant="soft" color="info" />
+              </Link>
+            )}
+          </>
+        )}
+      </Stack>
+      <Typography
+        className="text-gray-400"
+        sx={{
+          fontSize: '0.875rem',
+          fontWeight: 600,
+        }}
+      >
+        Token Balance
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: '2rem',
+          fontWeight: 600,
+        }}
+      >
+        {tokenBalance}
+      </Typography>
+    </Stack>
+  );
+}
