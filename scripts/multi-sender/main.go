0f886a54c100fa388b14e130a7b3a488abbf9f19
Joe Chen <geometryolife@gmail.com>
2024-11-28 18:50:05 +0800

    update

--- scripts/multi-sender/main.go
+++ scripts/multi-sender/main.go
@@ -10,7 +10,9 @@ import (
 )
 
 func main() {
-	change_env := exec.Command("rooch", "env", "switch", "--alias", "main")
+  // Change the network
+  const NETWORK = "test"
+	change_env := exec.Command("rooch", "env", "switch", "--alias", NETWORK)
 	out, err := change_env.CombinedOutput()
 	if err != nil {
 		fmt.Printf("combined out:\n%s\n", string(out))
@@ -38,6 +40,7 @@ func read_file(path string) error {
       break
     }
     // rooch account transfer --to rooch1z6jp5k2vj2hs75zdqe5rhgdaj4w3hjnm693ks6xaxxdr78vvxreqwrrvpt --coin-type 0x3::gas_coin::RGas --amount 1
+    // Transfer 100 Rgas, 1 RGas = 100000000
     send := exec.Command("rooch", "account", "transfer", "--coin-type", "0x3::gas_coin::RGas", "--amount", "10000000000", "--to", string(line))
     _, err = send.CombinedOutput()
     if err != nil {
@@ -48,9 +51,6 @@ func read_file(path string) error {
     }
       fmt.Println(string(line), "=> ok")
     // fmt.Printf("combined out\n%s\n", string(out))
-
-
-    // fmt.Println(string(line))
     results = append(results, string(line))
   }
   fmt.Printf("Read results: %v\n", results)
