aac7c7fc21d086b72b2d1936e15b669be2807394
popcnt1 <ism3y@proton.me>
2024-12-29 00:39:12 +0800

    refactor(rooch-db): refine best rollback logic and structure
    1. Revise the best rollback logic with enhanced hash comparison and clearer user feedback. Improve structure definitions, parameter handling, and overall usability.
    2. Fix infinite loop issue in segment fetching with increment logic.

--- crates/rooch/src/commands/db/commands/best_rollback.rs
+++ crates/rooch/src/commands/db/commands/best_rollback.rs
@@ -7,23 +7,26 @@ use rooch_types::da::chunk::chunk_from_segments;
 use rooch_types::da::segment::{segment_from_bytes, Segment};
 use rooch_types::error::RoochResult;
 use rooch_types::transaction::{LedgerTransaction, LedgerTxData};
-use std::cmp::min;
 use std::collections::HashSet;
 
 /// try to find the best tx_order for rollback caused by chain reorg
 #[derive(Debug, Parser)]
 pub struct BestRollbackCommand {
-    #[clap(long = "da-url")]
+    #[clap(
+        long = "da-url",
+        help = "open-da rpc url. e.g., https://storage.googleapis.com/rooch-openda-testnet/7a6b74d2"
+    )]
     pub da_url: String,
 
     #[clap(
         long = "last-block-number",
-        help = "last avail block number in DA, got by `rooch rpc request --method rooch_status`"
+        help = "last_avail_block_number in da_info, got by `rooch rpc request --method rooch_status`"
     )]
     pub last_l2_block_number: u128,
     #[clap(
         long = "search-depth",
-        help = "max blocks search depth for rollback, default is 16. If we cannot find best rollback tx_order in search depth, increase this value"
+        help = "max l2 blocks search depth for rollback, default is 16. If we cannot find best rollback tx_order in search depth, increase this value",
+        default_value = "16"
     )]
     pub search_depth: Option<u64>,
     #[clap(long = "main", help = "Bitcoin Mainnet or not. default is false")]
@@ -32,41 +35,67 @@ pub struct BestRollbackCommand {
 
 impl BestRollbackCommand {
     pub async fn execute(self) -> RoochResult<()> {
-        let depth = self.search_depth.unwrap_or(16);
+        let depth = self.search_depth.unwrap();
         let mut da_hashes =
             get_block_hash_from_da_rpc(&self.da_url, self.last_l2_block_number, depth as u128)
                 .await?;
-        da_hashes.sort_by(|a, b| b.0.cmp(&a.0)); // order by block_number desc
-        let max_block_number = da_hashes.first();
-        if max_block_number.is_none() {
+        da_hashes.sort_by(|a, b| a.block_height.cmp(&b.block_height)); // order by block_height
+        if da_hashes.len() == 0 {
             println!("no btc block found in DA, please increase search depth");
             return Ok(());
         }
-        // compare block hash from DA and BTC, find the best rollback tx_order
+
+        tracing::info!(
+            "found {} btc blocks in DA, block_height range: {}-{}",
+            da_hashes.len(),
+            da_hashes.first().unwrap().block_height,
+            da_hashes.last().unwrap().block_height
+        );
+
+        // compare block hash from DA and BTC, find the best rollback tx_order:
+        // find the first mismatched block hash, and rollback to the previous tx_order.
         let mut best_tx_order = 0;
         let mut found_matched_block_hash = false;
-        for (da_block_number, da_block_hash, previous_tx_order) in da_hashes.iter() {
-            let btc_block_hash = get_block_hash_from_btc_rpc(*da_block_number, self.main).await?;
-            if btc_block_hash != *da_block_hash {
-                best_tx_order = min(best_tx_order, *previous_tx_order);
+        let mut found_best_tx_order = false;
+        for da_block_hash in da_hashes.iter() {
+            let block_height = da_block_hash.block_height;
+            let rpc_block_hash = get_block_hash_from_btc_rpc(block_height, self.main).await?;
+            if rpc_block_hash != da_block_hash.block_hash {
+                if found_matched_block_hash {
+                    best_tx_order = da_block_hash.previous_tx_order;
+                    found_best_tx_order = true;
+                    break;
+                }
             } else {
                 found_matched_block_hash = true;
-                break;
             }
         }
+
         if !found_matched_block_hash {
-            println!("no matched block hash found in BTC, please increase search depth");
+            println!("no matched block hash found, please increase search depth");
+            return Ok(());
+        }
+
+        if !found_best_tx_order {
+            println!("all block hash matched, no need to rollback");
+            return Ok(());
         } else {
-            println!("best rollback tx_order is {}", best_tx_order);
+            println!("best rollback tx_order: {}", best_tx_order);
         }
 
         Ok(())
     }
 }
 
-fn filter_l1block(mut tx_list: Vec<LedgerTransaction>) -> Vec<(u64, String, u64)> {
-    tx_list.reverse();
-    let mut block_hashes = vec![];
+struct BTCBlockHash {
+    block_height: u64,
+    block_hash: String,
+    previous_tx_order: u64, // tx before this l1block tx
+}
+
+fn filter_l1block(mut tx_list: Vec<LedgerTransaction>) -> Vec<BTCBlockHash> {
+    tx_list.reverse(); // reverse order, from latest to oldest
+    let mut block_hashes: Vec<BTCBlockHash> = vec![];
     for tx in tx_list {
         let tx_order = tx.sequence_info.tx_order;
         let previous_tx_order = tx_order - 1; // if block_hash mismatched, rollback to previous tx_order
@@ -74,7 +103,11 @@ fn filter_l1block(mut tx_list: Vec<LedgerTransaction>) -> Vec<(u64, String, u64)
         if let LedgerTxData::L1Block(l1_block) = tx_data {
             let block_hash = bitcoin::block::BlockHash::from_slice(&l1_block.block_hash).unwrap();
             let block_hash_str = block_hash.to_string();
-            block_hashes.push((l1_block.block_height, block_hash_str, previous_tx_order));
+            block_hashes.push(BTCBlockHash {
+                block_height: l1_block.block_height,
+                block_hash: block_hash_str,
+                previous_tx_order,
+            });
         }
     }
     block_hashes
@@ -84,22 +117,22 @@ async fn get_block_hash_from_da_rpc(
     da_url: &str,
     last_block_number: u128,
     search_depth: u128,
-) -> anyhow::Result<Vec<(u64, String, u64)>> {
-    let mut block_hash = vec![];
+) -> anyhow::Result<Vec<BTCBlockHash>> {
+    let mut block_hash_with_depth: Vec<BTCBlockHash> = vec![];
     let mut block_height_set = HashSet::new();
     for i in 0..=search_depth {
         let block_number = last_block_number - i;
         let tx_list = get_tx_list_from_chunk(da_url, block_number).await?;
         let block_hash_in_chunk = filter_l1block(tx_list);
-        for (block_height, block_hash_str, previous_tx_order) in block_hash_in_chunk {
-            if block_height_set.contains(&block_height) {
+        for block_hash in block_hash_in_chunk {
+            if block_height_set.contains(&block_hash.block_height) {
                 continue; // Reverse order, skip duplicate block_height. Higher tx_order, newer block
             }
-            block_height_set.insert(block_height);
-            block_hash.push((block_height, block_hash_str, previous_tx_order));
+            block_height_set.insert(block_hash.block_height);
+            block_hash_with_depth.push(block_hash);
         }
     }
-    Ok(block_hash)
+    Ok(block_hash_with_depth)
 }
 
 async fn get_tx_list_from_chunk(
@@ -107,7 +140,7 @@ async fn get_tx_list_from_chunk(
     chunk_id: u128,
 ) -> anyhow::Result<Vec<LedgerTransaction>> {
     let mut segments = Vec::new();
-    let segment_id: u64 = 0;
+    let mut segment_id: u64 = 0;
     loop {
         let segment = get_segment(da_url, chunk_id, segment_id).await?;
         let is_last = segment.is_last();
@@ -115,6 +148,7 @@ async fn get_tx_list_from_chunk(
         if is_last {
             break;
         }
+        segment_id += 1;
     }
     let chunk = chunk_from_segments(segments)?;
     let batch = chunk.get_batches().into_iter().next().unwrap();
