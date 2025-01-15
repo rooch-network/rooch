58e1fb9b1bd67a08ec2a21a52bb22bc2a1dbf8ed
popcnt1 <142196625+popcnt1@users.noreply.github.com>
2025-01-16 05:00:10 +0800

    feat(rooch-da): add file-based index loading and verification
    Introduce the ability to load and verify transaction indices from a file-based source. This includes adding CLI options `--load-from` and `--verify` to support initialization and validation against the original index file.

--- crates/rooch/src/commands/da/commands/index.rs
+++ crates/rooch/src/commands/da/commands/index.rs
@@ -9,13 +9,15 @@ use heed::{Database, Env, EnvOpenOptions};
 use moveos_types::h256::H256;
 use serde::{Deserialize, Serialize};
 use std::cmp::max;
+use std::io::BufRead;
 use std::path::PathBuf;
+use std::str::FromStr;
 
 const MAP_SIZE: usize = 1 << 34; // 16G
 const MAX_DBS: u32 = 16;
 const ORDER_DATABASE_NAME: &str = "order_db";
 
-#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
 pub struct TxPosition {
     pub tx_hash: H256,
     pub block_number: u128,
@@ -35,6 +37,13 @@ pub struct IndexCommand {
     pub reset_from: Option<u64>,
     #[clap(long = "max-block-number", help = "Max block number to index")]
     pub max_block_number: Option<u128>,
+    #[clap(long = "load-from", help = "Load from original file based index")]
+    pub load_from: Option<PathBuf>,
+    #[clap(
+        long = "verify",
+        help = "Verify index by comparing with original file based index"
+    )]
+    pub verify: bool,
 }
 
 pub struct Indexer {
@@ -44,13 +53,94 @@ pub struct Indexer {
 }
 
 impl Indexer {
-    pub fn new(db_path: PathBuf, reset_from: Option<u64>) -> anyhow::Result<Self> {
+    pub fn load_from_file(
+        db_path: PathBuf,
+        file_path: PathBuf,
+        verify: bool,
+    ) -> anyhow::Result<Self> {
+        let file = std::fs::File::open(file_path.clone())?;
+        let reader = std::io::BufReader::new(file);
+
+        let mut last_tx_order = 0;
+        let mut last_block_number = 0;
+
+        let db_env = Self::create_env(db_path.clone())?;
+        let mut wtxn = db_env.write_txn()?;
+        let db: Database<U64<BigEndian>, SerdeBincode<TxPosition>> =
+            db_env.create_database(&mut wtxn, Some(ORDER_DATABASE_NAME))?;
+
+        for line in reader.lines() {
+            let line = line?;
+            let parts: Vec<&str> = line.split(':').collect();
+            if parts.len() != 3 {
+                return Err(anyhow!("invalid line: {}", line));
+            }
+            let tx_order = parts[0].parse::<u64>()?;
+            let tx_hash = H256::from_str(parts[1])?;
+            let block_number = parts[2].parse::<u128>()?;
+            let tx_position = TxPosition {
+                tx_hash,
+                block_number,
+            };
+            db.put(&mut wtxn, &tx_order, &tx_position)?;
+            last_tx_order = tx_order;
+            last_block_number = block_number;
+        }
+        wtxn.commit()?;
+        db_env.force_sync()?;
+
+        let mut indexer = Indexer {
+            db_env,
+            last_tx_order: 0,
+            last_block_number: 0,
+        };
+        indexer.init_cursor()?;
+        assert_eq!(last_tx_order, indexer.last_tx_order);
+        assert_eq!(last_block_number, indexer.last_block_number);
+
+        if verify {
+            let db_env = Self::create_env(db_path)?;
+            let file = std::fs::File::open(file_path)?;
+            let reader = std::io::BufReader::new(file);
+            let rtxn = db_env.read_txn()?;
+            let db: Database<U64<BigEndian>, SerdeBincode<TxPosition>> = db_env
+                .open_database(&rtxn, Some(ORDER_DATABASE_NAME))?
+                .ok_or(anyhow::anyhow!("db not found"))?;
+            for line in reader.lines() {
+                let line = line?;
+                let parts: Vec<&str> = line.split(':').collect();
+                if parts.len() != 3 {
+                    return Err(anyhow!("invalid line: {}", line));
+                }
+                let tx_order = parts[0].parse::<u64>()?;
+                let tx_hash = H256::from_str(parts[1])?;
+                let block_number = parts[2].parse::<u128>()?;
+                let tx_position = TxPosition {
+                    tx_hash,
+                    block_number,
+                };
+                let ret = db.get(&rtxn, &tx_order)?;
+                let ret = ret.ok_or(anyhow!("tx_order not found: {}", tx_order))?;
+                assert_eq!(ret, tx_position);
+            }
+            rtxn.commit()?;
+        }
+
+        Ok(indexer)
+    }
+
+    fn create_env(db_path: PathBuf) -> anyhow::Result<Env> {
         let env = unsafe {
             EnvOpenOptions::new()
                 .map_size(MAP_SIZE) // 16G
                 .max_dbs(MAX_DBS)
                 .open(db_path)?
         };
+        Ok(env)
+    }
+
+    pub fn new(db_path: PathBuf, reset_from: Option<u64>) -> anyhow::Result<Self> {
+        let env = Self::create_env(db_path)?;
         let mut indexer = Indexer {
             db_env: env,
             last_tx_order: 0,
