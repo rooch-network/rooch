5e3f2199d2fcb790cfdf5b36882713b2aa7b12d9
popcnt1 <142196625+popcnt1@users.noreply.github.com>
2025-01-21 14:35:06 +0800

    chore(rooch-da): update doc comments in status struct
    Clarified documentation for `avail_backends` to specify block number update behavior. Improved overall comment readability for better understanding.

--- crates/rooch-types/src/da/status.rs
+++ crates/rooch-types/src/da/status.rs
@@ -28,6 +28,7 @@ pub struct DAServerStatus {
     /// 1. DA backends collapse
     /// 2. RoochStore is not consistent (cannot get tx from DB by tx order)
     pub last_avail_block_update_time: Option<u64>,
-    /// The available backend_identifiers and their last block numbers. e.g., [("openda-gcs", 100), ("openda-fs", 200)]
+    /// The available backend_identifiers and their latest block numbers. e.g., [("openda-gcs", 100), ("openda-fs", 200)]
+    /// Block numbers only update in runtime, not in startup. In the beginning, it should be 0.
     pub avail_backends: Vec<(String, u128)>,
 }
