c57c39e270575bc7fa3df044669ba463620cc21c
jolestar <jolestar@gmail.com>
2025-02-10 14:18:46 +0800

    fixup

--- examples/onchain_ai_chat/web/src/networks.ts
+++ examples/onchain_ai_chat/web/src/networks.ts
@@ -1,32 +1,32 @@
 import { getRoochNodeUrl } from '@roochnetwork/rooch-sdk'
 import { createNetworkConfig } from "@roochnetwork/rooch-sdk-kit"
 
-import { DEVNET_PACKAGE_ID, MAINNET_PACKAGE_ID } from './constants'
+import { PACKAGE_ID } from './constants'
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
     mainnet: {
       url: getRoochNodeUrl("mainnet"),
       variables: {
-        packageId: MAINNET_PACKAGE_ID,
+        packageId: PACKAGE_ID,
       },
     },
     devnet: {
       url: getRoochNodeUrl("devnet"),
       variables: {
-        packageId: DEVNET_PACKAGE_ID,
+        packageId: PACKAGE_ID,
       },
     },
     testnet: {
       url: getRoochNodeUrl("testnet"),
       variables: {
-        packageId: DEVNET_PACKAGE_ID,
+        packageId: PACKAGE_ID,
       },
     },
     localnet: {
       url: getRoochNodeUrl("localnet"),
       variables: {
-        packageId: DEVNET_PACKAGE_ID,
+        packageId: PACKAGE_ID,
       },
     },
   })
