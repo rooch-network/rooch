2a35c9c54abf2c2e55c2c312f702c55a14bf498a
steelgeek091 <steelgeek091@gmail.com>
2025-02-18 22:25:48 +0800

    add license info

--- crates/rooch-framework-tests/src/tests/check_tx_size.rs
+++ crates/rooch-framework-tests/src/tests/check_tx_size.rs
@@ -1,3 +1,6 @@
+// Copyright (c) RoochNetwork
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::binding_test::RustBindingTest;
 use move_core_types::identifier::Identifier;
 use move_core_types::language_storage::ModuleId;
