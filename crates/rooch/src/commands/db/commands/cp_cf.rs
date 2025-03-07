40b76ea168f89aa65453586dc51de124909b0aa7
popcnt <142196625+popcnt1@users.noreply.github.com>
2025-03-08 01:15:21 +0800

    feat(db): support copying all column families if unspecified (#3407)
    Previously, the `CpCfCommand` required a specific column family name to copy. This update allows the omission of `cf-name` to copy all column families by default. Enhanced logic ensures batch migration across multiple column families seamlessly.

--- crates/rooch/src/commands/db/commands/cp_cf.rs
+++ crates/rooch/src/commands/db/commands/cp_cf.rs
@@ -9,8 +9,11 @@ use rooch_types::error::RoochResult;
 /// copy column family by column family name
 #[derive(Debug, Parser)]
 pub struct CpCfCommand {
-    #[clap(long = "cf-name")]
-    pub cf_name: String,
+    #[clap(
+        long = "cf-name",
+        help = "column family name, if not set, copy all column families"
+    )]
+    pub cf_name: Option<String>,
     #[clap(long = "dst", help = "destination path to new rocksdb")]
     pub dst: String,
     #[clap(long = "src", help = "source path to rocksdb")]
@@ -19,13 +22,21 @@ pub struct CpCfCommand {
 
 impl CpCfCommand {
     pub fn execute(self) -> RoochResult<()> {
-        let source_db = open_inner_rocks(&self.src, vec![self.cf_name.clone()], true)?;
-        let target_db = open_inner_rocks(&self.dst, vec![self.cf_name.clone()], false)?;
+        let cf_names = if let Some(cf_name) = self.cf_name.clone() {
+            vec![cf_name]
+        } else {
+            let mut column_families = moveos_store::StoreMeta::get_column_family_names().to_vec();
+            column_families.append(&mut rooch_store::StoreMeta::get_column_family_names().to_vec());
+            column_families.into_iter().map(String::from).collect()
+        };
+
+        let source_db = open_inner_rocks(&self.src, cf_names.clone(), true)?;
+        let target_db = open_inner_rocks(&self.dst, cf_names.clone(), false)?;
         let migrator = DBMigrator {
             source_db,
             target_db,
             batch_size: 1048576,
-            cf_name: self.cf_name.clone(),
+            cf_names,
         };
         migrator.migrate()?;
         Ok(())
@@ -36,40 +47,40 @@ struct DBMigrator {
     source_db: DB,
     target_db: DB,
     batch_size: usize,
-    cf_name: String,
+    cf_names: Vec<String>,
 }
 
 impl DBMigrator {
     pub fn migrate(&self) -> anyhow::Result<()> {
-        let cf_name = self.cf_name.clone();
+        for cf_name in self.cf_names.iter() {
+            let source_cf = self.source_db.cf_handle(cf_name).unwrap();
+            let target_cf = self.target_db.cf_handle(cf_name).unwrap();
 
-        let source_cf = self.source_db.cf_handle(&cf_name).unwrap();
-        let target_cf = self.target_db.cf_handle(&cf_name).unwrap();
+            let mut batch = WriteBatch::default();
+            let mut count = 0;
 
-        let mut batch = WriteBatch::default();
-        let mut count = 0;
+            let iter = self
+                .source_db
+                .iterator_cf(source_cf, rocksdb::IteratorMode::Start);
 
-        let iter = self
-            .source_db
-            .iterator_cf(source_cf, rocksdb::IteratorMode::Start);
+            for result in iter {
+                let (key, value) = result?;
+                batch.put_cf(&target_cf, key, value);
+                count += 1;
 
-        for result in iter {
-            let (key, value) = result?;
-            batch.put_cf(&target_cf, key, value);
-            count += 1;
-
-            if count % self.batch_size == 0 {
-                self.target_db.write(batch)?;
-                batch = WriteBatch::default();
-                tracing::info!("{} records migrated", count);
+                if count % self.batch_size == 0 {
+                    self.target_db.write(batch)?;
+                    batch = WriteBatch::default();
+                    tracing::info!("{} records migrated", count);
+                }
             }
-        }
 
-        if !batch.is_empty() {
-            self.target_db.write(batch)?;
-        }
+            if !batch.is_empty() {
+                self.target_db.write(batch)?;
+            }
 
-        self.target_db.flush()?;
+            self.target_db.flush()?;
+        }
 
         Ok(())
     }
