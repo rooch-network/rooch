b0999f8a53bd281f9ab566dd09a4c4425b8309c6
Joe Chen <geometryolife@gmail.com>
2025-03-20 12:21:53 +0800

    add sdk example getChainId

---
+++ docs/website/examples/ts-sdk/getChainId/src/index.ts
@@ -0,0 +1,14 @@
+import { getRoochNodeUrl, RoochClient } from '@roochnetwork/rooch-sdk'
+
+const NETWORK = 'mainnet' // 'mainnet' or 'testnet'
+
+async function main() {
+  const client = new RoochClient({
+    url: getRoochNodeUrl(NETWORK)
+  });
+
+  const chainId = await client.getChainId();
+  console.log('Chain ID:', chainId);
+}
+
+main()
