0411922f856615ad9b0f0243b5648e32df3dabb3
popcnt1 <142196625+popcnt1@users.noreply.github.com>
2025-03-26 04:00:53 +0800

    Run tasks and tests in parallel with error handling
    Updated workflows and scripts to execute tasks and tests in parallel, improving efficiency. Added detailed error handling to ensure failures are properly logged and the correct exit codes are returned. This enhances reliability and provides clearer feedback during CI/CD processes.

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -121,9 +121,23 @@ if [ ! -z "$STD_TEST" ]; then
         --cargo-profile optci \
 
     # Run framework tests in parallel
-    cargo test --profile optci -p rooch-framework-tests -p rooch-integration-test-runner -- --test-threads=8
-    cargo test --profile optci -p rooch-framework-tests bitcoin_test -- --test-threads=8
-    wait
+    cargo test --profile optci -p rooch-framework-tests -p rooch-integration-test-runner -- --test-threads=8 &
+    PID_INTEG_TESTS=$!
+    cargo test --profile optci -p rooch-framework-tests bitcoin_test -- --test-threads=8 &
+    PID_BITCOIN_TESTS=$!
+    wait $PID_INTEG_TESTS
+    EXIT_CODE_INTEG_TESTS=$?
+    wait $PID_BITCOIN_TESTS
+    EXIT_CODE_BITCOIN_TESTS=$?
+    if [ $EXIT_CODE_INTEG_TESTS -ne 0 ]; then
+    echo "Error: The framework-rooch-integration-test failed."
+    exit $EXIT_CODE_INTEG_TESTS
+    fi
+    if [ $EXIT_CODE_BITCOIN_TESTS -ne 0 ]; then
+    echo "Error: The framework-rooch-bitcoin-test failed."
+    exit $EXIT_CODE_BITCOIN_TESTS
+    fi
+    echo "Framework tests successfully."
 
     # Run integration tests separately without parallel execution
     echo "Running integration tests..."
