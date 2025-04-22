91faa806ac6621013798091d9c4cc82f8d7ab2fa
baichuan3 <muzixinly@gmail.com>
2025-04-22 15:02:41 +0800

    fixup typo

--- frameworks/rooch-framework/src/natives/gas_parameter/ability.rs
+++ frameworks/rooch-framework/src/natives/gas_parameter/ability.rs
@@ -4,6 +4,6 @@
 use moveos_stdlib::natives::moveos_stdlib::ability::GasParameters;
 
 crate::natives::gas_parameter::native::define_gas_parameters_for_natives!(GasParameters, "ability", [
-    [.get_abilities.base, optional "decode.base", 0],
-    [.get_abilities.per_byte_in_str, optional "decode.per_byte_in_str", 0],
+    [.get_abilities.base, optional "get_abilities.base", 0],
+    [.get_abilities.per_byte_in_str, optional "get_abilities.per_byte_in_str", 0],
 ]);
