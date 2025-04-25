9dddf60639ef37e4f33097f1203cc21e0c86b0ef
ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4 <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
2025-04-25 13:42:58 +0800

    sdk/typescript: add schnorr signature support to typescript sdk. (#3560)
    * sdk/typescript: add schnorr signature support to typescript sdk.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * sdk/typescript: add schnorr signing support.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>

--- sdk/typescript/rooch-sdk/src/crypto/signatureScheme.ts
+++ sdk/typescript/rooch-sdk/src/crypto/signatureScheme.ts
@@ -5,10 +5,6 @@ export const SIGNATURE_SCHEME_TO_FLAG = {
   ED25519: 0x00,
   Secp256k1: 0x01,
 } as const
-export const SIGNATURE_SCHEME_TO_SIZE = {
-  ED25519: 32,
-  Secp256k1: 33,
-}
 
 export const SIGNATURE_FLAG_TO_SCHEME = {
   0x00: 'ED25519',
