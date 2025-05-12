93d324a5cb59c585487b64758a5881ac52863a09
ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4 <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
2025-05-12 21:49:43 +0800

    docs/website: publish blog posts of schnorr signature and update move examples tutorial to include nostr. (#3581)
    * docs/website: publish blog posts.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * docs/website: update blog posts.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * docs/website: fix build error by adding an author to team.tsx.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * docs/website: use permalinks instead of relative paths.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * docs/website: revert package.json change.
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    
    * Update schnorr-signature-and-their-applications.zh-CN.mdx
    
    * Update schnorr-signature-and-their-applications.en-US.mdx
    
    ---------
    
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
    Co-authored-by: jolestar <jolestar@gmail.com>

--- docs/website/data/team.tsx
+++ docs/website/data/team.tsx
@@ -1,51 +1,55 @@
 const ROOCH_TEAM: Record<string, AuthorDetails> = {
   jolestar: {
-    name: "Jolestar",
-    twitterUsername: "jolestar",
-    avatar: "/images/people/jolestar.jpg",
+    name: 'Jolestar',
+    twitterUsername: 'jolestar',
+    avatar: '/images/people/jolestar.jpg',
   },
   haichao: {
-    name: "Haichao",
-    twitterUsername: "HaichaoZ",
-    avatar: "/images/people/haichao.jpg",
+    name: 'Haichao',
+    twitterUsername: 'HaichaoZ',
+    avatar: '/images/people/haichao.jpg',
   },
   ren: {
-    name: "Ren",
-    twitterUsername: "renryderauthor",
-    avatar: "/images/people/ren.jpg",
+    name: 'Ren',
+    twitterUsername: 'renryderauthor',
+    avatar: '/images/people/ren.jpg',
   },
   omnihand: {
-    name: "Omnihand",
-    twitterUsername: "metairis",
-    avatar: "/images/people/omnihand.jpg",
+    name: 'Omnihand',
+    twitterUsername: 'metairis',
+    avatar: '/images/people/omnihand.jpg',
   },
   popcnt: {
-    name: "POPCNT",
-    twitterUsername: "evex_popcnt",
-    avatar: "/images/people/popcnt.jpeg",
+    name: 'POPCNT',
+    twitterUsername: 'evex_popcnt',
+    avatar: '/images/people/popcnt.jpeg',
   },
   geometryolife: {
-      name: "Joe Chen",
-      twitterUsername: "joechendev",
-      avatar: "/images/people/joe.jpg"
-    },
+    name: 'Joe Chen',
+    twitterUsername: 'joechendev',
+    avatar: '/images/people/joe.jpg',
+  },
   justin: {
-    name: "Justin Y.",
-    twitterUsername: "justinkn08",
-    avatar: "/images/people/justin.jpg"
+    name: 'Justin Y.',
+    twitterUsername: 'justinkn08',
+    avatar: '/images/people/justin.jpg',
   },
   baichuan: {
-    name: "Bai Chuan",
-    twitterUsername: "muzixinly",
-    avatar: "/images/people/baichuan.jpg"
-  }
-};
+    name: 'Bai Chuan',
+    twitterUsername: 'muzixinly',
+    avatar: '/images/people/baichuan.jpg',
+  },
+  ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4: {
+    name: 'ZHANG, HENGMING',
+    avatar: '/images/people/ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4.jpg', // Initials by DiceBear, licensed under CC0 1.0 .
+  },
+}
 
-export type Author = keyof typeof ROOCH_TEAM;
+export type Author = keyof typeof ROOCH_TEAM
 export type AuthorDetails = {
-  name: string;
-  twitterUsername?: string;
-  avatar: string;
-};
+  name: string
+  twitterUsername?: string
+  avatar: string
+}
 
-export default ROOCH_TEAM;
+export default ROOCH_TEAM
