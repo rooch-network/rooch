09f99b8094baa1c175e677005072ccb5788d880a
ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>
2025-06-18 20:29:56 +0800

    commands/account: add sequence_number to account show command and add test case.
    Signed-off-by: ZHANG, HENGMING <22887031+ghpZ54K8ZRwU62zGVSePPs97yAv9swuAY0mVDR4@users.noreply.github.com>

--- crates/rooch/src/commands/account/commands/show.rs
+++ crates/rooch/src/commands/account/commands/show.rs
@@ -4,7 +4,8 @@
 use crate::cli_types::{CommandAction, WalletContextOptions};
 use async_trait::async_trait;
 use clap::Parser;
-use rooch_rpc_api::jsonrpc_types::{BitcoinAddressView, RoochAddressView};
+use rooch_rpc_api::jsonrpc_types::{BitcoinAddressView, NetworkAddressView};
+use rooch_types::address::NetworkAddress;
 use rooch_types::{address::ParsedAddress, error::RoochResult};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
@@ -25,82 +26,71 @@ pub struct ShowCommand {
     #[clap(flatten)]
     pub context_options: WalletContextOptions,
 
-    #[clap(short = 'l', long = "limit", default_value = "50")]
-    limit: u64,
-
     /// Return command outputs in json format
     #[clap(long, default_value = "false")]
     json: bool,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
-pub struct AccountInfoView {
-    pub address: RoochAddressView,
+pub struct NetworkAccountView {
+    pub network_address: NetworkAddressView,
     pub bitcoin_address: Option<BitcoinAddressView>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, JsonSchema)]
-pub struct RoochNetworkAccountView {
-    pub account: AccountInfoView,
-}
-
-impl RoochNetworkAccountView {
-    pub fn from_account(
-        address: RoochAddressView,
+impl NetworkAccountView {
+    pub fn new(
+        network_address: NetworkAddressView,
         bitcoin_address: Option<BitcoinAddressView>,
     ) -> Self {
-        // account info view with rooch address
-        let account_info_view = AccountInfoView {
-            address,
+        // network account view with network address (rooch address and sequence number) and bitcoin address
+        NetworkAccountView {
+            network_address,
             bitcoin_address,
-        };
-        RoochNetworkAccountView {
-            account: account_info_view,
         }
     }
 }
 
 #[async_trait]
-impl CommandAction<Option<RoochNetworkAccountView>> for ShowCommand {
-    async fn execute(self) -> RoochResult<Option<RoochNetworkAccountView>> {
+impl CommandAction<Option<NetworkAccountView>> for ShowCommand {
+    async fn execute(self) -> RoochResult<Option<NetworkAccountView>> {
         let context = self.context_options.build()?;
         let client = context.get_client().await?;
         let mapping = context.address_mapping();
-        let rooch_address = self.address.into_rooch_address(&mapping)?;
-        let rooch_address_view = RoochAddressView::from(rooch_address);
+        let rooch_address = self.address.clone().into_rooch_address(&mapping)?;
+        let sequence_number = client.rooch.get_sequence_number(rooch_address).await?;
+        let network_address = NetworkAddress::new(rooch_address, sequence_number);
+        let network_address_view = NetworkAddressView::from(network_address);
         let bitcoin_address_opt = client.rooch.resolve_bitcoin_address(rooch_address).await?;
         // rooch network account info from input address
-        let rooch_network_account_view = if bitcoin_address_opt.clone().is_some() {
+        let network_account_view = if bitcoin_address_opt.clone().is_some() {
             let bitcoin_address = bitcoin_address_opt.clone().unwrap();
             let bitcoin_address_view = BitcoinAddressView::from(bitcoin_address);
-            RoochNetworkAccountView::from_account(rooch_address_view, Some(bitcoin_address_view))
+            NetworkAccountView::new(network_address_view, Some(bitcoin_address_view))
         } else {
-            RoochNetworkAccountView::from_account(rooch_address_view, None)
+            NetworkAccountView::new(network_address_view, None)
         };
 
         if self.json {
-            Ok(Some(rooch_network_account_view))
+            Ok(Some(network_account_view))
         } else {
             // vectors
-            let mut formatted_account_info_header = vec![];
-            let mut formatted_account_info = vec![];
+            let mut formatted_network_address_header = vec![];
+            let mut formatted_network_address = vec![];
 
             // terminal
             let (width, height) = get_terminal_size();
 
-            // account
-            let mut account_info_builder = Builder::default();
-            formatted_account_info_header.push("Rooch Address".to_owned());
-            formatted_account_info.push(rooch_address.to_bech32());
-            if bitcoin_address_opt.clone().is_some() {
-                formatted_account_info_header.push("Bitcoin Address".to_owned());
-                formatted_account_info.push(bitcoin_address_opt.unwrap().to_string());
-            }
-            account_info_builder.push_record(formatted_account_info_header);
-            account_info_builder.push_record(formatted_account_info);
-            let mut account_info_table = account_info_builder.build();
-            account_info_table
-                .with(Panel::header("Account Info"))
+            // network address
+            let mut network_address_builder = Builder::default();
+            formatted_network_address_header.push("Rooch Address".to_owned());
+            formatted_network_address.push(rooch_address.to_bech32());
+            formatted_network_address_header.push("Sequence Number".to_owned());
+            formatted_network_address.push(sequence_number.to_string());
+            network_address_builder.push_record(formatted_network_address_header);
+            network_address_builder.push_record(formatted_network_address);
+            let mut network_address_table = network_address_builder.build();
+            network_address_table
+                .with(Panel::header("Network Address"))
                 .with(Style::rounded())
                 .with(Width::wrap(width).priority(PriorityRight::new()))
                 .with(Width::increase(width))
@@ -108,7 +98,25 @@ impl CommandAction<Option<RoochNetworkAccountView>> for ShowCommand {
                 .with(Height::increase(height))
                 .to_string();
 
-            println!("{}", account_info_table);
+            println!("{}", network_address_table);
+
+            // bitcoin address
+            if bitcoin_address_opt.clone().is_some() {
+                let mut formatted_bitcoin_address = vec![];
+                let mut bitcoin_address_builder = Builder::default();
+                formatted_bitcoin_address.push(bitcoin_address_opt.unwrap().to_string());
+                bitcoin_address_builder.push_record(formatted_bitcoin_address);
+                let mut bitcoin_address_table = bitcoin_address_builder.build();
+                bitcoin_address_table
+                    .with(Panel::header("Bitcoin Address"))
+                    .with(Style::rounded())
+                    .with(Width::wrap(width).priority(PriorityRight::new()))
+                    .with(Width::increase(width))
+                    .with(Height::limit(height))
+                    .with(Height::increase(height))
+                    .to_string();
+                println!("{}", bitcoin_address_table);
+            }
 
             Ok(None)
         }
