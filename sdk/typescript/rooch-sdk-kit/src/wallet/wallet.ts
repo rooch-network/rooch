9500583a6c1507b84181e5ee0704fd12d858411d
jolestar <jolestar@gmail.com>
2025-09-25 19:52:38 +0800

    Release v0.11 and typescript sdk v0.4.0

--- sdk/typescript/rooch-sdk-kit/src/wallet/wallet.ts
+++ sdk/typescript/rooch-sdk-kit/src/wallet/wallet.ts
@@ -1,12 +1,12 @@
 // Copyright (c) RoochNetwork
 // SPDX-License-Identifier: Apache-2.0
 
-import { ThirdPartyAddress, Bytes, Signer } from '@roochnetwork/rooch-sdk'
+import { ThirdPartyAddress, Bytes, BitcoinWalletSigner } from '@roochnetwork/rooch-sdk'
 
 import { SupportChain } from '../feature/index.js'
 import { WalletNetworkType } from './types.js'
 
-export abstract class Wallet extends Signer {
+export abstract class Wallet extends BitcoinWalletSigner {
   protected address: ThirdPartyAddress[] | undefined
   protected publicKey: string | undefined
   protected currentAddress: ThirdPartyAddress | undefined
